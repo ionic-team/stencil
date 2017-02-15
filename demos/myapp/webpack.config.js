3955ff0858f97a61f4e4cc111f3b8cb39fdf3c7c
Adam Bradley <adambradley25@gmail.com>
2017-02-15 14:36:37 -0600

    add webpack and closure to demo

---
+++ demos/myapp/webpack.config.js
@@ -0,0 +1,18 @@
+var webpack = require('webpack');
+var path = require('path');
+
+
+module.exports = {
+  entry: {
+    app: './dist/app/app.client.js'
+  },
+  output: {
+    filename: './www/build/app.client.js'
+  },
+  resolve: {
+    alias: {
+        'ionic-core': path.resolve(__dirname, '../../ionic-core/dist/es2015/index.js'),
+        'ionic-ui': path.resolve(__dirname, '../../ionic-ui/dist/es2015/index.js')
+    }
+  }
+};
