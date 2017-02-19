49c1684bd1a4b8b7947abc7af0f83b8622cff7f0
Adam Bradley <adambradley25@gmail.com>
2017-02-19 10:21:20 -0600

    platform server stub

--- ionic-core/src/server/platform-server.ts
+++ ionic-core/src/server/platform-server.ts
@@ -1,6 +1,16 @@
 import { AppInitOptions } from '../shared/interfaces';
+import { createApp } from '../shared/renderer';
 
 
 export function bootstrapServer(rootComponent: any, opts?: AppInitOptions) {
+  console.time(`bootstrapServer`);
 
+  opts = opts || {};
+
+  const window = global;
+  const document = {};
+
+  createApp(window, document, rootComponent, opts);
+
+  console.timeEnd(`bootstrapServer`);
 }
