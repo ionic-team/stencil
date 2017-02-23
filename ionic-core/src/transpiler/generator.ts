05cd5ca6e0c776135e9bc1e6ca97b4be293caf54
Adam Bradley <adambradley25@gmail.com>
2017-02-22 20:27:12 -0600

    expose generateComponentMeta

--- ionic-core/src/transpiler/generator.ts
+++ ionic-core/src/transpiler/generator.ts
@@ -1,4 +1,5 @@
 import { TranspileOptions, TranspileContext, ComponentMeta, FileMeta } from './interfaces';
+import { compileTemplate } from './template-compiler';
 
 
 export function generateComponentFile(file: FileMeta, opts?: TranspileOptions, ctx?: TranspileContext) {
@@ -27,19 +28,38 @@ export function generateComponentDecorator(c: ComponentMeta) {
   c.outputComponentDecorator = c.inputComponentDecorator;
 
   if (c.templateRenderFn && (c.templateUrl || c.template)) {
-    const d: string[] = [];
+    const meta = generateComponentDecoratorMeta(c);
 
-    if (c.tag) {
-      d.push(`  tag: '${c.tag}'`);
-    }
-
-    d.push(`  render: ${c.templateRenderFn}`);
-
-    if (c.templateStaticRenderFns) {
-      d.push(`  staticRenderFns: ${c.templateStaticRenderFns}`);
-    }
-
-    c.outputComponentDecorator = `@Component({\n${d.join(',\n')}\n})`;
+    c.outputComponentDecorator = `@Component(${meta})`;
   }
 }
 
+
+export function generateComponentDecoratorMeta(c: ComponentMeta) {
+  const d: string[] = [];
+
+  if (c.tag) {
+    d.push(`  tag: '${c.tag}'`);
+  }
+
+  d.push(`  render: ${c.templateRenderFn}`);
+
+  if (c.templateStaticRenderFns) {
+    d.push(`  staticRenderFns: ${c.templateStaticRenderFns}`);
+  }
+
+  return `{\n${d.join(',\n')}\n}`;
+}
+
+
+export function generateComponentMeta(tag: string, template: string) {
+  const c = compileTemplate(template)
+
+  if (c.errors && c.errors.length) {
+    throw c.errors.join('\n');
+  }
+
+  c.tag = tag;
+
+  return generateComponentDecoratorMeta(c);
+}
--- ionic-core/src/transpiler/index.ts
+++ ionic-core/src/transpiler/index.ts
@@ -1,3 +1,4 @@
 
 export { transpileProject } from './transpiler';
 export { compileTemplate } from './template-compiler';
+export { generateComponentMeta } from './generator';
