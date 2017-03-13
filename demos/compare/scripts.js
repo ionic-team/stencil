4b69d3ab3dc01f44efe1e66d8bfdaadcc06e4349
Adam Bradley <adambradley25@gmail.com>
2017-03-13 10:28:38 -0500

    compare page

---
+++ demos/compare/scripts.js
@@ -0,0 +1,48 @@
+
+var compareIndexes = Object.keys(compareData)
+var rootEle = document.createElement('div');
+
+var search = window.location.search.replace('?', '');
+
+for (var i = 0; i < compareIndexes.length; i++) {
+  if (ionic2Data[compareIndexes[i]]) {
+
+    if (search) {
+      if (compareIndexes[i].indexOf(search) === -1) {
+        continue;
+      }
+    }
+
+    buildCompareRow(compareIndexes[i]);
+  }
+}
+
+document.body.appendChild(rootEle);
+
+
+function buildCompareRow(compareIndex) {
+  console.log(compareIndex);
+
+  var rowEle = document.createElement('div');
+
+  var ionic2IFrame = buildIframe('/demos/ionic2' + compareIndex);
+  rowEle.appendChild(ionic2IFrame);
+
+  var compareIFrame = buildIframe('/demos/web' + compareIndex);
+  rowEle.appendChild(compareIFrame);
+
+  rootEle.appendChild(rowEle);
+}
+
+
+function buildIframe(src) {
+  var iframeEle = document.createElement('iframe');
+
+  iframeEle.src = src;
+  iframeEle.frameBorder = 0;
+
+  var wrapper = document.createElement('div');
+  wrapper.className = 'iframe-wrapper';
+  wrapper.appendChild(iframeEle);
+  return wrapper;
+}
---
+++ scripts/build.compare.js
@@ -0,0 +1,121 @@
+var path = require('path');
+var fs = require('fs');
+
+
+function buildWebDemos() {
+  var data = {};
+  var rootDir = path.join('demos/web');
+
+  fs.readdirSync(rootDir).forEach(file => {
+    var e2eDir = path.join(rootDir, file);
+
+    var stats = fs.statSync(e2eDir);
+    if (!stats.isDirectory()) return;
+
+    fs.readdirSync(e2eDir).forEach(file => {
+      var e2eSubDir = path.join(e2eDir, file);
+
+      var stats = fs.statSync(e2eSubDir);
+      if (!stats.isDirectory()) return;
+
+      fs.readdirSync(e2eSubDir).forEach(file => {
+        var f = path.join(e2eSubDir, file);
+
+        if (file === 'main.html') {
+          var index = path.join(e2eSubDir, 'index.html');
+          var mainContent = fs.readFileSync(f).toString();
+
+          createWebIndex(index, mainContent);
+
+          data[index.replace(rootDir, '')] = true;
+        }
+
+      });
+
+    });
+
+  });
+
+  var data = `var compareData = ${JSON.stringify(data, null, 2)};`
+
+  fs.writeFileSync('demos/compare/web.js', data);
+}
+
+
+function createWebIndex(index, mainContent) {
+  console.log(index);
+
+  mainContent = mainContent.replace(/ item-left/g, ' slot="item-left"');
+  mainContent = mainContent.replace(/ item-right/g, ' slot="item-right"');
+
+  var content = `<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="UTF-8">
+  <title>Ionic</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  <meta name="format-detection" content="telephone=no">
+  <meta name="msapplication-tap-highlight" content="no">
+  <link href="/dist/themes/ionic.css" rel="stylesheet">
+  <script src="/dist/ionic-web/dist/ionic.web.js" static-dir="/dist/ionic-web/dist/"></script>
+</head>
+<body>
+
+  <ion-app>
+
+<!-- main start -->
+
+${mainContent}
+
+<!-- main end -->
+
+  </ion-app>
+
+</body>
+</html>`;
+
+
+  fs.writeFileSync(index, content);
+
+}
+
+
+function buildIonic2Compare() {
+  var data = {};
+  var rootDir = path.join('demos/ionic2');
+
+  fs.readdirSync(rootDir).forEach(file => {
+    var e2eDir = path.join(rootDir, file);
+
+    var stats = fs.statSync(e2eDir);
+    if (!stats.isDirectory()) return;
+
+    fs.readdirSync(e2eDir).forEach(file => {
+      var e2eSubDir = path.join(e2eDir, file);
+
+      var stats = fs.statSync(e2eSubDir);
+      if (!stats.isDirectory()) return;
+
+      fs.readdirSync(e2eSubDir).forEach(file => {
+        var f = path.join(e2eSubDir, file);
+
+        if (file === 'index.html') {
+          var index = path.join(e2eSubDir, 'index.html');
+
+          data[index.replace(rootDir, '')] = true;
+        }
+
+      });
+
+    });
+
+  });
+
+  var data = `var ionic2Data = ${JSON.stringify(data, null, 2)};`
+
+  fs.writeFileSync('demos/compare/ionic2.js', data);
+}
+
+
+buildWebDemos();
+buildIonic2Compare();
