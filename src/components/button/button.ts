23b21724fd9560cb41ba8985b2ca2f1c6cebcc23
Adam Bradley <adambradley25@gmail.com>
2017-03-14 00:11:03 -0500

    fix button props

--- src/components/button/button.ts
+++ src/components/button/button.ts
@@ -1,4 +1,4 @@
-import { IonElement, IonicComponent, h, VNode } from '../../element/ion-element';
+import { IonElement, IonicComponent, h, VNode, VNodeData } from '../../element/ion-element';
 
 
 export class IonButton extends IonElement {
@@ -17,10 +17,42 @@ export class IonButton extends IonElement {
 
 
   render(): VNode {
+    const host = this;
+    const vnodeData: VNodeData = { attrs: {} };
+    const attrs = vnodeData.attrs;
+
+    if (host.large) {
+      attrs['large'] = '';
+    } else if (host.small) {
+      attrs['small'] = '';
+    }
+
+    if (host.outline) {
+      attrs['outline'] = '';
+    } else if (host.clear) {
+      attrs['clear'] = '';
+    } else if (host.solid) {
+      attrs['solid'] = '';
+    }
+
+    if (host.block) {
+      attrs['block'] = '';
+    } else if (host.full) {
+      attrs['full'] = '';
+    }
+
+    if (host.round) {
+      attrs['round'] = '';
+    }
+
+    if (host.strong) {
+      attrs['strong'] = '';
+    }
+
     const buttonInnerTag = 'a.button-inner';
     const hasButtonEffect = true;
 
-    return h('.button', [
+    return h('.button', vnodeData, [
       h(buttonInnerTag, h('slot')),
       (hasButtonEffect) ? h('div.button-effect') : null
     ]);
@@ -30,6 +62,19 @@ export class IonButton extends IonElement {
 
 (<IonicComponent>IonButton).$annotations = {
   tag: 'ion-button',
+  props: {
+    role: {},
+    large: {type: 'boolean'},
+    small: {type: 'boolean'},
+    default: {type: 'boolean'},
+    outline: {type: 'boolean'},
+    clear: {type: 'boolean'},
+    solid: {type: 'boolean'},
+    round: {type: 'boolean'},
+    block: {type: 'boolean'},
+    full: {type: 'boolean'},
+    strong: {type: 'boolean'}
+  },
   preprocessStyles: [
     'button.ios.scss',
     'button.md.scss',
