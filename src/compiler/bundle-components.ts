1efbbc350e706f7fca643cada4f05f9c5a2c5fca
Adam Bradley <adambradley25@gmail.com>
2017-03-17 16:57:46 -0500

    minify

--- src/compiler/bundle-components.ts
+++ src/compiler/bundle-components.ts
@@ -1,13 +1,14 @@
 import { CompilerOptions, CompilerContext } from './interfaces';
+import { writeFile } from './util';
 import * as fs from 'fs-extra'
 import * as path from 'path';
+import * as babel from 'babel-core';
 
 
 export function bundleComponents(opts: CompilerOptions, ctx: CompilerContext) {
   return Promise.all([
     createIonicJs(opts, ctx),
-    createComponentRegistry(opts, ctx),
-    createES5ComponentRegistry(opts, ctx)
+    createComponentJs(opts, ctx)
   ]);
 }
 
@@ -29,35 +30,50 @@ export function createIonicJs(opts: CompilerOptions, ctx: CompilerContext) {
 }
 
 
-function createComponentRegistry(opts: CompilerOptions, ctx: CompilerContext) {
+function createComponentJs(opts: CompilerOptions, ctx: CompilerContext) {
   return new Promise(resolve => {
     const fileName = 'ionic.components.js';
 
     const src = path.join(opts.ionicBundlesDir, fileName);
     const dest = path.join(opts.destDir, fileName);
 
-    fs.copy(src, dest, err => {
-      if (err) {
-        console.log(err);
-      }
-      resolve();
-    });
+    return transpile(src, dest, [
+      'transform-es2015-arrow-functions',
+      'transform-es2015-block-scoped-functions',
+      'transform-es2015-block-scoping',
+      'transform-es2015-destructuring',
+      'transform-es2015-parameters',
+      'transform-es2015-shorthand-properties',
+      'transform-es2015-template-literals'
+    ]);
   });
 }
 
 
-function createES5ComponentRegistry(opts: CompilerOptions, ctx: CompilerContext) {
+function transpile(src: string, dest: string, plugins: string[]) {
   return new Promise(resolve => {
-    const fileName = 'ionic.components.es5.js';
 
-    const src = path.join(opts.ionicBundlesDir, fileName);
-    const dest = path.join(opts.destDir, fileName);
-
-    fs.copy(src, dest, err => {
+    babel.transformFile(src, { plugins: plugins }, (err, transpileResult) => {
       if (err) {
         console.log(err);
       }
-      resolve();
+
+      const minifyResult = babel.transformFromAst(transpileResult.ast, transpileResult.code, {
+        presets: [
+          ['babili', { removeConsole: true, removeDebugger: true } ]
+        ]
+      });
+
+      const destMin = dest.replace('.js', '.min.js');
+
+      Promise.all([
+        writeFile(dest, transpileResult.code),
+        writeFile(destMin, minifyResult.code),
+
+      ]).then(() => {
+        resolve();
+      });
     });
+
   });
 }
