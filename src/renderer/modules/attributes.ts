38897d3134274aa53851f66e1f8be2858339ea59
Adam Bradley <adambradley25@gmail.com>
2017-03-25 21:32:43 -0500

    Performance improvement for attribute patching

--- src/renderer/modules/attributes.ts
+++ src/renderer/modules/attributes.ts
@@ -1,16 +1,17 @@
 import { Module, VNode, VNodeData } from '../../utils/interfaces';
 
 
-const NamespaceURIs = {
-  "xlink": "http://www.w3.org/1999/xlink"
-};
-
 const booleanAttrs = ["allowfullscreen", "async", "autofocus", "autoplay", "checked", "compact", "controls", "declare",
                 "default", "defaultchecked", "defaultmuted", "defaultselected", "defer", "disabled", "draggable",
                 "enabled", "formnovalidate", "hidden", "indeterminate", "inert", "ismap", "itemscope", "loop", "multiple",
                 "muted", "nohref", "noresize", "noshade", "novalidate", "nowrap", "open", "pauseonexit", "readonly",
                 "required", "reversed", "scoped", "seamless", "selected", "sortable", "spellcheck", "translate",
                 "truespeed", "typemustmatch", "visible"];
+
+const xlinkNS = 'http://www.w3.org/1999/xlink';
+const xmlNS = 'http://www.w3.org/XML/1998/namespace';
+const colonChar = 58;
+const xChar = 120;
 
 const booleanAttrsDict: {[attribute: string]: boolean} = Object.create(null);
 for (let i = 0, len = booleanAttrs.length; i < len; i++) {
@@ -20,7 +21,7 @@ for (let i = 0, len = booleanAttrs.length; i < len; i++) {
 function updateAttrs(oldVnode: VNode, vnode: VNode): void {
   var key: string, cur: any, old: any, elm: Element = vnode.elm as Element,
       oldAttrs = (oldVnode.data as VNodeData).attrs,
-      attrs = (vnode.data as VNodeData).attrs, namespaceSplit: Array<string>;
+      attrs = (vnode.data as VNodeData).attrs;
 
   if (!oldAttrs && !attrs) return;
   if (oldAttrs === attrs) return;
@@ -39,9 +40,14 @@ function updateAttrs(oldVnode: VNode, vnode: VNode): void {
           elm.removeAttribute(key);
         }
       } else {
-        namespaceSplit = key.split(":");
-        if (namespaceSplit.length > 1 && NamespaceURIs.hasOwnProperty(namespaceSplit[0])) {
-          elm.setAttributeNS((NamespaceURIs as any)[namespaceSplit[0]], key, cur);
+        if (key.charCodeAt(0) !== xChar) {
+          elm.setAttribute(key, cur);
+        } else if (key.charCodeAt(3) === colonChar) {
+          // Assume xml namespace
+          elm.setAttributeNS(xmlNS, key, cur);
+        } else if (key.charCodeAt(5) === colonChar) {
+          // Assume xlink namespace
+          elm.setAttributeNS(xlinkNS, key, cur);
         } else {
           elm.setAttribute(key, cur);
         }
