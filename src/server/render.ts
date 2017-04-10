a2d16359f1d6fe8758d4bbae8e44c2e12a4d98e5
Adam Bradley <adambradley25@gmail.com>
2017-04-09 22:08:42 -0500

    init renderToString

--- src/server/render.ts
+++ src/server/render.ts
@@ -1,4 +1,4 @@
-import { ComponentMeta, IonicUtils, PlatformApi, RendererApi } from '../util/interfaces';
+import { ComponentMeta, IonicUtils, PlatformApi, RendererApi, VNode } from '../util/interfaces';
 import { generateVNode } from '../element/host';
 import * as parse5 from 'parse5';
 
@@ -21,11 +21,11 @@ export function inspectNode(utils: IonicUtils, plt: PlatformApi, renderer: Rende
     if (cmpMeta) {
       promises.push(upgradeNode(utils, plt, renderer, node, cmpMeta));
     }
+  }
 
-    if (node.childNodes) {
-      for (var i = 0; i < node.childNodes.length; i++) {
-        promises.push(inspectNode(utils, plt, renderer, node.childNodes[i]));
-      }
+  if (node.childNodes) {
+    for (var i = 0; i < node.childNodes.length; i++) {
+      promises.push(inspectNode(utils, plt, renderer, node.childNodes[i]));
     }
   }
 
@@ -34,15 +34,71 @@ export function inspectNode(utils: IonicUtils, plt: PlatformApi, renderer: Rende
 
 
 export function upgradeNode(utils: IonicUtils, plt: PlatformApi, renderer: RendererApi, elm: Element, cmpMeta: ComponentMeta) {
+  console.log(`upgradeNode ${elm.tagName}`)
   const instance = new cmpMeta.componentModule();
 
   const cmpMode = cmpMeta.modes[instance.mode];
   const cmpModeId = `${cmpMeta.tag}.${instance.mode}`;
   const rootElm = plt.$attachShadow(elm, cmpMode, cmpModeId);
 
-  const vnode = generateVNode(utils, rootElm, instance, cmpMeta.hostCss);
+  let vnode = generateVNode(utils, rootElm, instance, cmpMeta.hostCss);
 
-  renderer(elm, vnode);
+  vnode = renderer(elm, vnode);
+
+  const html = renderToString(vnode);
+
+  console.log(html)
 
   return Promise.resolve();
+}
+
+
+function renderToString(vnode: VNode) {
+  if (!vnode.sel && vnode.vtext) {
+    return vnode.vtext;
+  }
+
+  const tagName = getTagName(vnode.sel);
+
+  if (tagName === '!') {
+    return `<!--${vnode.vtext}-->`;
+  }
+
+  vnode.vdata = vnode.vdata || {};
+
+  // open element
+  // open start tag
+  const html = ['<' + tagName];
+
+  const attributes = {};
+
+  Object.keys(attributes).forEach(attrName => {
+    const attrVal = attributes[attrName];
+    html.push(` ${attrName}="${attrVal}"`);
+  });
+
+  // close start tag
+  html.push('>');
+
+  // Ccose tag
+  if (vnode.vtext) {
+    html.push(vnode.vtext);
+
+  } else if (vnode.vchildren) {
+    vnode.vchildren.forEach(child => {
+      html.push(renderToString(<VNode>child));
+    });
+  }
+
+  // close element
+  html.push(`</${tagName}>`);
+
+  return html.join('');
+}
+
+
+function getTagName(selector: string) {
+  selector = selector || '';
+  const parts = selector.split('.');
+  return parts[0] || 'div';
 }
