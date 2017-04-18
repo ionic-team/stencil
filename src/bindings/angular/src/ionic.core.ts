d5df09c5e14a72d72db2333d5f0218d4e5af6cf8
Adam Bradley <adambradley25@gmail.com>
2017-04-18 10:10:34 -0500

    fix import

--- src/bindings/angular/src/ionic.core.ts
+++ src/bindings/angular/src/ionic.core.ts
@@ -1,10 +1,10 @@
-import { Ionic } from '../../../util/interfaces';
+import { IonicGlobal } from '../../../util/interfaces';
 import { PlatformClient } from '../../../client/platform-client';
 import { registerComponents } from '../../../client/registry';
 import { Renderer } from '../../../client/renderer/core';
 
 
-const ionic: Ionic = (<any>window).Ionic = (<any>window).Ionic || {};
+const ionic: IonicGlobal = (<any>window).Ionic = (<any>window).Ionic || {};
 
 // most of the controllers are added to window.Ionic within ionic-angular
 const plt = PlatformClient(window, document, ionic, ionic.staticDir, ionic.domCtrl, ionic.nextTickCtrl);
--- src/bindings/web/src/ionic.core.ts
+++ src/bindings/web/src/ionic.core.ts
@@ -1,13 +1,13 @@
 import { ConfigController } from '../../../client/config-controller';
 import { DomController } from '../../../client/dom-controller';
-import { Ionic } from '../../../util/interfaces';
+import { IonicGlobal } from '../../../util/interfaces';
 import { NextTickController } from '../../../client/next-tick-controller';
 import { PlatformClient } from '../../../client/platform-client';
 import { registerComponents } from '../../../client/registry';
 import { Renderer } from '../../../client/renderer/core';
 
 
-const ionic: Ionic = (<any>window).Ionic = (<any>window).Ionic || {};
+const ionic: IonicGlobal = (<any>window).Ionic = (<any>window).Ionic || {};
 
 const domCtrl = DomController(window);
 
