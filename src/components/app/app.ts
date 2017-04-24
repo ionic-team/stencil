7b84eb72c42457bf23ad00f488746423e25b4651
Adam Bradley <adambradley25@gmail.com>
2017-04-24 00:11:56 -0500

    init app

---
+++ src/components/app/app.ts
@@ -0,0 +1,19 @@
+import { Component } from '../../index';
+
+
+@Component({
+  tag: 'ion-app',
+  styleUrls: {
+    ios: 'app.ios.scss',
+    md: 'app.md.scss',
+    wp: 'app.wp.scss'
+  },
+  shadow: false
+})
+export class App {
+
+  ionViewDidLoad() {
+
+  }
+
+}
