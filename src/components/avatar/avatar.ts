29ad55f0a7d248ba1c34ff590413ba6d76841562
Adam Bradley <adambradley25@gmail.com>
2017-04-29 01:18:22 -0500

    stub avatar/thumbnail

--- src/components/avatar/avatar.ts
+++ src/components/avatar/avatar.ts
@@ -2,6 +2,11 @@ import { Component } from '../index';
 
 
 @Component({
-  tag: 'ion-avatar'
+  tag: 'ion-avatar',
+  styleUrls: {
+    ios: 'avatar.ios.scss',
+    md: 'avatar.md.scss',
+    wp: 'avatar.wp.scss'
+  }
 })
 export class Avatar {}
--- src/components/thumbnail/thumbnail.ts
+++ src/components/thumbnail/thumbnail.ts
@@ -2,6 +2,11 @@ import { Component } from '../index';
 
 
 @Component({
-  tag: 'ion-thumbnail'
+  tag: 'ion-thumbnail',
+  styleUrls: {
+    ios: 'thumbnail.ios.scss',
+    md: 'thumbnail.md.scss',
+    wp: 'thumbnail.wp.scss'
+  }
 })
 export class Thumbnail {}
