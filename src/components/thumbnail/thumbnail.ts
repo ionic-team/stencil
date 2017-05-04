22b2d01809ce8dd090d63e25b75d126872887237
Brandy Carney <brandyscarney@gmail.com>
2017-05-04 15:54:20 -0400

    refactor(thumbnail): remove shadow DOM

--- src/components/thumbnail/thumbnail.ts
+++ src/components/thumbnail/thumbnail.ts
@@ -7,7 +7,8 @@ import { Component, h, Ionic } from '../index';
     ios: 'thumbnail.ios.scss',
     md: 'thumbnail.md.scss',
     wp: 'thumbnail.wp.scss'
-  }
+  },
+  shadow: false
 })
 export class Thumbnail {
   render() {
