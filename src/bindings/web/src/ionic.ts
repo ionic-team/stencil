4b2de3880534cb68e1be873e9bb99717c33e3010
Adam Bradley <adambradley25@gmail.com>
2017-05-24 21:44:34 -0500

    fix(safari): requestIdleCallback shim

--- src/bindings/web/src/ionic.ts
+++ src/bindings/web/src/ionic.ts
@@ -22,7 +22,7 @@ import { IonicGlobal } from '../../../util/interfaces';
   }
 
   var style = document.createElement('style');
-  style.innerHTML = ionic.components.map(c => c[0]).join(',') + '{visibility:hidden}';
+  style.innerHTML = ionic.components.map(function(c) { return c[0]; }).join(',') + '{visibility:hidden}';
   document.head.appendChild(style);
 
   // build up a path for the exact ionic core javascript file this browser needs
@@ -38,32 +38,32 @@ import { IonicGlobal } from '../../../util/interfaces';
     pathItems.push('ce');
   }
 
-  // async load the ionic animation
-  function asyncLoadAnimations() {
-    var s = document.createElement('script');
-    s.src = ionic.staticDir + 'ionic.animation.js';
-    document.head.appendChild(s);
-  }
-
-  if ('requestIdleCallback' in window) {
-    // cool, this browser supports requestIdleCallback (and requestAnimationFrame)
-    // no need to also load the window polyfills/shims
-
-    // using requestIdleCallback, async load the animation core
-    window.requestIdleCallback(asyncLoadAnimations);
-
-  } else {
-    // this browser doesn't have the native requestIdleCallback and/or requestAnimationFrame
-    // load the window polyfills/shims
-    pathItems.push('wn');
-
-    // using setTimeout, async load the animation core
-    setTimeout(asyncLoadAnimations, 500);
+  if (!('requestIdleCallback' in window)) {
+    // darn, this browser doesn't support requestIdleCallback
+    // let's shim it!
+    window.requestIdleCallback = function(cb) {
+      var start = Date.now();
+      return setTimeout(function() {
+        cb({
+          didTimeout: false,
+          timeRemaining: function() {
+            return Math.max(0, 50 - (Date.now() - start));
+          }
+        });
+      }, 1);
+    };
   }
 
   // request the ionic core file this browser needs
   var s = document.createElement('script');
   s.src = ionic.staticDir + 'ionic.' + pathItems.join('.') + '.js';
   document.head.appendChild(s);
+
+  // using requestIdleCallback, async load the animation core
+  window.requestIdleCallback(function() {
+    var s = document.createElement('script');
+    s.src = ionic.staticDir + 'ionic.animation.js';
+    document.head.appendChild(s);
+  });
 
 })(window, document);
