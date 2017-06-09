8742d6797c37a123c7a5c2e3f484309b731ac1bf
Adam Bradley <adambradley25@gmail.com>
2017-06-09 16:17:44 -0500

    feat(skeleton): add ion-skeleton-text

--- scripts/build-web.ts
+++ scripts/build-web.ts
@@ -24,7 +24,7 @@ const BUNDLES = [
   { components: ['ion-card', 'ion-card-content', 'ion-card-header', 'ion-card-title'] },
   { components: ['ion-gesture', 'ion-scroll'], priority: 'low' },
   { components: ['ion-icon'] },
-  { components: ['ion-item', 'ion-item-divider', 'ion-label', 'ion-list', 'ion-list-header'] },
+  { components: ['ion-item', 'ion-item-divider', 'ion-label', 'ion-list', 'ion-list-header', 'ion-skeleton-text'] },
   { components: ['ion-loading', 'ion-loading-controller'] },
   { components: ['ion-menu'], priority: 'low' },
   { components: ['ion-modal', 'ion-modal-controller'] },
