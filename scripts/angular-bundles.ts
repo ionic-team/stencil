6efaa09cc9b32ef58f325fb07085eb5a7b9d586c
Adam Bradley <adambradley25@gmail.com>
2017-06-16 14:45:31 -0500

    chore(): bundle button, buttons and icon together

--- scripts/angular-bundles.ts
+++ scripts/angular-bundles.ts
@@ -2,10 +2,9 @@
 export const BUNDLES = [
   { components: ['ion-avatar'] },
   { components: ['ion-badge'] },
-  { components: ['ion-button'] },
+  { components: ['ion-button', 'ion-buttons', 'ion-icon'] },
   { components: ['ion-card', 'ion-card-content', 'ion-card-header', 'ion-card-title'] },
   { components: ['ion-gesture'], priority: 'low' },
-  { components: ['ion-icon'] },
   { components: ['ion-slides', 'ion-slide'] },
   { components: ['ion-toggle'] },
 ];
