9ef652ff32a2aa482380e8c744c4aac361f0d392
Max Lynch <max@drifty.com>
2017-06-29 21:49:39 -0500

    Moving over comments and a few features

--- src/components/tabs/tab.tsx
+++ src/components/tabs/tab.tsx
@@ -9,21 +9,74 @@ import { VNodeData } from '../../util/interfaces';
   }
 })
 export class Tab {
+  /**
+   * @prop {Page} Set the root component for this tab.
+   */
   @Prop() root: string;
 
+  /**
+   * @prop {object} Any nav-params to pass to the root componentof this tab.
+   */
+  @Prop() rootParams: any;
+
+  /**
+   * @prop {boolean} If true, the tab is selected
+   */
+  @Prop() isSelected: Boolean = false;
+
+
+  /**
+   * @prop {string} The title of the tab button.
+   */
   @Prop() tabTitle: string;
+
+  /**
+   * @prop {string} The icon for the tab button.
+   */
   @Prop() tabIcon: string;
+
+  /**
+   * @prop {string} The badge for the tab button.
+   */
   @Prop() tabBadge: string;
+
+  /**
+   * @prop {string} The badge color for the tab button.
+   */
   @Prop() tabBadgeStyle: string;
 
-  @Prop() onSelected: Function;
+  /**
+   * @prop {boolean} If true, enable the tab. If false,
+   * the user cannot interact with this element.
+   * Default: `true`.
+   */
+  @Prop() enabled: boolean = true;
 
-  @Prop() isSelected: Boolean = false;
+  /**
+   * @prop {boolean} If true, the tab button is visible within the
+   * tabbar. Default: `true`.
+   */
+  @Prop() shown: boolean = true;
+
+  /**
+   * @prop {boolean} If true, hide the tabs on child pages.
+   */
+  @Prop() tabsHideOnSubPages: boolean = false;
+
+  /**
+   * @prop {Tab} Emitted when the current tab is selected.
+   */
+  @Prop() onSelected: Function;
 
   hostData(): VNodeData {
     return {
       style: {
         display: !this.isSelected && 'none' || ''
+      },
+      attrs: {
+        'role': 'tabpanel'
+        //'id': _tabId,
+        //aria-labelledby: _btnId
       },
       class: {
       }
@@ -51,6 +104,9 @@ export class Tab {
 
   render() {
     const RootComponent = this.root;
-    return <RootComponent />
+    return [
+      <RootComponent />,
+      <div class="nav-decor"></div>
+    ]
   }
 }
--- src/components/tabs/tabs.tsx
+++ src/components/tabs/tabs.tsx
@@ -1,6 +1,137 @@
 import { Component, h, Listen } from '../index';
 
-
+/**
+ * @name Tabs
+ * @description
+ * Tabs make it easy to navigate between different pages or functional
+ * aspects of an app. The Tabs component, written as `<ion-tabs>`, is
+ * a container of individual [Tab](../Tab/) components. Each individual `ion-tab`
+ * is a declarative component for a [NavController](../../../navigation/NavController/)
+ *
+ * For more information on using nav controllers like Tab or [Nav](../../nav/Nav/),
+ * take a look at the [NavController API Docs](../../../navigation/NavController/).
+ *
+ * ### Placement
+ *
+ * The position of the tabs relative to the content varies based on
+ * the mode. The tabs are placed at the bottom of the screen
+ * for iOS and Android, and at the top for Windows by default. The position can
+ * be configured using the `tabsPlacement` attribute on the `<ion-tabs>` component,
+ * or in an app's [config](../../config/Config/).
+ * See the [Input Properties](#input-properties) below for the available
+ * values of `tabsPlacement`.
+ *
+ * ### Layout
+ *
+ * The layout for all of the tabs can be defined using the `tabsLayout`
+ * property. If the individual tab has a title and icon, the icons will
+ * show on top of the title by default. All tabs can be changed by setting
+ * the value of `tabsLayout` on the `<ion-tabs>` element, or in your
+ * app's [config](../../config/Config/). For example, this is useful if
+ * you want to show tabs with a title only on Android, but show icons
+ * and a title for iOS. See the [Input Properties](#input-properties)
+ * below for the available values of `tabsLayout`.
+ *
+ * ### Selecting a Tab
+ *
+ * There are different ways you can select a specific tab from the tabs
+ * component. You can use the `selectedIndex` property to set the index
+ * on the `<ion-tabs>` element, or you can call `select()` from the `Tabs`
+ * instance after creation. See [usage](#usage) below for more information.
+ *
+ * @usage
+ *
+ * You can add a basic tabs template to a `@Component` using the following
+ * template:
+ *
+ * ```html
+ * <ion-tabs>
+ *   <ion-tab [root]="tab1Root"></ion-tab>
+ *   <ion-tab [root]="tab2Root"></ion-tab>
+ *   <ion-tab [root]="tab3Root"></ion-tab>
+ * </ion-tabs>
+ * ```
+ *
+ * Where `tab1Root`, `tab2Root`, and `tab3Root` are each a page:
+ *
+ *```ts
+ * @Component({
+ *   templateUrl: 'build/pages/tabs/tabs.html'
+ * })
+ * export class TabsPage {
+ *   // this tells the tabs component which Pages
+ *   // should be each tab's root Page
+ *   tab1Root = Page1;
+ *   tab2Root = Page2;
+ *   tab3Root = Page3;
+ *
+ *   constructor() {
+ *
+ *   }
+ * }
+ *```
+ *
+ * By default, the first tab will be selected upon navigation to the
+ * Tabs page. We can change the selected tab by using `selectedIndex`
+ * on the `<ion-tabs>` element:
+ *
+ * ```html
+ * <ion-tabs selectedIndex="2">
+ *   <ion-tab [root]="tab1Root"></ion-tab>
+ *   <ion-tab [root]="tab2Root"></ion-tab>
+ *   <ion-tab [root]="tab3Root"></ion-tab>
+ * </ion-tabs>
+ * ```
+ *
+ * Since the index starts at `0`, this will select the 3rd tab which has
+ * root set to `tab3Root`. If you wanted to change it dynamically from
+ * your class, you could use [property binding](https://angular.io/docs/ts/latest/guide/template-syntax.html#!#property-binding).
+ *
+ * Alternatively, you can grab the `Tabs` instance and call the `select()`
+ * method. This requires the `<ion-tabs>` element to have an `id`. For
+ * example, set the value of `id` to `myTabs`:
+ *
+ * ```html
+ * <ion-tabs #myTabs>
+ *   <ion-tab [root]="tab1Root"></ion-tab>
+ *   <ion-tab [root]="tab2Root"></ion-tab>
+ *   <ion-tab [root]="tab3Root"></ion-tab>
+ * </ion-tabs>
+ * ```
+ *
+ * Then in your class you can grab the `Tabs` instance and call `select()`,
+ * passing the index of the tab as the argument. Here we're grabbing the tabs
+ * by using ViewChild.
+ *
+ *```ts
+ * export class TabsPage {
+ *
+ * @ViewChild('myTabs') tabRef: Tabs;
+ *
+ * ionViewDidEnter() {
+ *   this.tabRef.select(2);
+ *  }
+ *
+ * }
+ *```
+ *
+ * You can also switch tabs from a child component by calling `select()` on the
+ * parent view using the `NavController` instance. For example, assuming you have
+ * a `TabsPage` component, you could call the following from any of the child
+ * components to switch to `TabsRoot3`:
+ *
+ *```ts
+ * switchTabs() {
+ *   this.navCtrl.parent.select(2);
+ * }
+ *```
+ * @demo /docs/demos/src/tabs/
+ *
+ * @see {@link /docs/components#tabs Tabs Component Docs}
+ * @see {@link ../Tab Tab API Docs}
+ * @see {@link ../../config/Config Config API Docs}
+ *
+ */
 @Component({
   tag: 'ion-tabs',
   styleUrls: {
@@ -13,12 +144,34 @@ import { Component, h, Listen } from '../index';
   }
 })
 export class Tabs {
-  @State() selectedTab: Number = 0;
-
+  // Current list of tabs
   @State() tabs: [Tab] = []
 
-  @Prop() tabsPlacement: string = 'bottom';
+  /**
+   * @input {number} The default selected tab index when first loaded. If a selected index isn't provided then it will use `0`, the first tab.
+   */
+  @Prop() selectedIndex: Number = 0;
+
+  /**
+   * @input {string} Set the tabbar layout: `icon-top`, `icon-start`, `icon-end`, `icon-bottom`, `icon-hide`, `title-hide`.
+   */
   @Prop() tabsLayout: string = 'icon-top'
+
+  /**
+   * @input {string} Set position of the tabbar: `top`, `bottom`.
+   */
+  @Prop() tabsPlacement: string = 'bottom';
+
+  /**
+   * @input {boolean} If true, show the tab highlight bar under the selected tab.
+   */
+  @Prop() tabsHighlight: boolean = false;
+
+
+  /**
+   * @output {any} Emitted when the tab changes.
+   */
+  @Prop() ionChange: Function;
 
   @Listen('ionTabDidLoad')
   tabDidLoad(ev) {
@@ -31,10 +184,9 @@ export class Tabs {
     this.tabs = this.tabs.filter(t => t !== ev.detail.tab)
   }
 
-
   handleTabButtonClick(tab) {
-    console.log('Handling tab button click')
     tab.onSelected()
+    this.ionChange && this.ionChange(tab);
   }
 
   render() {
