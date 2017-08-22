05d0811a65cbcf88868d7f6e7ef9d9d686c2658b
Ken Sodemann <ken@ionic.io>
2017-08-22 18:19:00 -0500

    test(compiler): add app-core test and strengthen app-loader test

---
+++ src/compiler/app/test/app-core.spec.ts
@@ -0,0 +1,150 @@
+import { BuildConfig } from '../../../util/interfaces';
+import { CORE_NAME } from '../../../util/constants';
+import { generatePreamble } from '../../util';
+import { mockStencilSystem } from '../../../test';
+
+import * as core from '../app-core';
+
+describe('app-core', () => {
+  let config: BuildConfig;
+
+  beforeEach(() => {
+    config = {
+      sys: mockStencilSystem()
+    };
+  });
+
+  describe('generateCore', () => {
+    let mockGetClientCoreFile: jest.Mock<Promise<string>>;
+    beforeEach(() => {
+      config.namespace = 'WillyWendLeSWeTWasaBi';
+      config.publicPath = 'Projects/Ionic/Stencil';
+      mockGetClientCoreFile = jest.fn();
+      mockGetClientCoreFile.mockReturnValue(Promise.resolve(''));
+      config.sys.getClientCoreFile = mockGetClientCoreFile;
+    });
+
+    it('uses the core file name', () => {
+      core.generateCore(config, ['']);
+      expect(mockGetClientCoreFile.mock.calls.length).toEqual(1);
+      expect(mockGetClientCoreFile.mock.calls[0][0]).toEqual({ staticName: `${CORE_NAME}.js` });
+    });
+
+    it('uses the core dev file name', () => {
+      config.devMode = true;
+      core.generateCore(config, ['']);
+      expect(mockGetClientCoreFile.mock.calls.length).toEqual(1);
+      expect(mockGetClientCoreFile.mock.calls[0][0]).toEqual({ staticName: `${CORE_NAME}.dev.js` });
+    });
+
+    it('genertes the full wrapped content', async () => {
+      const preamble = generatePreamble(config).trim();
+      mockGetClientCoreFile.mockReturnValue(Promise.resolve('I am core'));
+      const res = await core.generateCore(config, ['global line 1', 'global line 2']);
+      const lines = res.split('\n');
+      expect(lines[0]).toEqual(preamble);
+      expect(lines[1]).toEqual(`(function(Context,appNamespace,publicPath){"use strict";`);
+      expect(lines[2]).toEqual('global line 1');
+      expect(lines[3]).toEqual('global line 2');
+      expect(lines[4]).toEqual('I am core');
+      expect(lines[5]).toEqual(`})({},"${config.namespace}","Projects/Ionic/Stencil/willywendleswetwasabi/");`);
+    });
+  });
+
+  describe('generateCoreES5WithPolyfills', () => {
+    let mockGetClientCoreFile: jest.Mock<Promise<string>>;
+    beforeEach(() => {
+      config.namespace = 'WillyWendLeSWeTWasaBi';
+      config.publicPath = 'Projects/Ionic/Stencil';
+      mockGetClientCoreFile = jest.fn();
+      mockGetClientCoreFile.mockReturnValue(Promise.resolve(''));
+      config.sys.getClientCoreFile = mockGetClientCoreFile;
+    });
+
+    it('includes the required polyfills', () => {
+      core.generateCoreES5WithPolyfills(config, ['']);
+      expect(mockGetClientCoreFile.mock.calls.length).toEqual(4);
+      expect(mockGetClientCoreFile.mock.calls[0][0]).toEqual({ staticName: 'polyfills/document-register-element.js' });
+      expect(mockGetClientCoreFile.mock.calls[1][0]).toEqual({ staticName: 'polyfills/fetch.js' });
+      expect(mockGetClientCoreFile.mock.calls[2][0]).toEqual({ staticName: 'polyfills/performance-now.js' });
+    });
+
+    it('uses the core file name', () => {
+      core.generateCoreES5WithPolyfills(config, ['']);
+      const lastCall = mockGetClientCoreFile.mock.calls.length - 1;
+      expect(mockGetClientCoreFile.mock.calls[lastCall][0]).toEqual({ staticName: `${CORE_NAME}.es5.js` });
+    });
+
+    it('uses the core dev file name', () => {
+      config.devMode = true;
+      core.generateCoreES5WithPolyfills(config, ['']);
+      const lastCall = mockGetClientCoreFile.mock.calls.length - 1;
+      expect(mockGetClientCoreFile.mock.calls[lastCall][0]).toEqual({ staticName: `${CORE_NAME}.es5.dev.js` });
+    });
+
+    it('genertes the full wrapped content', async () => {
+      const preamble = generatePreamble(config).trim();
+      mockGetClientCoreFile.mockReturnValueOnce(Promise.resolve('I am document register element'));
+      mockGetClientCoreFile.mockReturnValueOnce(Promise.resolve('I am fetch'));
+      mockGetClientCoreFile.mockReturnValueOnce(Promise.resolve('I am performance now'));
+      mockGetClientCoreFile.mockReturnValueOnce(Promise.resolve('I am core'));
+      const res = await core.generateCoreES5WithPolyfills(config, ['global line 1', 'global line 2']);
+      const lines = res.split('\n');
+      expect(lines[0]).toEqual('I am document register element');
+      expect(lines[1]).toEqual('I am fetch');
+      expect(lines[2]).toEqual('I am performance now');
+      expect(lines[3]).toEqual(preamble);
+      expect(lines[4]).toEqual(`(function(Context,appNamespace,publicPath){"use strict";`);
+      expect(lines[5]).toEqual('global line 1');
+      expect(lines[6]).toEqual('global line 2');
+      expect(lines[7]).toEqual('I am core');
+      expect(lines[8]).toEqual(`})({},"${config.namespace}","Projects/Ionic/Stencil/willywendleswetwasabi/");`);
+    });
+  });
+
+  describe('getAppFileName', () => {
+    it('returns the lower-cased namespace', () => {
+      config.namespace = 'BarnAcleBobSBigBoaTs';
+      expect(core.getAppFileName(config)).toEqual('barnaclebobsbigboats');
+    });
+  });
+
+  describe('getAppPublicPath', () => {
+    it('concatinates public path and namespace', () => {
+      config.namespace = 'WillyWendLeSWeTWasaBi';
+      config.publicPath = 'Projects/Ionic/Stencil';
+      expect(core.getAppPublicPath(config)).toEqual('Projects/Ionic/Stencil/willywendleswetwasabi/');
+    });
+
+    it('handles windows paths', () => {
+      config.namespace = 'WillyWendLeSWeTWasaBi';
+      config.publicPath = 'Projects\\Ionic\\Stencil';
+      expect(core.getAppPublicPath(config)).toEqual('Projects/Ionic/Stencil/willywendleswetwasabi/');
+    });
+  });
+
+  describe('wrapCoreJs', () => {
+    beforeEach(() => {
+      config.namespace = 'WillyWendLeSWeTWasaBi';
+      config.publicPath = 'Projects\\Ionic\\Stencil';
+    });
+
+    it('starts with the preamble', () => {
+      const preamble = generatePreamble(config).trim();
+      const lines = core.wrapCoreJs(config, '').split('\n');
+      expect(lines[0]).toEqual(preamble);
+    });
+
+    it('wraps the JS content in an IFEE', () => {
+      const lines = core.wrapCoreJs(config, 'this is JavaScript code, really it is').split('\n');
+      expect(lines[1]).toEqual(`(function(Context,appNamespace,publicPath){"use strict";`);
+      expect(lines[2]).toEqual('this is JavaScript code, really it is');
+      expect(lines[3]).toEqual(`})({},"${config.namespace}","Projects/Ionic/Stencil/willywendleswetwasabi/");`);
+    });
+
+    it('trims the JS content', () => {
+      const lines = core.wrapCoreJs(config, '  this is JavaScript code, really it is     ').split('\n');
+      expect(lines[2]).toEqual('this is JavaScript code, really it is');
+    });
+  });
+});
---
+++ src/compiler/app/test/app-loader.spec.ts
@@ -0,0 +1,183 @@
+import { generateLoader, injectAppIntoLoader } from '../app-loader';
+import { getAppPublicPath } from '../app-core';
+import { BuildConfig, LoadComponentRegistry } from '../../../util/interfaces';
+import { mockLogger, mockStencilSystem } from '../../../test';
+import { generatePreamble } from '../../util';
+
+
+describe('build-project-files', () => {
+  let mockStencilContent: string;
+  let config: BuildConfig;
+
+  beforeEach(() => {
+    mockStencilContent = `('__STENCIL__APP__')`;
+    config = {
+      logger: mockLogger(),
+      sys: mockStencilSystem()
+    };
+  });
+
+  describe('inject project', () => {
+    let mockMinify: jest.Mock<any>;
+    beforeEach(() => {
+      mockMinify = jest.fn();
+      mockMinify.mockReturnValue({ diagnostics: [] });  // NOTE: bad idea - typedef has this as optional, but not optional in the code under test...
+      config.sys.minifyJs = mockMinify;
+    });
+
+    it('should set the loader arguments', () => {
+      const projectLoader = callInjectAppIntoLoader({ componentRegistry: [['my-app', 'MyApp.Module', { Mode1: 'something', Mode2: 'Something Else' }, [], [], 42, 73]] });
+      expect(projectLoader).toBe(`("MyApp","build/myapp/myapp.core.js","build/myapp/myapp.core.pf.js",[["my-app","MyApp.Module",{"Mode1":"something","Mode2":"Something Else"},[],[],42,73]])`);
+    });
+
+    it('only replaces the magic string', () => {
+      mockStencilContent = `(This is bogus text'__STENCIL__APP__'yeah, me too)`;
+      const projectLoader = callInjectAppIntoLoader();
+      expect(projectLoader).toBe(`(This is bogus text"MyApp","build/myapp/myapp.core.js","build/myapp/myapp.core.pf.js",[]yeah, me too)`);
+    });
+
+    describe('with minifyJs true', () => {
+      beforeEach(() => {
+        config.minifyJs = true;
+      });
+
+      it('calls the minify routine', () => {
+        callInjectAppIntoLoader();
+        expect(mockMinify.mock.calls.length).toEqual(1);
+        expect(mockMinify.mock.calls[0][0]).toEqual(`("MyApp","build/myapp/myapp.core.js","build/myapp/myapp.core.pf.js",[])`);
+      });
+
+      it('returns minified output', () => {
+        mockMinify.mockReturnValue({
+          diagnostics: [],
+          output: 'a'
+        });
+        const projectLoader = callInjectAppIntoLoader();
+        expect(projectLoader).toEqual('a');
+      });
+
+      describe('with diagnostic messages', () => {
+        it('logs the messages', () => {
+          const errorMock = jest.fn();
+          const warnMock = jest.fn();
+          config.logger.error = errorMock;
+          config.logger.warn = warnMock;
+          mockMinify.mockReturnValue({
+            diagnostics: [{
+              level: 'error',
+              messageText: 'no workie workie'
+            }, {
+              level: 'warn',
+              messageText: 'meh'
+            }, {
+              level: 'error',
+              messageText: 'nope'
+            }],
+            output: 'b'
+          });
+          callInjectAppIntoLoader();
+          expect(errorMock.mock.calls.length).toEqual(2);
+          expect(warnMock.mock.calls.length).toEqual(1);
+          expect(errorMock.mock.calls[0][0]).toEqual('no workie workie');
+          expect(errorMock.mock.calls[1][0]).toEqual('nope');
+          expect(warnMock.mock.calls[0][0]).toEqual('meh');
+        });
+
+        it('returns the non-minified data', () => {
+          mockMinify.mockReturnValue({
+            diagnostics: [{
+              level: 'error',
+              messageText: 'no workie workie'
+            }],
+            output: 'b'
+          });
+          const projectLoader = callInjectAppIntoLoader();
+          expect(projectLoader).toBe(`("MyApp","build/myapp/myapp.core.js","build/myapp/myapp.core.pf.js",[])`);
+        });
+      });
+    });
+
+    describe('with minifyJs falsey', () => {
+      it('does not minify', () => {
+        callInjectAppIntoLoader();
+        config.minifyJs = false;
+        callInjectAppIntoLoader();
+        expect(mockMinify.mock.calls.length).toEqual(0);
+      });
+    });
+
+  });
+
+  describe('generate loader', () => {
+    let mockGetClientCoreFile: jest.Mock<Promise<string>>;
+    beforeEach(() => {
+      mockGetClientCoreFile = jest.fn();
+      mockGetClientCoreFile.mockReturnValue(Promise.resolve(''));
+      config.sys.getClientCoreFile = mockGetClientCoreFile;
+    });
+
+    it('gets the client core file', () => {
+      callGenerateLoader();
+      expect(mockGetClientCoreFile.mock.calls.length).toEqual(1);
+      expect(mockGetClientCoreFile.mock.calls[0][0]).toEqual({ staticName: 'loader.js' });
+    });
+
+    it('uses the dev loader if devMode', () => {
+      config.devMode = true;
+      callGenerateLoader();
+      expect(mockGetClientCoreFile.mock.calls[0][0]).toEqual({ staticName: 'loader.dev.js' });
+    });
+
+    it('includes the generted banner', async () => {
+      const preamble = generatePreamble(config); // lack of DI makes this harder to test, so will just pre-calc
+      const res = await callGenerateLoader();
+      expect(res).toEqual(preamble);
+    });
+
+    it('includes the injected app', async () => {
+      mockGetClientCoreFile.mockReturnValue(Promise.resolve(`pretend i am code ('__STENCIL__APP__') yeah me too`));
+      const res = await callGenerateLoader();
+      let lines = res.split('\n');
+      expect(lines[1]).toEqual(`pretend i am code ("MyApp","build/myapp/myapp.core.js","build/myapp/myapp.core.pf.js",[]) yeah me too`);
+    });
+  });
+
+  function callInjectAppIntoLoader(params?: {
+    namespace?: string,
+    publicPath?: string,
+    appCoreFileName?: string,
+    appCorePolyfillFileName?: string,
+    componentRegistry?: Array<LoadComponentRegistry>
+  }): string {
+    let p = params || {};
+    config.namespace = p.namespace || 'MyApp';
+    config.publicPath = p.publicPath || 'build/';
+    return injectAppIntoLoader(
+      config,
+      p.appCoreFileName || 'myapp.core.js',
+      p.appCorePolyfillFileName || 'myapp.core.pf.js',
+      getAppPublicPath(config),
+      p.componentRegistry || [],
+      mockStencilContent
+    );
+  }
+
+  function callGenerateLoader(params?: {
+    namespace?: string,
+    publicPath?: string,
+    appCoreFileName?: string,
+    appCorePolyfillFileName?: string,
+    componentRegistry?: Array<LoadComponentRegistry>
+  }): Promise<string> {
+    let p = params || {};
+    config.namespace = p.namespace || 'MyApp';
+    config.publicPath = p.publicPath || 'build/';
+    return generateLoader(
+      config,
+      p.appCoreFileName || 'myapp.core.js',
+      p.appCorePolyfillFileName || 'myapp.core.pf.js',
+      p.componentRegistry || []
+    );
+  }
+
+});
