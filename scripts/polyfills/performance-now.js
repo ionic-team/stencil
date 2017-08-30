7250f527dc73e0465ac1fbf7f4624c670bff617d
Adam Bradley <adambradley25@gmail.com>
2017-08-30 01:56:13 -0500

    chore(Date.now): add Date.now polyfill

--- scripts/polyfills/performance-now.js
+++ scripts/polyfills/performance-now.js
@@ -1,4 +1,4 @@
 /*! window.performance.now
 http://opensource.org/licenses/MIT
 Copyright Paul Irish 2015 */
-(function(a){0=="performance"in a&&(a.performance={});if(0=="now"in a.performance){var b=Date.now();performance.timing&&performance.timing.navigationStart&&(b=performance.timing.navigationStart);a.performance.now=function(){return Date.now()-b}}})(window);
+!function(){if("performance"in window==0&&(window.performance={}),Date.now=Date.now||function(){return(new Date).getTime()},"now"in window.performance==0){var n=Date.now();performance.timing&&performance.timing.navigationStart&&(n=performance.timing.navigationStart),window.performance.now=function(){return Date.now()-n}}}();
