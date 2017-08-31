5fb0d8d0d7aa2d254ea5dca95fbfff4bf689f2ee
Ken Sodemann <ken@ionic.io>
2017-08-31 17:03:48 -0500

    feat(testing): layout the basic testing API

---
+++ src/core/testing/util.ts
@@ -0,0 +1,60 @@
+import { initHostConstructor } from '../../core/instance/init';
+import { ComponentMeta, HostElement, PlatformApi } from '../../util/interfaces';
+import { MockedPlatform, mockConnect, mockDefine, mockPlatform } from '../../test';
+
+export function register(components?: Array<ComponentMeta>): PlatformApi {
+  const platform = mockPlatform();
+
+  if (components) {
+    components.forEach(c => mockDefine(platform, c));
+  }
+
+  return platform as PlatformApi;
+}
+
+export function render(platform: PlatformApi, html: string): Promise<HostElement> {
+  const node = mockConnect(platform as MockedPlatform, html);
+  return waitForLoad(platform, node);
+}
+
+function waitForLoad(platform: PlatformApi, rootNode: any): Promise<HostElement> {
+  return new Promise((resolve: (elm: HostElement) => void) => {
+    (<MockedPlatform>platform).$flushQueue(() => {
+      // flush to read attribute mode on host elment
+      (<MockedPlatform>platform).$flushLoadBundle(() => {
+        // flush to load component mode data
+        (<MockedPlatform>platform).$flushQueue(() => {
+          // flush to do the update
+          connectComponents(platform, rootNode);
+          resolve(rootNode.childNodes[0]);
+        });
+      });
+    });
+
+  }).catch(err => {
+    console.error('waitForLoad', err);
+    return null;
+  });
+}
+
+// Copied verbatim from test/inject.js, more of that should be moved here and cleaned up as needed or removed
+// in a couple of iterations of:
+//   1 -
+function connectComponents(platform: PlatformApi, node: HostElement) {
+  if (!node) return;
+
+  if (node.tagName) {
+    if (!node._hasConnected) {
+      const cmpMeta = platform.getComponentMeta(node);
+      if (cmpMeta) {
+        initHostConstructor(platform, node);
+        (<HostElement>node).connectedCallback();
+      }
+    }
+  }
+  if (node.childNodes) {
+    for (var i = 0; i < node.childNodes.length; i++) {
+      connectComponents(platform, <HostElement>node.childNodes[i]);
+    }
+  }
+}
