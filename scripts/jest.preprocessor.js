4c01fe02692169d91b5aad5e4c9a5260c9b476a2
vvakame <vvakame+dev@gmail.com>
2017-09-10 17:53:47 +0900

    refactor(testing): use typescript hosted tsconfig.json parser

--- scripts/jest.preprocessor.js
+++ scripts/jest.preprocessor.js
@@ -1,15 +1,18 @@
-const tsc = require('typescript');
-const tsConfig = require('../src/tsconfig.json');
+const fs = require('fs');
+const path = require('path');
+
+const ts = require('typescript');
+const tsConfig = parseTsConfig(path.resolve(__dirname, '../src/tsconfig.json'));
 
 // force the output to use commonjs modules required by jest
-tsConfig.compilerOptions.module = 'commonjs';
+tsConfig.options.module = 'commonjs';
 
 module.exports = {
   process(src, path) {
     if (path.endsWith('.ts')) {
-      return tsc.transpile(
+      return ts.transpile(
         src,
-        tsConfig.compilerOptions,
+        tsConfig.options,
         path,
         []
       );
@@ -17,3 +20,24 @@ module.exports = {
     return src;
   },
 };
+
+function parseTsConfig(configPath) {
+  const jsonText = fs.readFileSync(configPath, "utf-8");
+  const result = ts.parseConfigFileTextToJson(configPath, jsonText);
+  if (result.error) {
+      throw new Error("JSON parse error");
+  }
+
+  const host = {
+    useCaseSensitiveFileNames: ts.sys.useCaseSensitiveFileNames,
+    readDirectory: ts.sys.readDirectory,
+    fileExists: path => fs.existsSync(path),
+    readFile: path => fs.readFileSync(path, "utf-8"),
+  };
+  const parsed = ts.parseJsonConfigFileContent(result.config, host, path.dirname(configPath));
+  if (parsed.errors && parsed.errors.length !== 0) {
+      throw new Error(parsed.errors.map(e => e.messageText).join("\n"));
+  }
+
+  return parsed;
+}
