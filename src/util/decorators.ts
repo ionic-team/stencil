35bc852b77bb2c44322ddab26908f621687e7d1a
Adam Bradley <adambradley25@gmail.com>
2017-09-26 18:36:37 -0500

    chore(decorators): move decorators to util file

---
+++ src/util/decorators.ts
@@ -0,0 +1,29 @@
+import {
+  ComponentDecorator,
+  ElementDecorator,
+  EventDecorator,
+  ListenDecorator,
+  MethodDecorator,
+  PropDecorator,
+  PropChangeDecorator,
+  StateDecorator,
+} from './interfaces';
+
+
+export declare const Component: ComponentDecorator;
+
+export declare const Element: ElementDecorator;
+
+export declare const Event: EventDecorator;
+
+export declare const Listen: ListenDecorator;
+
+export declare const Method: MethodDecorator;
+
+export declare const Prop: PropDecorator;
+
+export declare const PropWillChange: PropChangeDecorator;
+
+export declare const PropDidChange: PropChangeDecorator;
+
+export declare const State: StateDecorator;
