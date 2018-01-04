56e8f519a93ea2791518407fccf70cf6afbb3520
Josh Thomas <jthoms1@gmail.com>
2018-01-03 21:29:14 -0600

    chore(): add more tests for graph splitting.

--- src/compiler/bundle/test/graph.spec.ts
+++ src/compiler/bundle/test/graph.spec.ts
@@ -14,32 +14,22 @@ describe('chunk-plugin', () => {
     expect([...graphedCommons]).toEqual([
       {
         'id': 'a.js',
-        'srcs': [
-          'a.js',
-          '1.js'
-        ],
+        'srcs': ['a.js', '1.js'],
         'entrypoint': true
       },
       {
         'id': 'b.js',
-        'srcs': [
-          'b.js'
-        ],
+        'srcs': ['b.js'],
         'entrypoint': true
       },
       {
         'id': '2.js',
-        'srcs': [
-          '2.js'
-        ],
+        'srcs': ['2.js'],
         'entrypoint': false
       },
       {
         'id': 'c.js',
-        'srcs': [
-          'c.js',
-          '4.js'
-        ],
+        'srcs': ['c.js', '4.js'],
         'entrypoint': true
       }
     ]);
@@ -58,40 +48,73 @@ describe('chunk-plugin', () => {
     expect([...graphedCommons]).toEqual([
       {
         'id': 'a.js',
-        'srcs': [
-          'a.js',
-          '1.js'
-        ],
+        'srcs': ['a.js', '1.js'],
         'entrypoint': true
       },
       {
         'id': 'b.js',
-        'srcs': [
-          'b.js'
-        ],
+        'srcs': ['b.js'],
         'entrypoint': true
       },
       {
         'id': '2.js',
-        'srcs': [
-          '2.js'
-        ],
+        'srcs': ['2.js'],
         'entrypoint': false
       },
       {
         'id': '3.js',
-        'srcs': [
-          '3.js'
-        ],
+        'srcs': ['3.js'],
         'entrypoint': false
       },
       {
         'id': 'c.js',
-        'srcs': [
-          'c.js',
-          '4.js'
-        ],
+        'srcs': ['c.js', '4.js'],
         'entrypoint': true
+      }
+    ]);
+  });
+  it('if a module that is not an entry point relies on a file then all associated should be seperated', () => {
+    const modules = new Map([
+      ['a.js',
+        [ '1.js' ]],
+      ['b.js',
+        [ '2.js', '3.js' ]],
+      ['c.js',
+        [ '2.js', '3.js', '4.js' ]],
+      ['1.js',
+        [ '4.js' ]]
+    ]);
+    const graphedCommons = processGraph(modules, ['a.js', 'b.js', 'c.js']);
+    expect([...graphedCommons]).toEqual([
+      {
+        'entrypoint': true,
+        'id': 'a.js',
+        'srcs': ['a.js', '1.js']
+      },
+      {
+        'entrypoint': false,
+        'id': '4.js',
+        'srcs': ['4.js']
+      },
+      {
+        'entrypoint': true,
+        'id': 'b.js',
+        'srcs': ['b.js']
+      },
+      {
+        'entrypoint': false,
+        'id': '2.js',
+        'srcs': ['2.js']
+      },
+      {
+        'entrypoint': false,
+        'id': '3.js',
+        'srcs': ['3.js']
+      },
+      {
+        'entrypoint': true,
+        'id': 'c.js',
+        'srcs': ['c.js']
       }
     ]);
   });
