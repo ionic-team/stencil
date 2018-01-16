0fa0eaa363c1d61aab296c6b681db96f222a005c
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2018-01-16 16:40:54 +0100

    perf(sw): serviceWorker script is minified

--- src/compiler/service-worker/inject-sw-script.ts
+++ src/compiler/service-worker/inject-sw-script.ts
@@ -1,4 +1,5 @@
 import { BuildConfig, ServiceWorkerConfig } from '../../util/interfaces';
+import { minifyJs } from '../util';
 
 
 export function injectRegisterServiceWorker(config: BuildConfig, swConfig: ServiceWorkerConfig, indexHtml: string) {
@@ -8,9 +9,19 @@ export function injectRegisterServiceWorker(config: BuildConfig, swConfig: Servi
   if (swUrl.charAt(0) !== '/') {
     swUrl = '/' + swUrl;
   }
-
   if (match) {
-    indexHtml = indexHtml.replace(match[0], `${getRegisterSwScript(swUrl)}\n${match[0]}`);
+    let serviceWorker = getRegisterSwScript(swUrl);
+    if (config.minifyJs) {
+      const minifyResults = minifyJs(config, serviceWorker, 'es5', false);
+      minifyResults.diagnostics.forEach(d => {
+        (config.logger as any)[d.level](d.messageText);
+      });
+
+      if (!minifyResults.diagnostics.length) {
+        serviceWorker = minifyResults.output;
+      }
+    }
+    indexHtml = indexHtml.replace(match[0], `<script>${serviceWorker}</script>\n${match[0]}`);
   }
 
   return indexHtml;
@@ -30,7 +41,6 @@ export function injectUnregisterServiceWorker(indexHtml: string) {
 
 function getRegisterSwScript(swUrl: string) {
   return `
-  <script>
     if ('serviceWorker' in navigator && location.protocol !== 'file:') {
       window.addEventListener('load', function(){
         navigator.serviceWorker.register('${swUrl}')
@@ -50,7 +60,6 @@ function getRegisterSwScript(swUrl: string) {
           .catch(function(err) { console.log('service worker error', err) });
       });
     }
-  </script>
 `;
 }
 
