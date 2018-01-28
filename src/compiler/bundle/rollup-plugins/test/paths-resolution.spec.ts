a830f4835fa1d475158475cceff16219d2bcd2fc
Bogdan Matei <contact@bmatei.com>
2018-01-28 12:55:18 +0200

    enhance unit tests

--- src/compiler/bundle/rollup-plugins/test/paths-resolution.spec.ts
+++ src/compiler/bundle/rollup-plugins/test/paths-resolution.spec.ts
@@ -14,131 +14,151 @@ import pathsResolver from '../paths-resolution';
 
 describe('paths-resolution', () => {
 
-  describe('resolve paths correctly', () => {
-
-    it('should resolve UNIX paths', async () => {
-      const pathsResolverInstance = await pathsResolver(config, compilerCtx, {
-        paths: {
-          '~components/*': [
-            'src/components/*'
-          ],
-          '~decorators/test': [
-            'src/decorators/test/test'
-          ],
-          '~test-helper': [
-            'src/helpers/test-helper/test-helper'
-          ]
-        }
-      })
-
-      expect(
-        pathsResolverInstance.resolveId(
-          '~components/app-home/app-home',
-          '/virtual/path/workspace/src/components/app-home/app-home.actions.ts'
-        )
-      ).toBe('/virtual/path/workspace/dist/collection/components/app-home/app-home.js');
-
-      expect(
-        pathsResolverInstance.resolveId(
-          '~decorators/test',
-          '/virtual/path/workspace/src/components/app-home/app-home.actions.ts'
-        )
-      ).toBe('/virtual/path/workspace/dist/collection/decorators/test/test.js');
-
-      expect(
-        pathsResolverInstance.resolveId(
-          '~test-helper',
-          '/virtual/path/workspace/src/helpers/test-helper/test-helper.ts'
-        )
-      ).toBe('/virtual/path/workspace/dist/collection/helpers/test-helper/test-helper.js');
+  it('should resolve UNIX paths', async () => {
+    const pathsResolverInstance = await pathsResolver(config, compilerCtx, {
+      paths: {
+        '~components/*': [
+          'src/components/*'
+        ],
+        'decorators/test': [
+          'src/decorators/test/test'
+        ],
+        '~test-helper': [
+          'src/helpers/test-helper/test-helper'
+        ]
+      }
     });
 
-    it('should resolve Windows paths', async () => {
-      const pathsResolverInstance = await pathsResolver(config, compilerCtx, {
-        paths: {
-          '~components\\*': [
-            'src\\components\\*'
-          ],
-          '~decorators\\test': [
-            'src\\decorators\\test\\test'
-          ],
-          '~test-helper': [
-            'src\\helpers\\test-helper\\test-helper'
-          ]
-        }
-      })
+    expect(
+      pathsResolverInstance.resolveId(
+        '~components/app-home/app-home',
+        '/virtual/path/workspace/src/components/app-home/app-home.actions.ts'
+      )
+    ).toBe('/virtual/path/workspace/dist/collection/components/app-home/app-home.js');
 
-      expect(
-        pathsResolverInstance.resolveId(
-          '~components\\app-home\\app-home',
-          '/virtual/path/workspace/src/components/app-home/app-home.actions.ts'
-        )).toBe('/virtual/path/workspace/dist/collection/components/app-home/app-home.js');
+    expect(
+      pathsResolverInstance.resolveId(
+        'decorators/test',
+        '/virtual/path/workspace/src/components/app-home/app-home.actions.ts'
+      )
+    ).toBe('/virtual/path/workspace/dist/collection/decorators/test/test.js');
 
-      expect(
-        pathsResolverInstance.resolveId(
-          '~components\\app-home\\app-home',
-          '/virtual/path/workspace/src/components/app-home/app-home.actions.ts'
-        )
-      ).toBe('/virtual/path/workspace/dist/collection/components/app-home/app-home.js');
+    expect(
+      pathsResolverInstance.resolveId(
+        '~test-helper',
+        '/virtual/path/workspace/src/helpers/test-helper/test-helper.ts'
+      )
+    ).toBe('/virtual/path/workspace/dist/collection/helpers/test-helper/test-helper.js');
+  });
 
-      expect(
-        pathsResolverInstance.resolveId(
-          '~decorators\\test',
-          '/virtual/path/workspace/src/components/app-home/app-home.actions.ts'
-        )
-      ).toBe('/virtual/path/workspace/dist/collection/decorators/test/test.js');
-
-      expect(
-        pathsResolverInstance.resolveId(
-          '~test-helper',
-          '/virtual/path/workspace/src/helpers/test-helper/test-helper.ts'
-        )
-      ).toBe('/virtual/path/workspace/dist/collection/helpers/test-helper/test-helper.js');
+  it('should resolve Windows paths', async () => {
+    const pathsResolverInstance = await pathsResolver(config, compilerCtx, {
+      paths: {
+        '~components\\*': [
+          'src\\components\\*'
+        ],
+        'decorators\\test': [
+          'src\\decorators\\test\\test'
+        ],
+        '~test-helper': [
+          'src\\helpers\\test-helper\\test-helper'
+        ]
+      }
     });
 
-    it('should resolve mixed paths', async () => {
-      const pathsResolverInstance = await pathsResolver(config, compilerCtx, {
-        paths: {
-          '~components/*': [
-            'src\\components\\*'
-          ],
-          '~decorators\\test': [
-            'src\\decorators\\test\\test'
-          ],
-          '~test-helper': [
-            'src/helpers/test-helper/test-helper'
-          ]
-        }
-      })
+    expect(
+      pathsResolverInstance.resolveId(
+        '~components\\app-home\\app-home',
+        '/virtual/path/workspace/src/components/app-home/app-home.actions.ts'
+      )).toBe('/virtual/path/workspace/dist/collection/components/app-home/app-home.js');
 
-      expect(
-        pathsResolverInstance.resolveId(
-          '~components/app-home/app-home',
-          '/virtual/path/workspace/src/components/app-home/app-home.actions.ts'
-        )).toBe('/virtual/path/workspace/dist/collection/components/app-home/app-home.js');
+    expect(
+      pathsResolverInstance.resolveId(
+        '~components\\app-home\\app-home',
+        '/virtual/path/workspace/src/components/app-home/app-home.actions.ts'
+      )
+    ).toBe('/virtual/path/workspace/dist/collection/components/app-home/app-home.js');
 
-      expect(
-        pathsResolverInstance.resolveId(
-          '~components/app-home/app-home',
-          '/virtual/path/workspace/src/components/app-home/app-home.actions.ts'
-        )
-      ).toBe('/virtual/path/workspace/dist/collection/components/app-home/app-home.js');
+    expect(
+      pathsResolverInstance.resolveId(
+        'decorators\\test',
+        '/virtual/path/workspace/src/components/app-home/app-home.actions.ts'
+      )
+    ).toBe('/virtual/path/workspace/dist/collection/decorators/test/test.js');
 
-      expect(
-        pathsResolverInstance.resolveId(
-          '~decorators\\test',
-          '/virtual/path/workspace/src/components/app-home/app-home.actions.ts'
-        )
-      ).toBe('/virtual/path/workspace/dist/collection/decorators/test/test.js');
+    expect(
+      pathsResolverInstance.resolveId(
+        '~test-helper',
+        '/virtual/path/workspace/src/helpers/test-helper/test-helper.ts'
+      )
+    ).toBe('/virtual/path/workspace/dist/collection/helpers/test-helper/test-helper.js');
+  });
 
-      expect(
-        pathsResolverInstance.resolveId(
-          '~test-helper',
-          '/virtual/path/workspace/src/helpers/test-helper/test-helper.ts'
-        )
-      ).toBe('/virtual/path/workspace/dist/collection/helpers/test-helper/test-helper.js');
+  it('should resolve mixed paths', async () => {
+    const pathsResolverInstance = await pathsResolver(config, compilerCtx, {
+      paths: {
+        '~components/*': [
+          'src\\components\\*'
+        ],
+        'decorators\\test': [
+          'src\\decorators/test\\test'
+        ],
+        '~test-helper': [
+          'src/helpers\\test-helper/test-helper'
+        ]
+      }
     });
 
+    expect(
+      pathsResolverInstance.resolveId(
+        '~components/app-home/app-home',
+        '/virtual/path/workspace/src/components/app-home/app-home.actions.ts'
+      )).toBe('/virtual/path/workspace/dist/collection/components/app-home/app-home.js');
+
+    expect(
+      pathsResolverInstance.resolveId(
+        '~components/app-home\\app-home',
+        '/virtual/path/workspace/src/components/app-home/app-home.actions.ts'
+      )
+    ).toBe('/virtual/path/workspace/dist/collection/components/app-home/app-home.js');
+
+    expect(
+      pathsResolverInstance.resolveId(
+        'decorators\\test',
+        '/virtual/path/workspace/src/components/app-home/app-home.actions.ts'
+      )
+    ).toBe('/virtual/path/workspace/dist/collection/decorators/test/test.js');
+
+    expect(
+      pathsResolverInstance.resolveId(
+        '~test-helper',
+        '/virtual/path/workspace/src/helpers/test-helper/test-helper.ts'
+      )
+    ).toBe('/virtual/path/workspace/dist/collection/helpers/test-helper/test-helper.js');
+
+  });
+
+  it('should return null for unexisting paths', async () => {
+    const pathsResolverInstance = await pathsResolver(config, compilerCtx, {
+      paths: {
+        '~components/*': [
+          'src/components/*'
+        ],
+        'decorators/test': [
+          'src/decorators/test/test'
+        ],
+        '~test-helper': [
+          'src/helpers/test-helper/test-helper'
+        ]
+      }
+    });
+
+    expect(
+      pathsResolverInstance.resolveId(
+        '~unexisting-path/app-home/app-home',
+        '/virtual/path/workspace/src/components/app-home/app-home.actions.ts'
+      )
+    ).toBe(null);
   });
 
   var config: Config;
