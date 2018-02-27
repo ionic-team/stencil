53bb4d1c11b77c0a3f11fc8db345fe195c200a4a
Adam Bradley <adambradley25@gmail.com>
2018-02-27 14:09:35 -0600

    fix(types): add JSX types to injected stencil core dts

--- scripts/declarations/stencil.core.d.ts
+++ scripts/declarations/stencil.core.d.ts
@@ -6,3 +6,18 @@ export interface EventEmitter<T= any> {
 export interface EventListenerEnable {
   (instance: any, eventName: string, enabled: boolean, attachTo?: string|Element, passive?: boolean): void;
 }
+
+declare global {
+  namespace JSX {
+    interface Element {}
+    export interface IntrinsicElements {}
+  }
+  namespace JSXElements {}
+
+  interface HTMLStencilElement extends HTMLElement {
+    componentOnReady(): Promise<this>;
+    componentOnReady(done: (ele?: this) => void): void;
+  }
+
+  interface HTMLAttributes {}
+}
