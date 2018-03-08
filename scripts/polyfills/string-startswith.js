2c09fa59564a626ef81bbada2e99b6d61222cfe7
Fin Hopkins <fin.hopkins@boston.gov>
2018-03-08 11:40:34 -0500

    Makes polyfills writable properties
    Defining polyfills as non-writable causes "Assignment to read-only
    properties is not allowed in strict mode" errors in certain
    circumstances, such as using MobX and its ObservableArray subclass of
    Array.
    
    This follows the conventions from polyfill.io around the options passed
    to `defineProperty`.
    
    Fixes ionic-team/stencil#606

--- scripts/polyfills/string-startswith.js
+++ scripts/polyfills/string-startswith.js
@@ -1,4 +1,4 @@
 /*!
 String.prototype.startsWith
 */
-String.prototype.startsWith||Object.defineProperty(String.prototype,"startsWith",{value:function(b,a){return this.substr(!a||0>a?0:+a,b.length)===b}});
+String.prototype.startsWith||Object.defineProperty(String.prototype,"startsWith",{writable:!0,configurable:!0,value:function(b,a){return this.substr(!a||0>a?0:+a,b.length)===b}});
