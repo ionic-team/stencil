b439cadbd837f163fc23cb8dfd2b5e4e7f12ff83
Fin Hopkins <phopkins@gmail.com>
2018-03-08 13:12:50 -0500

    fix(polyfills): makes polyfills writable properties
    Defining polyfills as non-writable causes "Assignment to read-only
    properties is not allowed in strict mode" errors in certain
    circumstances, such as using MobX and its ObservableArray subclass of
    Array.
    
    This follows the conventions from polyfill.io around the options passed
    to `defineProperty`.
    
    Fixes #606

--- scripts/polyfills/string-endswith.js
+++ scripts/polyfills/string-endswith.js
@@ -1,4 +1,4 @@
 /*!
 String.prototype.endsWith
 */
-String.prototype.endsWith||(String.prototype.endsWith=function(b,a){if(void 0===a||a>this.length)a=this.length;return this.substring(a-b.length,a)===b});
+String.prototype.endsWith||Object.defineProperty(String.prototype,"endsWith",{writable:!0,configurable:!0,value:function(b,a){if(void 0===a||a>this.length)a=this.length;return this.substring(a-b.length,a)===b}});
