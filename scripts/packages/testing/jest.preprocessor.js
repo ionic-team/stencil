8787f9191f920e97f5f0c856659af7b1bd22c223
jgw96 <jgw9617@gmail.com>
2018-04-09 11:51:13 -0500

    chore(testing): remove un-necessary code

--- scripts/packages/testing/jest.preprocessor.js
+++ scripts/packages/testing/jest.preprocessor.js
@@ -12,30 +12,11 @@ function normalizePath(str) {
   var NON_ASCII_REGEX = /[^\x00-\x80]+/;
   var SLASH_REGEX = /\\/g;
 
-  if (typeof str !== 'string') {
-    throw new Error(`invalid path to normalize`);
-  }
-  str = str.trim();
-
   if (EXTENDED_PATH_REGEX.test(str) || NON_ASCII_REGEX.test(str)) {
     return str;
   }
 
   str = str.replace(SLASH_REGEX, '/');
-
-  // always remove the trailing /
-  // this makes our file cache look ups consistent
-  if (str.charAt(str.length - 1) === '/') {
-    const colonIndex = str.indexOf(':');
-    if (colonIndex > -1) {
-      if (colonIndex < str.length - 2) {
-        str = str.substring(0, str.length - 1);
-      }
-
-    } else if (str.length > 1) {
-      str = str.substring(0, str.length - 1);
-    }
-  }
 
   return str;
 }
