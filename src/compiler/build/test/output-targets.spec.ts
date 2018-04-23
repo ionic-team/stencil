177618222b85c7871e458e59e514dce2181ae086
Josh Thomas <jthoms1@gmail.com>
2018-04-23 15:47:12 -0500

    chore(): testing root filepath fix for windows.

--- src/compiler/build/test/output-targets.spec.ts
+++ src/compiler/build/test/output-targets.spec.ts
@@ -3,6 +3,7 @@ import { doNotExpectFiles, expectFiles } from '../../../testing/utils';
 import { TestingCompiler } from '../../../testing/testing-compiler';
 import { TestingConfig } from '../../../testing/testing-config';
 
+const root = path.resolve('/');
 
 describe('outputTargets', () => {
 
@@ -10,18 +11,17 @@ describe('outputTargets', () => {
   let c: TestingCompiler;
   let config: TestingConfig;
 
-
   it('default www files', async () => {
 
     config = new TestingConfig();
     config.buildAppCore = true;
-    config.rootDir = path.join('/', 'User', 'testing', '/');
+    config.rootDir = path.join(root, 'User', 'testing', '/');
 
     c = new TestingCompiler(config);
 
     await c.fs.writeFiles({
-      [path.join('/', 'User', 'testing', 'src', 'index.html')]: `<cmp-a></cmp-a>`,
-      [path.join('/', 'User', 'testing', 'src', 'components', 'cmp-a.tsx')]: `@Component({ tag: 'cmp-a' }) export class CmpA {}`,
+      [path.join(root, 'User', 'testing', 'src', 'index.html')]: `<cmp-a></cmp-a>`,
+      [path.join(root, 'User', 'testing', 'src', 'components', 'cmp-a.tsx')]: `@Component({ tag: 'cmp-a' }) export class CmpA {}`,
     });
     await c.fs.commit();
 
@@ -29,60 +29,60 @@ describe('outputTargets', () => {
     expect(r.diagnostics).toEqual([]);
 
     expectFiles(c.fs, [
-      path.join('/', 'User', 'testing', 'www'),
-      path.join('/', 'User', 'testing', 'www', 'build'),
-      path.join('/', 'User', 'testing', 'www', 'build', 'app'),
-      path.join('/', 'User', 'testing', 'www', 'build', 'app.js'),
-      path.join('/', 'User', 'testing', 'www', 'build', 'app', 'cmp-a.js'),
-      path.join('/', 'User', 'testing', 'www', 'build', 'app', 'es5-build-disabled.js'),
-      path.join('/', 'User', 'testing', 'www', 'build', 'app', 'app.core.js'),
-      path.join('/', 'User', 'testing', 'www', 'build', 'app', 'app.registry.json'),
+      path.join(root, 'User', 'testing', 'www'),
+      path.join(root, 'User', 'testing', 'www', 'build'),
+      path.join(root, 'User', 'testing', 'www', 'build', 'app'),
+      path.join(root, 'User', 'testing', 'www', 'build', 'app.js'),
+      path.join(root, 'User', 'testing', 'www', 'build', 'app', 'cmp-a.js'),
+      path.join(root, 'User', 'testing', 'www', 'build', 'app', 'es5-build-disabled.js'),
+      path.join(root, 'User', 'testing', 'www', 'build', 'app', 'app.core.js'),
+      path.join(root, 'User', 'testing', 'www', 'build', 'app', 'app.registry.json'),
 
-      path.join('/', 'User', 'testing', 'www', 'index.html'),
+      path.join(root, 'User', 'testing', 'www', 'index.html'),
 
-      path.join('/', 'User', 'testing', 'src', 'components.d.ts'),
+      path.join(root, 'User', 'testing', 'src', 'components.d.ts'),
     ]);
 
     doNotExpectFiles(c.fs, [
-      path.join('/', 'User', 'testing', '__tmp__in__memory__', 'components', 'cmp-a.js'),
+      path.join(root, 'User', 'testing', '__tmp__in__memory__', 'components', 'cmp-a.js'),
 
-      path.join('/', 'User', 'testing', 'dist', '/'),
-      path.join('/', 'User', 'testing', 'dist', 'collection'),
-      path.join('/', 'User', 'testing', 'dist', 'collection', 'collection-manifest.json'),
-      path.join('/', 'User', 'testing', 'dist', 'collection', 'components'),
-      path.join('/', 'User', 'testing', 'dist', 'collection', 'components', 'cmp-a.js'),
+      path.join(root, 'User', 'testing', 'dist', '/'),
+      path.join(root, 'User', 'testing', 'dist', 'collection'),
+      path.join(root, 'User', 'testing', 'dist', 'collection', 'collection-manifest.json'),
+      path.join(root, 'User', 'testing', 'dist', 'collection', 'components'),
+      path.join(root, 'User', 'testing', 'dist', 'collection', 'components', 'cmp-a.js'),
 
-      path.join('/', 'User', 'testing', 'dist', 'testapp', '/'),
-      path.join('/', 'User', 'testing', 'dist', 'testapp.js'),
-      path.join('/', 'User', 'testing', 'dist', 'testapp', 'cmp-a.js'),
-      path.join('/', 'User', 'testing', 'dist', 'testapp', 'es5-build-disabled.js'),
-      path.join('/', 'User', 'testing', 'dist', 'testapp', 'testapp.core.js'),
+      path.join(root, 'User', 'testing', 'dist', 'testapp', '/'),
+      path.join(root, 'User', 'testing', 'dist', 'testapp.js'),
+      path.join(root, 'User', 'testing', 'dist', 'testapp', 'cmp-a.js'),
+      path.join(root, 'User', 'testing', 'dist', 'testapp', 'es5-build-disabled.js'),
+      path.join(root, 'User', 'testing', 'dist', 'testapp', 'testapp.core.js'),
 
-      path.join('/', 'User', 'testing', 'dist', 'types'),
-      path.join('/', 'User', 'testing', 'dist', 'types', 'components'),
-      path.join('/', 'User', 'testing', 'dist', 'types', 'components.d.ts'),
-      path.join('/', 'User', 'testing', 'dist', 'types', 'components', 'cmp-a.d.ts'),
-      path.join('/', 'User', 'testing', 'dist', 'types', 'stencil.core.d.ts'),
+      path.join(root, 'User', 'testing', 'dist', 'types'),
+      path.join(root, 'User', 'testing', 'dist', 'types', 'components'),
+      path.join(root, 'User', 'testing', 'dist', 'types', 'components.d.ts'),
+      path.join(root, 'User', 'testing', 'dist', 'types', 'components', 'cmp-a.d.ts'),
+      path.join(root, 'User', 'testing', 'dist', 'types', 'stencil.core.d.ts'),
     ]);
   });
 
   it('default dist files', async () => {
     config = new TestingConfig();
     config.buildAppCore = true;
-    config.rootDir = path.join('/', 'User', 'testing', '/');
+    config.rootDir = path.join(root, 'User', 'testing', '/');
     config.namespace = 'TestApp';
     config.outputTargets = [{ type: 'dist' }];
 
     c = new TestingCompiler(config);
 
     await c.fs.writeFiles({
-      [path.join('/', 'User', 'testing', 'package.json')]: `{
+      [path.join(root, 'User', 'testing', 'package.json')]: `{
         "main": "dist/testapp.js",
         "collection": "dist/collection/collection-manifest.json",
         "types": "dist/types/components.d.ts"
       }`,
-      [path.join('/', 'User', 'testing', 'src', 'index.html')]: `<cmp-a></cmp-a>`,
-      [path.join('/', 'User', 'testing', 'src', 'components', 'cmp-a.tsx')]: `@Component({ tag: 'cmp-a' }) export class CmpA {}`,
+      [path.join(root, 'User', 'testing', 'src', 'index.html')]: `<cmp-a></cmp-a>`,
+      [path.join(root, 'User', 'testing', 'src', 'components', 'cmp-a.tsx')]: `@Component({ tag: 'cmp-a' }) export class CmpA {}`,
     });
     await c.fs.commit();
 
@@ -90,31 +90,31 @@ describe('outputTargets', () => {
     expect(r.diagnostics).toEqual([]);
 
     expectFiles(c.fs, [
-      path.join('/', 'User', 'testing', 'dist', '/'),
+      path.join(root, 'User', 'testing', 'dist', '/'),
 
-      path.join('/', 'User', 'testing', 'dist', 'collection'),
-      path.join('/', 'User', 'testing', 'dist', 'collection', 'collection-manifest.json'),
-      path.join('/', 'User', 'testing', 'dist', 'collection', 'components'),
-      path.join('/', 'User', 'testing', 'dist', 'collection', 'components', 'cmp-a.js'),
+      path.join(root, 'User', 'testing', 'dist', 'collection'),
+      path.join(root, 'User', 'testing', 'dist', 'collection', 'collection-manifest.json'),
+      path.join(root, 'User', 'testing', 'dist', 'collection', 'components'),
+      path.join(root, 'User', 'testing', 'dist', 'collection', 'components', 'cmp-a.js'),
 
-      path.join('/', 'User', 'testing', 'dist', 'testapp', '/'),
-      path.join('/', 'User', 'testing', 'dist', 'testapp.js'),
-      path.join('/', 'User', 'testing', 'dist', 'testapp', 'cmp-a.js'),
-      path.join('/', 'User', 'testing', 'dist', 'testapp', 'es5-build-disabled.js'),
-      path.join('/', 'User', 'testing', 'dist', 'testapp', 'testapp.core.js'),
+      path.join(root, 'User', 'testing', 'dist', 'testapp', '/'),
+      path.join(root, 'User', 'testing', 'dist', 'testapp.js'),
+      path.join(root, 'User', 'testing', 'dist', 'testapp', 'cmp-a.js'),
+      path.join(root, 'User', 'testing', 'dist', 'testapp', 'es5-build-disabled.js'),
+      path.join(root, 'User', 'testing', 'dist', 'testapp', 'testapp.core.js'),
 
-      path.join('/', 'User', 'testing', 'dist', 'types'),
-      path.join('/', 'User', 'testing', 'dist', 'types', 'components'),
-      path.join('/', 'User', 'testing', 'dist', 'types', 'components.d.ts'),
-      path.join('/', 'User', 'testing', 'dist', 'types', 'components', 'cmp-a.d.ts'),
-      path.join('/', 'User', 'testing', 'dist', 'types', 'stencil.core.d.ts'),
+      path.join(root, 'User', 'testing', 'dist', 'types'),
+      path.join(root, 'User', 'testing', 'dist', 'types', 'components'),
+      path.join(root, 'User', 'testing', 'dist', 'types', 'components.d.ts'),
+      path.join(root, 'User', 'testing', 'dist', 'types', 'components', 'cmp-a.d.ts'),
+      path.join(root, 'User', 'testing', 'dist', 'types', 'stencil.core.d.ts'),
 
-      path.join('/', 'User', 'testing', 'src', 'components.d.ts'),
+      path.join(root, 'User', 'testing', 'src', 'components.d.ts'),
     ]);
 
     doNotExpectFiles(c.fs, [
-      path.join('/', 'User', 'testing', 'www', '/'),
-      path.join('/', 'User', 'testing', 'www', 'index.html'),
+      path.join(root, 'User', 'testing', 'www', '/'),
+      path.join(root, 'User', 'testing', 'www', 'index.html'),
     ]);
   });
 
@@ -122,7 +122,7 @@ describe('outputTargets', () => {
     config = new TestingConfig();
     config.flags.docs = true;
     config.buildAppCore = true;
-    config.rootDir = path.join('/', 'User', 'testing', '/');
+    config.rootDir = path.join(root, 'User', 'testing', '/');
     config.namespace = 'TestApp';
     config.outputTargets = [
       {
@@ -147,13 +147,13 @@ describe('outputTargets', () => {
     c = new TestingCompiler(config);
 
     await c.fs.writeFiles({
-      [path.join('/', 'User', 'testing', 'package.json')]: `{
+      [path.join(root, 'User', 'testing', 'package.json')]: `{
         "main": "custom-dist/dist-build/testapp.js",
         "collection": "custom-dist/dist-collection/collection-manifest.json",
         "types": "custom-dist/custom-types/components.d.ts"
       }`,
-      [path.join('/', 'User', 'testing', 'src', 'index.html')]: `<cmp-a></cmp-a>`,
-      [path.join('/', 'User', 'testing', 'src', 'components', 'cmp-a.tsx')]: `@Component({ tag: 'cmp-a' }) export class CmpA {}`,
+      [path.join(root, 'User', 'testing', 'src', 'index.html')]: `<cmp-a></cmp-a>`,
+      [path.join(root, 'User', 'testing', 'src', 'components', 'cmp-a.tsx')]: `@Component({ tag: 'cmp-a' }) export class CmpA {}`,
     });
     await c.fs.commit();
 
@@ -161,42 +161,42 @@ describe('outputTargets', () => {
     expect(r.diagnostics).toEqual([]);
 
     expectFiles(c.fs, [
-      path.join('/', 'User', 'testing', 'custom-dist'),
-      path.join('/', 'User', 'testing', 'custom-dist', 'dist-collection'),
-      path.join('/', 'User', 'testing', 'custom-dist', 'dist-collection', 'collection-manifest.json'),
-      path.join('/', 'User', 'testing', 'custom-dist', 'dist-collection', 'components'),
-      path.join('/', 'User', 'testing', 'custom-dist', 'dist-collection', 'components', 'cmp-a.js'),
+      path.join(root, 'User', 'testing', 'custom-dist'),
+      path.join(root, 'User', 'testing', 'custom-dist', 'dist-collection'),
+      path.join(root, 'User', 'testing', 'custom-dist', 'dist-collection', 'collection-manifest.json'),
+      path.join(root, 'User', 'testing', 'custom-dist', 'dist-collection', 'components'),
+      path.join(root, 'User', 'testing', 'custom-dist', 'dist-collection', 'components', 'cmp-a.js'),
 
-      path.join('/', 'User', 'testing', 'custom-dist', 'dist-build', 'testapp'),
-      path.join('/', 'User', 'testing', 'custom-dist', 'dist-build', 'testapp.js'),
-      path.join('/', 'User', 'testing', 'custom-dist', 'dist-build', 'testapp', 'cmp-a.js'),
-      path.join('/', 'User', 'testing', 'custom-dist', 'dist-build', 'testapp', 'es5-build-disabled.js'),
-      path.join('/', 'User', 'testing', 'custom-dist', 'dist-build', 'testapp', 'testapp.core.js'),
+      path.join(root, 'User', 'testing', 'custom-dist', 'dist-build', 'testapp'),
+      path.join(root, 'User', 'testing', 'custom-dist', 'dist-build', 'testapp.js'),
+      path.join(root, 'User', 'testing', 'custom-dist', 'dist-build', 'testapp', 'cmp-a.js'),
+      path.join(root, 'User', 'testing', 'custom-dist', 'dist-build', 'testapp', 'es5-build-disabled.js'),
+      path.join(root, 'User', 'testing', 'custom-dist', 'dist-build', 'testapp', 'testapp.core.js'),
 
-      path.join('/', 'User', 'testing', 'custom-dist', 'custom-types'),
-      path.join('/', 'User', 'testing', 'custom-dist', 'custom-types', 'components'),
-      path.join('/', 'User', 'testing', 'custom-dist', 'custom-types', 'components.d.ts'),
-      path.join('/', 'User', 'testing', 'custom-dist', 'custom-types', 'components', 'cmp-a.d.ts'),
-      path.join('/', 'User', 'testing', 'custom-dist', 'custom-types', 'stencil.core.d.ts'),
+      path.join(root, 'User', 'testing', 'custom-dist', 'custom-types'),
+      path.join(root, 'User', 'testing', 'custom-dist', 'custom-types', 'components'),
+      path.join(root, 'User', 'testing', 'custom-dist', 'custom-types', 'components.d.ts'),
+      path.join(root, 'User', 'testing', 'custom-dist', 'custom-types', 'components', 'cmp-a.d.ts'),
+      path.join(root, 'User', 'testing', 'custom-dist', 'custom-types', 'stencil.core.d.ts'),
 
-      path.join('/', 'User', 'testing', 'custom-www'),
-      path.join('/', 'User', 'testing', 'custom-www', 'www-build'),
-      path.join('/', 'User', 'testing', 'custom-www', 'www-build', 'testapp'),
-      path.join('/', 'User', 'testing', 'custom-www', 'www-build', 'testapp.js'),
-      path.join('/', 'User', 'testing', 'custom-www', 'www-build', 'testapp', 'cmp-a.js'),
-      path.join('/', 'User', 'testing', 'custom-www', 'www-build', 'testapp', 'es5-build-disabled.js'),
-      path.join('/', 'User', 'testing', 'custom-www', 'www-build', 'testapp', 'testapp.core.js'),
-      path.join('/', 'User', 'testing', 'custom-www', 'www-build', 'testapp', 'testapp.registry.json'),
-      path.join('/', 'User', 'testing', 'custom-www', 'custom-index.htm'),
+      path.join(root, 'User', 'testing', 'custom-www'),
+      path.join(root, 'User', 'testing', 'custom-www', 'www-build'),
+      path.join(root, 'User', 'testing', 'custom-www', 'www-build', 'testapp'),
+      path.join(root, 'User', 'testing', 'custom-www', 'www-build', 'testapp.js'),
+      path.join(root, 'User', 'testing', 'custom-www', 'www-build', 'testapp', 'cmp-a.js'),
+      path.join(root, 'User', 'testing', 'custom-www', 'www-build', 'testapp', 'es5-build-disabled.js'),
+      path.join(root, 'User', 'testing', 'custom-www', 'www-build', 'testapp', 'testapp.core.js'),
+      path.join(root, 'User', 'testing', 'custom-www', 'www-build', 'testapp', 'testapp.registry.json'),
+      path.join(root, 'User', 'testing', 'custom-www', 'custom-index.htm'),
 
-      path.join('/', 'User', 'testing', 'src', 'components', 'readme.md')
+      path.join(root, 'User', 'testing', 'src', 'components', 'readme.md')
     ]);
 
     doNotExpectFiles(c.fs, [
-      path.join('/', 'User', 'testing', 'www', '/'),
-      path.join('/', 'User', 'testing', 'www', 'index.html'),
-      path.join('/', 'User', 'testing', 'www', 'custom-index.htm'),
-      path.join('/', 'User', 'testing', 'custom-www', 'index.html'),
+      path.join(root, 'User', 'testing', 'www', '/'),
+      path.join(root, 'User', 'testing', 'www', 'index.html'),
+      path.join(root, 'User', 'testing', 'www', 'custom-index.htm'),
+      path.join(root, 'User', 'testing', 'custom-www', 'index.html'),
     ]);
   });
 
