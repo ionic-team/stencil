fb1f6ccc611925fc0db14751cc4e3b95f0bf4c14
Adam Bradley <adambradley25@gmail.com>
2018-04-24 22:20:34 -0500

    test(slot): fix slot-array test for ie11

--- test/karma/src/slot-array/karma.spec.ts
+++ test/karma/src/slot-array/karma.spec.ts
@@ -9,12 +9,9 @@ describe('slot array', () => {
   it('renders slotted content in the right position for polyfilled elements', async function() {
     if (!('attachShadow' in HTMLElement.prototype)) {
       const component = await renderTest('/slot-array/index.html');
-      const children = component.childNodes;
-      const firstNode = children.item(1);
-      expect(firstNode.nodeName).toEqual('SPAN');
 
-      const secondNode = children.item(2);
-      expect(secondNode.nodeName).toEqual('P');
+      expect(component.firstElementChild.nodeName).toEqual('SPAN');
+      expect(component.lastElementChild.nodeName).toEqual('P');
     }
   });
 });
