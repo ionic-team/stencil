aa9762c3e3db05b6d0884efb36a2951fc48688bf
Adam Bradley <adambradley25@gmail.com>
2018-05-20 23:21:11 -0500

    chore(sys): use config.sys path instead of node path

--- src/compiler/build/empty-dir.ts
+++ src/compiler/build/empty-dir.ts
@@ -1,5 +1,4 @@
 import * as d from '../../declarations';
-import * as p from 'path';
 
 
 export async function emptyOutputTargetDirs(config: d.Config, compilerCtx: d.CompilerCtx) {
@@ -17,7 +16,7 @@ export async function emptyOutputTargetDirs(config: d.Config, compilerCtx: d.Com
     // Check if there is a .gitkeep file
     // We want to keep it so people don't have to readd manually
     // to their projects each time.
-    const gitkeepPath = p.join(outputTarget.dir, '.gitkeep');
+    const gitkeepPath = config.sys.path.join(outputTarget.dir, '.gitkeep');
     const existsGitkeep = await compilerCtx.fs.access(gitkeepPath);
 
     await compilerCtx.fs.emptyDir(outputTarget.dir);
