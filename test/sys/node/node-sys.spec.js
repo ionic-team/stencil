ed3a01d392e72a1294ce01b97f280580f08eeadb
Adam Bradley <adambradley25@gmail.com>
2018-05-28 10:33:19 -0500

    test(node): fix sys.node tests for node6

--- test/sys/node/node-sys.spec.js
+++ test/sys/node/node-sys.spec.js
@@ -13,56 +13,47 @@ describe('test/sys/node', () => {
   });
 
 
-  it('autoprefixCss', async () => {
+  it('autoprefixCss', () => {
     const input = `
       div {
         flex: 1;
       }
     `;
-
-    let output = await sys.autoprefixCss(input);
-    expect(output).toContain('-webkit-box-flex: 1');
-    expect(output).toContain('-webkit-flex: 1');
-    expect(output).toContain('-ms-flex: 1');
-    expect(output).toContain('flex: 1');
-
-    output = await sys.autoprefixCss(input, null);
-    expect(output).toContain('-webkit-flex: 1');
-
-    output = await sys.autoprefixCss(input, undefined);
-    expect(output).toContain('-webkit-flex: 1');
-
-    output = await sys.autoprefixCss(input, true);
-    expect(output).toContain('-webkit-flex: 1');
+    return sys.autoprefixCss(input).then(output => {
+      expect(output).toContain('-webkit-box-flex: 1');
+      expect(output).toContain('-ms-flex: 1');
+      expect(output).toContain('flex: 1');
+    });
   });
 
-  it('gzipSize', async () => {
-    let size = await sys.gzipSize('88888888888888888888888888888888888888888888888888');
-    expect(size).toBe(24);
-
-    size = await sys.gzipSize('888888888888888888');
-    expect(size).toBe(23);
+  it('gzipSize', () => {
+    return sys.gzipSize('88888888888888888888888888888888888888888888888888').then(size => {
+      expect(size).toBe(24);
+    });
   });
 
-  it('minifyCss', async () => {
+  it('minifyCss', () => {
     const input = `
       /** comment **/
       body {
         color: #FF0000;
       }
     `;
-    const results = await sys.minifyCss(input);
-    expect(results.output).toBe(`body{color:red}`);
+
+    return sys.minifyCss(input).then(results => {
+      expect(results.output).toBe(`body{color:red}`);
+    });
   });
 
-  it('scopeCss', async () => {
+  it('scopeCss', () => {
     const cssText = `::slotted(*) {}`;
     const scopeAttribute = `data-ion-tag`;
     const hostScopeAttr = `data-ion-tag-host`;
     const slotScopeAttr = `data-ion-tag-slot`;
 
-    const scopeCss = await sys.scopeCss(cssText, scopeAttribute, hostScopeAttr, slotScopeAttr);
-    expect(scopeCss).toBe(`[data-ion-tag-slot] > * {}`);
+    return sys.scopeCss(cssText, scopeAttribute, hostScopeAttr, slotScopeAttr).then(scopeCss => {
+      expect(scopeCss).toBe(`[data-ion-tag-slot] > * {}`);
+    });
   });
 
 });
