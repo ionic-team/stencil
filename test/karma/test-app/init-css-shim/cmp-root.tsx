2cdfde81bd619faac878991f1ac1e109fcfa7760
Unknown <josh.vazq@outlook.com>
2018-06-26 22:26:06 +0200

    fix(init-css-shims): CSS_URL_REGEXP check relative to root paths

---
+++ test/karma/test-app/init-css-shim/cmp-root.tsx
@@ -0,0 +1,18 @@
+import { Component } from '../../../../dist/index';
+
+
+@Component({
+  tag: 'init-css-root',
+  styleUrl: 'cmp-root.css'
+})
+export class InitCssRoot {
+
+  render() {
+    return [
+    <div id="relative"></div>,
+    <div id="relativeToRoot"></div>,
+    <div id="absolute"></div>
+  ];
+  }
+
+}
