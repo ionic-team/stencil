c1bd8e0e77988371311f35c33acd395d62598a26
Adam Bradley <adambradley25@gmail.com>
2018-06-26 21:23:21 -0500

    chore(cli): add serve to cli help task

--- src/cli/task-help.ts
+++ src/cli/task-help.ts
@@ -9,18 +9,19 @@ export function helpTask(process: NodeJS.Process, logger: d.Logger) {
 
     ${p} ${logger.green('stencil build [--dev] [--watch] [--prerender] [--debug]')}
 
-      ${logger.green('--dev')} ${logger.dim('..................')} Execute a development build
-      ${logger.green('--watch')} ${logger.dim('................')} Execute a build in watch mode
-      ${logger.green('--prerender')} ${logger.dim('............')} Prerender URLs
-      ${logger.green('--stats')} ${logger.dim('................')} Write stencil-stats.json file
-      ${logger.green('--log')} ${logger.dim('..................')} Write stencil-build.log file
-      ${logger.green('--config')} ${logger.dim('...............')} Set stencil config file
-      ${logger.green('--docs')} ${logger.dim('.................')} Generate readme.md docs for each component
-      ${logger.green('--debug')} ${logger.dim('................')} Set the log level to debug
+      ${logger.green('--dev')} ${logger.dim('...............')} Execute a development build
+      ${logger.green('--watch')} ${logger.dim('.............')} Execute a build in watch mode
+      ${logger.green('--serve')} ${logger.dim('.............')} Start the dev-server
+      ${logger.green('--prerender')} ${logger.dim('.........')} Prerender URLs
+      ${logger.green('--stats')} ${logger.dim('.............')} Write stencil-stats.json file
+      ${logger.green('--log')} ${logger.dim('...............')} Write stencil-build.log file
+      ${logger.green('--config')} ${logger.dim('............')} Set stencil config file
+      ${logger.green('--docs')} ${logger.dim('..............')} Generate readme.md docs for each component
+      ${logger.green('--debug')} ${logger.dim('.............')} Set the log level to debug
 
   ${logger.bold('Examples:')}
 
-    ${p} ${logger.green('stencil build --dev --watch')}
+    ${p} ${logger.green('stencil build --dev --watch --serve')}
     ${p} ${logger.green('stencil build --prerender')}
 
 `);
