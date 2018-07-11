14951b1b6b064e6a4ea2774f0c0707ac2a995fb7
Adam Bradley <adambradley25@gmail.com>
2018-07-11 13:55:16 -0500

    chore(package-lock): remove package-lock

--- scripts/set-version.js
+++ scripts/set-version.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const path = require('path');
 
 const packageJsonPath = path.join(__dirname, '..', 'package.json');
-const packageLockPath = path.join(__dirname, '..', 'package-lock.json');
 
 
 function getVersion() {
@@ -12,8 +11,4 @@ function getVersion() {
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
 packageJson.version = getVersion();
 
-const packageLock = JSON.parse(fs.readFileSync(packageLockPath, 'utf8'));
-packageLock.version = packageJson.version;
-
 fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2) + '\n');
-fs.writeFileSync(packageLockPath, JSON.stringify(packageLock, null, 2) + '\n');
