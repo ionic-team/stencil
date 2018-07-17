72612e91df4a4303e972b3438b507b626cbf94bd
Josh Thomas <jthoms1@gmail.com>
2018-07-17 08:50:06 -0500

    remove console.log

--- src/compiler/bundle/rollup-plugins/bundle-entry-file.ts
+++ src/compiler/bundle/rollup-plugins/bundle-entry-file.ts
@@ -16,7 +16,6 @@ export default function bundleEntryFile(config: d.Config, buildCtx: d.BuildCtx, 
 
       if (importee.startsWith(ENTRY_KEY_PREFIX)) {
         const bundle = entryModules.find(b => b.entryKey === importee);
-        console.log(bundle);
         if (bundle) {
           return bundle.entryKey;
         }
