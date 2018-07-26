71b2c8d396f3c36cd47b1b59aedc8cca2d5b3353
Adam Bradley <adambradley25@gmail.com>
2018-07-25 22:38:59 -0500

    fix(minify): do not minify inline scripts that are not js types
    Closes #948

--- src/compiler/html/minify-inline-scripts.ts
+++ src/compiler/html/minify-inline-scripts.ts
@@ -7,28 +7,55 @@ export async function minifyInlineScripts(config: d.Config, compilerCtx: d.Compi
   const promises: Promise<any>[] = [];
 
   for (let i = 0; i < scripts.length; i++) {
-    promises.push(minifyInlineStyle(config, compilerCtx, diagnostics, scripts[i]));
+    promises.push(minifyInlineScript(config, compilerCtx, diagnostics, scripts[i]));
   }
 
   await Promise.all(promises);
 }
 
 
-export async function minifyInlineStyle(config: d.Config, compilerCtx: d.CompilerCtx, diagnostics: d.Diagnostic[], script: HTMLScriptElement) {
+export function canMinifyInlineScript(script: HTMLScriptElement) {
   if (script.hasAttribute('src')) {
+    return false;
+  }
+
+  let type = script.getAttribute('type');
+  if (typeof type === 'string') {
+    type = type.trim().toLowerCase();
+    if (!VALID_SCRIPT_TYPES.includes(type)) {
+      return false;
+    }
+  }
+
+  if (script.innerHTML.includes('/*')) {
+    return true;
+  }
+
+  if (script.innerHTML.includes('  ')) {
+    return true;
+  }
+
+  if (script.innerHTML.includes('\t')) {
+    return true;
+  }
+
+  return false;
+}
+
+const VALID_SCRIPT_TYPES = ['application/javascript', 'application/ecmascript', ''];
+
+
+export async function minifyInlineScript(config: d.Config, compilerCtx: d.CompilerCtx, diagnostics: d.Diagnostic[], script: HTMLScriptElement) {
+  if (!canMinifyInlineScript(script)) {
     return;
   }
 
-  if (script.innerHTML.includes('  ') || script.innerHTML.includes('\t')) {
+  const minifyResults = await minifyJs(config, compilerCtx, script.innerHTML, 'es5', false);
+  minifyResults.diagnostics.forEach(diagnostic => {
+    diagnostics.push(diagnostic);
+  });
 
-    const minifyResults = await minifyJs(config, compilerCtx, script.innerHTML, 'es5', false);
-    minifyResults.diagnostics.forEach(diagnostic => {
-      diagnostics.push(diagnostic);
-    });
-
-    if (typeof minifyResults.output === 'string') {
-      script.innerHTML = minifyResults.output;
-    }
-
+  if (typeof minifyResults.output === 'string') {
+    script.innerHTML = minifyResults.output;
   }
 }
