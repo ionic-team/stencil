d051f759abf1686451d2351012dfd4588646bace
Josh Thomas <jthoms1@gmail.com>
2018-07-27 11:45:15 -0500

    make typescript stop complaning about node globals

--- src/testing/mocks.ts
+++ src/testing/mocks.ts
@@ -15,7 +15,7 @@ import { validateConfig } from '../compiler/config/validate-config';
 import { BuildContext } from '../compiler/build/build-ctx';
 import { getTestBuildConditionals } from '../compiler/app/build-conditionals';
 
-global.__BUILD_CONDITIONALS__ = getTestBuildConditionals();
+(global as any).__BUILD_CONDITIONALS__ = getTestBuildConditionals();
 
 export function mockPlatform(win?: any, domApi?: d.DomApi, cmpRegistry?: d.ComponentRegistry) {
   const hydrateResults: d.HydrateResults = {
