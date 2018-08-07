132afe24fc16e1853f01de811e64b605362263d0
Josh Thomas <jthoms1@gmail.com>
2018-08-07 09:41:56 -0500

    wip

--- src/declarations/vdom.ts
+++ src/declarations/vdom.ts
@@ -1,5 +1,22 @@
 import * as d from '.';
 
+export interface Hyperscript {
+  (sel: any): VNode;
+  (sel: Node, data: VNodeData): VNode;
+  (sel: any, data: VNodeData): VNode;
+  (sel: any, text: string): VNode;
+  (sel: any, children: Array<VNode | undefined | null>): VNode;
+  (sel: any, data: VNodeData, text: string): VNode;
+  (sel: any, data: VNodeData, children: Array<VNode | undefined | null>): VNode;
+  (sel: any, data: VNodeData, children: VNode): VNode;
+}
+
+// these must be "var" variables
+// so that they could be re-declared by
+// other collections, do not use "const" or "let"
+// "h" function is global so JSX doesn't throw typescript errors
+export var h: Hyperscript;
+
 export interface VNode {
   // using v prefixes largely so closure has no issue property renaming
   vtag?: string | number | Function;
