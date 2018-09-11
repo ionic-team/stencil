d23469dd8eb35e2939a7e3d8ccefb6db5022124a
CaptGrumpyPants <cary.j.smith@gmail.com>
2018-09-11 09:00:30 -0500

    fix(dom-api): adjust $addClass for IE11 svgs
    IE11 does not support classList on SVG (or MathML)

--- src/renderer/dom-api.ts
+++ src/renderer/dom-api.ts
@@ -55,8 +55,19 @@ export function createDomApi(App: AppGlobal, win: any, doc: Document): DomApi {
     $appendChild: (parentNode: Node, childNode: Node) =>
       parentNode.appendChild(childNode),
 
-    $addClass: (elm: Element, cssClass: string) =>
-      elm.classList.add(cssClass),
+    $addClass: (elm: Element, cssClass: string) => {
+      if (!!elm.classList) {
+        elm.classList.add(cssClass);
+      } else if (elm.nodeName === "SVG") {
+        // https://caniuse.com/#search=classList
+        // IE11 really does not do <svg> properly :-/
+        let appliedClasses = elm.getAttribute("class") || "";
+        appliedClasses = !appliedClasses.split(" ").includes(cssClass)
+          ? `${elm.className} ${cssClass}`
+          : appliedClasses;
+        elm.setAttribute("class", appliedClasses);
+      }
+    },
 
     $childNodes: (node: Node) =>
       node.childNodes,
