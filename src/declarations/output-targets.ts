7a1a98218830c18719c2a6d1b623f0e1864fb5a0
Adam Bradley <adambradley25@gmail.com>
2018-09-20 16:20:59 -0500

    docs(output-target): add OutputTargetWww jsdoc

--- src/declarations/output-targets.ts
+++ src/declarations/output-targets.ts
@@ -2,28 +2,142 @@ import * as d from '.';
 
 
 export interface OutputTargetWww extends OutputTargetBase {
+  /**
+   * Webapp output target.
+   */
   type: 'www';
 
+  /**
+   * The directory to write the app's JavaScript and CSS build
+   * files to. The default is to place this directory as a child
+   * to the `dir` config. Default: `build`
+   */
   buildDir?: string;
+
+  /**
+   * The directory to write the entire application to.
+   * Note, the `buildDir` is where the app's JavaScript and CSS build
+   * files are written. Default: `www`
+   */
   dir?: string;
+
+  /**
+   * Empty the build directory of all files and directories on first build.
+   * Default: `true`
+   */
   empty?: boolean;
+
   resourcesUrl?: string;
 
+  /**
+   * The default index html file of the app, commonly found at the
+   * root of the `src` directory.
+   * Default: `index.html`
+   */
   indexHtml?: string;
+
   serviceWorker?: d.ServiceWorkerConfig | null;
+
+  /**
+   * The base url of the app, which should be a relative path.
+   * Default: `/`
+   */
   baseUrl?: string;
+
+  /**
+   * Add a canonical link to the `<head>`. Default: `true`
+   */
   canonicalLink?: boolean;
+
+  /**
+   * If extra whitespace should be removed from the prerendered
+   * HTML or not. Default: `true`
+   */
   collapseWhitespace?: boolean;
+
+  /**
+   * If components should be hydrated while prerendering.
+   * Default: `true`
+   */
   hydrateComponents?: boolean;
+
+  /**
+   * If styles should be inlined during prerendering.
+   * Default: `true`
+   */
   inlineStyles?: boolean;
+
+  /**
+   * If the loader script should be inlined into the prerendered
+   * page or not. Inlining the loader script allows the first render
+   * to have one less request, but adds a small amount more to the file size.
+   * Default: `true`
+   */
   inlineLoaderScript?: boolean;
+
+
   inlineAssetsMaxSize?: number;
+
+  /**
+   * If prerendering should continue to crawl local links and prerender.
+   * Default: `true`
+   */
   prerenderUrlCrawl?: boolean;
-  prerenderLocations?: d.PrerenderLocation[];
+
+  /**
+   * The starting points for prerendering. This should be relative
+   * paths. Default config is to starting at the index page: `/`.
+   * Default: `[{ path: '/' }]`
+   */
+  prerenderLocations?: { path: string; }[];
+
+  /**
+   * This filter is called for every url found while crawling. Returning
+   * `true` allows the URL to be crawled, and returning `false` will skip
+   * the URL for prerendering. Default: `undefined`
+   */
   prerenderFilter?: (url: d.Url) => boolean;
-  prerenderPathHash?: boolean;
-  prerenderPathQuery?: boolean;
+
+  /**
+   * Format the HTML all pretty-like. Great for debugging, bad for build performance.
+   */
+  prettyHtml?: boolean;
+
+  /**
+   * Maximum number of pages to be prerendering at one time. The optimal number
+   * varies between machines and any feedback regarding the number that best
+   * works for your setup would help. Default: `12`
+   */
   prerenderMaxConcurrent?: number;
+
+  /**
+   * Keep hashes in the URL while prerendering. Default: `false`
+   */
+  prerenderPathHash?: boolean;
+
+  /**
+   * Keep querystrings in the URL while prerendering. Default: `false`
+   */
+  prerenderPathQuery?: boolean;
+
+  /**
+   * Network requests to abort while prerendering. Default is to ignore
+   * some common analytic and advertisement urls such `google-analytics.com`
+   * and `doubleclick`.
+   */
+  prerenderAbortRequests?: {
+    domain?: string;
+  }[];
+
+  /**
+   * Remove `<!--html comments-->` from prerendered output. Default: `true`
+   */
+  removeHtmlComments?: boolean;
+
+  /**
+   * Analyze each page after prerendering and removes any CSS not used.
+   * Default: `true`
+   */
   removeUnusedStyles?: boolean;
 }
 
