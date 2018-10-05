9f90d268d2e1492f2e9c36fb8d452cb1ea232abf
Adam Bradley <adambradley25@gmail.com>
2018-10-05 14:01:14 -0500

    test(screenshot): update screenshot app

--- screenshot/compare/build/app/chunk-b59b7ee0.js
+++
@@ -1,2 +0,0 @@
-/*! Built with http://stenciljs.com */
-const{h:n}=window.App;function t(n,t){if(!n){const n="ASSERT: "+t;throw console.error(n),new Error(n)}}function e(n){return n.timeStamp||Date.now()}function o(n){if(n){const t=n.changedTouches;if(t&&t.length>0){const n=t[0];return{x:n.clientX,y:n.clientY}}if(void 0!==n.pageX)return{x:n.pageX,y:n.pageY}}return{x:0,y:0}}function r(n,t=0){let e;return(...o)=>{clearTimeout(e),e=setTimeout(n,t,...o)}}export{t as a,r as b,e as c,o as d};
--- screenshot/compare/build/app/status-tap.es5.js
+++
@@ -1,2 +0,0 @@
-/*! Built with http://stenciljs.com */
-App.loadBundle("status-tap.js",["exports"],function(n){window,n.startStatusTap=function(n,t){n.addEventListener("statusTap",function(){t.read(function(){var e=n.document.elementFromPoint(n.innerWidth/2,n.innerHeight/2);if(e){var o=e.closest("ion-content");o&&o.componentOnReady().then(function(){t.write(function(){o.scrollToTop(300)})})}})})},Object.defineProperty(n,"__esModule",{value:!0})});
