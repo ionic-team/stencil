4501d5dc525862dc88d8e9e0b41e977acbfe949b
Adam Bradley <adambradley25@gmail.com>
2018-10-13 13:59:51 -0500

    test(screenshot): update screenshot app

--- screenshot/compare/build/app/chunk-f5f83825.js
+++
@@ -1,2 +0,0 @@
-/*! Built with http://stenciljs.com */
-const{h:e}=window.App;var n="undefined"!=typeof global?global:"undefined"!=typeof self?self:"undefined"!=typeof window?window:{};export{n as a};
