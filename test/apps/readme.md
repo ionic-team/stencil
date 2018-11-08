15936f1ebe76c0d4d3c1a67ab513dfce5e353370
Adam Bradley <adambradley25@gmail.com>
2018-11-08 23:28:46 +0000

    test(apps): add more test apps

---
+++ test/apps/readme.md
@@ -0,0 +1,18 @@
+
+## Hello World
+
+| File                       | Brotli   | Gzipped  | Minified |
+|----------------------------|----------|----------|----------|
+| hello-world.entry.js       | 110B     | 146B     | 159B     |
+| app.core.js                | 3.52KB   | 3.90KB   | 8.33KB   |
+
+
+
+## Todo App
+
+| File                       | Brotli   | Gzipped  | Minified |
+|----------------------------|----------|----------|----------|
+| my-todo.entry.js           | 1.42KB   | 1.70KB   | 4.84KB   |
+| app.core.js                | 5.36KB   | 5.90KB   | 13.00KB  |
+| app.css                    | 143B     | 188B     | 233B     |
+
---
+++ test/apps/todo-app/src/components/my-todo/my-todo.tsx
@@ -0,0 +1,55 @@
+import { Component, State, Listen } from '../../../../../../dist/index';
+
+@Component({
+  tag: 'my-todo',
+  styleUrl: 'my-todo.css'
+})
+export class MyTodo {
+  @State() list: TodoItem[] = [
+    { text: 'my initial todo', checked: false },
+    { text: 'Learn about Web Components', checked: true }
+  ];
+
+  @Listen('onTodoInputSubmit')
+  todoInputSubmiHandler(e: CustomEvent) {
+    this.list = [...this.list, { text: e.detail, checked: false, }];
+  }
+
+  @Listen('onTodoItemChecked')
+  todoItemCheckedHandler(e: CustomEvent) {
+    const list = [...this.list];
+    const item = list[e.detail];
+    list[e.detail] = Object.assign({}, item, { checked: !item.checked });
+    this.list = list;
+  }
+
+  @Listen('onTodoItemRemove')
+  todoItemRemoveHandler(e: CustomEvent) {
+    this.list = [...this.list.slice(0, e.detail), ...this.list.slice(e.detail + 1)];
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Todos Stencil</h1>
+        <section>
+          <todo-input></todo-input>
+          <ul id="list-container">
+            {this.list.map((item, index) => (
+              <todo-item
+                checked={item.checked}
+                text={item.text}
+                index={index}
+              />
+            ))}
+          </ul>
+        </section>
+      </div>
+    );
+  }
+}
+
+interface TodoItem {
+  text: string;
+  checked: boolean;
+}
