89245611ca32cd8adf596d1a64a442fcfb5386ac
Manu MA <manu.valladolid@gmail.com>
2018-12-01 15:43:54 +0100

    feat(): generate experimental web-components.json (#1256)

---
+++ src/compiler/docs/generate-web-components-json.ts
@@ -0,0 +1,22 @@
+import * as d from '../../declarations';
+import { WEB_COMPONENTS_JSON_FILE_NAME } from '../../util/constants';
+import { normalizePath } from '../util';
+
+export async function generateWebComponentsJson(config: d.Config, compilerCtx: d.CompilerCtx, distOutputs: d.OutputTargetDist[], docsData: d.JsonDocs) {
+  const json = {
+    'tags': docsData.components.map(cmp => ({
+      'label': cmp.tag,
+      'description': cmp.readme,
+      'attributes': cmp.props.filter(p => p.attr).map(p => ({
+        'label': p.attr,
+        'description': p.docs,
+        'required': p.required
+      }))
+    }))
+  };
+  const jsonContent = JSON.stringify(json, null, 2);
+  await Promise.all(distOutputs.map(async distOutput => {
+    const filePath = normalizePath(config.sys.path.join(distOutput.buildDir, WEB_COMPONENTS_JSON_FILE_NAME));
+    await compilerCtx.fs.writeFile(filePath, jsonContent);
+  }));
+}
