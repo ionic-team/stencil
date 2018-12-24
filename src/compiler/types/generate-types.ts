557289509df31bdb5a5d47289afea1626ba2ad09
Adam Bradley <adambradley25@gmail.com>
2018-12-24 11:02:58 -0600

    refactor(types): init types refactor

---
+++ src/compiler/types/generate-types.ts
@@ -0,0 +1,90 @@
+import * as d from '../../declarations';
+import { copyStencilCoreDts, updateStencilTypesImports } from './stencil-types';
+import { generateComponentTypes } from './generate-component-types';
+import { isDtsFile, pathJoin } from '../util';
+import * as v from './validate-package-json';
+
+
+export async function generateTypes(config: d.Config, compilerCtx: d.CompilerCtx, buildCtx: d.BuildCtx) {
+  const outputTargets = (config.outputTargets as d.OutputTargetDist[]).filter(o => {
+    return o.type === 'dist';
+  });
+
+  if (outputTargets.length === 0) {
+    return;
+  }
+
+  const pkgData = await readPackageJson(config, compilerCtx);
+
+  const promises = outputTargets.map(async outputTarget => {
+    await generateTypesAndValidate(config, compilerCtx, buildCtx, pkgData, outputTarget);
+  });
+
+  await Promise.all(promises);
+}
+
+
+async function generateTypesAndValidate(config: d.Config, compilerCtx: d.CompilerCtx, buildCtx: d.BuildCtx, pkgData: d.PackageJsonData, outputTarget: d.OutputTargetDist) {
+  v.validatePackageFiles(config, outputTarget, buildCtx.diagnostics, pkgData);
+  v.validateCollection(config, outputTarget, buildCtx.diagnostics, pkgData);
+  v.validateNamespace(config, buildCtx.diagnostics);
+  v.validateTypes(config, outputTarget, buildCtx.diagnostics, pkgData);
+
+  if (!buildCtx.hasError) {
+    await generateTypesOutput(config, compilerCtx, buildCtx, pkgData, outputTarget);
+
+    const existsTypes = await v.validateTypesExist(config, compilerCtx, outputTarget, buildCtx.diagnostics, pkgData);
+    if (existsTypes) {
+      await copyStencilCoreDts(config, compilerCtx);
+    }
+
+    await v.validateModule(config, compilerCtx, outputTarget, buildCtx.diagnostics, pkgData);
+    await v.validateMain(config, compilerCtx, outputTarget, buildCtx.diagnostics, pkgData);
+    v.validateBrowser(buildCtx.diagnostics, pkgData);
+  }
+}
+
+
+async function generateTypesOutput(config: d.Config, compilerCtx: d.CompilerCtx, buildCtx: d.BuildCtx, pkgData: d.PackageJsonData, outputTarget: d.OutputTargetDist) {
+  const srcDirItems = await compilerCtx.fs.readdir(config.srcDir, { recursive: false });
+  const srcDtsFiles = srcDirItems.filter(srcItem => srcItem.isFile && isDtsFile(srcItem.absPath));
+  const distTypesDir = config.sys.path.dirname(pkgData.types);
+
+  // Copy .d.ts files from src to dist
+  // In addition, all references to @stencil/core are replaced
+  await Promise.all(srcDtsFiles.map(async srcDtsFile => {
+    const relPath = config.sys.path.relative(config.srcDir, srcDtsFile.absPath);
+    const distPath = pathJoin(config, config.rootDir, distTypesDir, relPath);
+
+    const originalDtsContent = await compilerCtx.fs.readFile(srcDtsFile.absPath);
+    const distDtsContent = updateStencilTypesImports(config, outputTarget.typesDir, distPath, originalDtsContent);
+
+    await compilerCtx.fs.writeFile(distPath, distDtsContent);
+  }));
+
+  const distPath = pathJoin(config, config.rootDir, distTypesDir);
+  await generateComponentTypes(config, compilerCtx, buildCtx, distPath);
+}
+
+
+async function readPackageJson(config: d.Config, compilerCtx: d.CompilerCtx) {
+  const pkgJsonPath = config.sys.path.join(config.rootDir, 'package.json');
+
+  let pkgJson: string;
+  try {
+    pkgJson = await compilerCtx.fs.readFile(pkgJsonPath);
+
+  } catch (e) {
+    throw new Error(`Missing "package.json" file for distribution: ${pkgJsonPath}`);
+  }
+
+  let pkgData: d.PackageJsonData;
+  try {
+    pkgData = JSON.parse(pkgJson);
+
+  } catch (e) {
+    throw new Error(`Error parsing package.json: ${pkgJsonPath}, ${e}`);
+  }
+
+  return pkgData;
+}
---
+++ src/compiler/types/stencil-types.ts
@@ -0,0 +1,46 @@
+import * as d from '../../declarations';
+import { normalizePath } from '../util';
+
+
+export function updateStencilTypesImports(config: d.Config, typesDir: string, dtsFilePath: string, dtsContent: string) {
+  const dir = config.sys.path.dirname(dtsFilePath);
+  const relPath = config.sys.path.relative(dir, typesDir);
+
+  let coreDtsPath = normalizePath(config.sys.path.join(relPath, CORE_FILENAME));
+  if (!coreDtsPath.startsWith('.')) {
+    coreDtsPath = `./${coreDtsPath}`;
+  }
+
+  if (dtsContent.includes('JSX')) {
+    dtsContent = `import '${coreDtsPath}';\n${dtsContent}`;
+  }
+
+  if (dtsContent.includes('@stencil/core')) {
+    dtsContent = dtsContent.replace(/\@stencil\/core/g, coreDtsPath);
+  }
+
+  return dtsContent;
+}
+
+
+export async function copyStencilCoreDts(config: d.Config, compilerCtx: d.CompilerCtx) {
+  const typesOutputTargets = (config.outputTargets as d.OutputTargetDist[]).filter(o => o.typesDir);
+
+  await Promise.all(typesOutputTargets.map(async typesOutputTarget => {
+    await copyStencilCoreDtsOutput(config, compilerCtx, typesOutputTarget);
+  }));
+}
+
+
+async function copyStencilCoreDtsOutput(config: d.Config, compilerCtx: d.CompilerCtx, outputTarget: d.OutputTargetDist) {
+  const srcDts = await config.sys.getClientCoreFile({
+   staticName: 'declarations/stencil.core.d.ts'
+  });
+
+  const coreDtsFilePath = normalizePath(config.sys.path.join(outputTarget.typesDir, CORE_DTS));
+  await compilerCtx.fs.writeFile(coreDtsFilePath, srcDts);
+}
+
+
+const CORE_FILENAME = `stencil.core`;
+const CORE_DTS = `${CORE_FILENAME}.d.ts`;
