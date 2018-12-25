a25600d795e96a0de03059284d19ce3086ad35ee
Adam Bradley <adambradley25@gmail.com>
2018-12-24 23:56:48 -0600

    chore(declarations): generate single file declarations for export

--- scripts/build-core-runtime.js
+++ scripts/build-core-runtime.js
@@ -4,6 +4,7 @@ const rollup = require('rollup');
 const transpile = require('./transpile');
 
 const ROOT_DIR = path.join(__dirname, '..');
+const SRC_DIR = path.join(ROOT_DIR, 'src');
 const DST_DIR = path.join(ROOT_DIR, 'dist');
 const TRANSPILED_DIR = path.join(DST_DIR, 'transpiled-runtime');
 const DIST_RUNTIME_DIR = path.join(DST_DIR, 'runtime');
@@ -33,6 +34,68 @@ async function bundleRuntimeCore() {
   fs.writeFileSync(outputFile, code);
 }
 
+
+function createPublicJavaScriptExports() {
+  const entryPath = path.join(DST_DIR, 'index.js');
+  fs.writeFileSync(entryPath,
+    `export { h } from './runtime/index.js';`
+  );
+}
+
+
+function createPublicTypeExports() {
+  const declarationsContent = [];
+  const declarationsDir = path.join(TRANSPILED_DIR, 'declarations');
+
+  const declarationFileNames = fs.readdirSync(declarationsDir).filter(f => f !== 'index.d.ts');
+
+  declarationFileNames.forEach(declarationsFile => {
+    const declarationsPath = path.join(declarationsDir, declarationsFile);
+    if (declarationsPath.endsWith('.d.ts')) {
+
+      let fileContent = fs.readFileSync(declarationsPath, 'utf8');
+      fileContent = fileContent.replace(/import \* as d (.*);/g, '');
+      fileContent = fileContent.replace(/\: d\./g, ': ');
+      fileContent = fileContent.replace(/<d\./g, '<');
+      fileContent = fileContent.replace(/\, d\./g, ', ');
+      fileContent = fileContent.replace(/=> d\./g, '=> ');
+      fileContent = fileContent.replace(/\| d\./g, '| ');
+      fileContent = fileContent.replace(/extends d\./g, 'extends ');
+      fileContent = fileContent.trim();
+
+      declarationsContent.push(fileContent);
+    }
+  });
+
+  let fileContent = declarationsContent.join('\n');
+
+  const outputDeclarationsFile = path.join(DST_DIR, 'declarations', 'index.d.ts');
+  fs.emptyDirSync(path.dirname(outputDeclarationsFile));
+  fs.writeFileSync(outputDeclarationsFile, fileContent);
+
+  const transpiledDeclarationsIndexPath = path.join(TRANSPILED_DIR, 'index.d.ts');
+  const distDeclarationsIndexPath = path.join(DST_DIR, 'index.d.ts');
+  fs.copyFileSync(transpiledDeclarationsIndexPath, distDeclarationsIndexPath);
+}
+
+
+function createRuntimeDts() {
+  const declarationSrcFiles = [
+    path.join(SRC_DIR, 'declarations', 'component-interfaces.ts'),
+    path.join(SRC_DIR, 'declarations', 'jsx.ts'),
+    path.join(SRC_DIR, 'declarations', 'vdom.ts')
+  ];
+
+  const declarationsFileContents = declarationSrcFiles
+    .map(sf => fs.readFileSync(sf, { encoding: 'utf8'} ).toString())
+    .join('\n');
+
+  const declarationsFilePath = path.join(DST_DIR, 'runtime', 'declarations', 'stencil.core.d.ts');
+  fs.emptyDirSync(path.dirname(declarationsFilePath));
+  fs.writeFileSync(declarationsFilePath, declarationsFileContents);
+}
+
+
 process.on('exit', () => {
   fs.removeSync(TRANSPILED_DIR);
   console.log(`✅ runtime: ${outputFile}`);
@@ -45,4 +108,7 @@ if (success) {
   fs.emptyDirSync(DIST_RUNTIME_DIR);
 
   bundleRuntimeCore();
+  createRuntimeDts();
+  createPublicTypeExports();
+  createPublicJavaScriptExports();
 }
