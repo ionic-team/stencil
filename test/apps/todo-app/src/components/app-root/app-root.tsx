cda0601a84157d5ecd51548dea416bf7664b21b7
Adam Bradley <adambradley25@gmail.com>
2019-01-01 10:59:09 -0600

    test(todo): update todo app

--- test/apps/todo-app/src/components/app-root/app-root.tsx
+++ test/apps/todo-app/src/components/app-root/app-root.tsx
@@ -25,15 +25,29 @@ export class AppRoot {
     this.list = [...this.list.slice(0, e.detail), ...this.list.slice(e.detail + 1)];
   }
 
+  toggleAllHandler = (e: CustomEvent) => {
+    const isChecked = (e.target as HTMLInputElement).checked;
+    this.list = this.list.map(item => {
+      item.checked = !!isChecked;
+      return item;
+    });
+  }
+
   render() {
+    const allChecked = this.list.every(item => item.checked);
     return (
       <div>
         <header class="header">
           <h1>Todos Stencil</h1>
           <todo-input onInputSubmit={this.inputSubmiHandler}></todo-input>
         </header>
-        <section class="main">
-          <input id="toggle-all" class="toggle-all" type="checkbox"/>
+        <section class="main" hidden={this.list.length === 0}>
+          <input
+            id="toggle-all"
+            onInput={this.toggleAllHandler}
+            class="toggle-all"
+            type="checkbox"
+            checked={allChecked}/>
           <label htmlFor="toggle-all"/>
           <ul class="todo-list">
             {this.list.map((item, index) => (
