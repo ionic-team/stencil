424be2e2972ac6e28ce345502ad394b1f9857c0a
Adam Bradley <adambradley25@gmail.com>
2019-02-06 22:22:22 -0600

    test(listener): add listener parse tests

--- src/compiler/transformers/test/parse-listeners.spec.ts
+++ src/compiler/transformers/test/parse-listeners.spec.ts
@@ -9,13 +9,49 @@ describe('parse listeners', () => {
       export class CmpA {
         @Listen('click')
         onClick(ev: UIEvent) {
-          console.log('hi!');
+          console.log('click!');
+        }
+      }
+    `);
+    expect(t.listeners[0].name).toBe('click');
+    expect(t.listeners[0].method).toBe('onClick');
+  });
+
+  it('target', () => {
+    const t = transpileModule(`
+      @Component({tag: 'cmp-a'})
+      export class CmpA {
+        @Listen('resize', { target: 'window' })
+        windowResize(ev: UIEvent) {
+          console.log('resize!');
+        }
+      }
+    `);
+    expect(t.listeners[0].name).toBe('resize');
+    expect(t.listeners[0].method).toBe('windowResize');
+    expect(t.listeners[0].target).toBe('window');
+  });
+
+  it('multiple listeners', () => {
+    const t = transpileModule(`
+      @Component({tag: 'cmp-a'})
+      export class CmpA {
+        @Listen('click')
+        onClick(ev: UIEvent) {
+          console.log('click!');
+        }
+        @Listen('mousedown')
+        onMouseDown(ev: UIEvent) {
+          console.log('mousedown!');
         }
       }
     `);
 
-    expect(t.listener.name).toBe('click');
-    expect(t.listener.method).toBe('onClick');
+    expect(t.listeners[0].name).toBe('click');
+    expect(t.listeners[0].method).toBe('onClick');
+
+    expect(t.listeners[1].name).toBe('mousedown');
+    expect(t.listeners[1].method).toBe('onMouseDown');
   });
 
 });
