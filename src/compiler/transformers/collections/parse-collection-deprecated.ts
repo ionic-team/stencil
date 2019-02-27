2ac8fc44aa62aba89192dacda3b7ff3ba746004b
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2019-02-27 23:17:23 +0100

    fix conflict

--- src/compiler/transformers/collections/parse-collection-deprecated.ts
+++ src/compiler/transformers/collections/parse-collection-deprecated.ts
@@ -262,12 +262,6 @@ function parseProps(cmpData: d.ComponentDataDeprecated) {
       docs: {
         text: '',
         tags: []
-      },
-      complexType: {
-        // TODO
-        original: null,
-        resolved: null,
-        references: {}
       }
     };
     return prop;
