dc2b5b6c462a3dbe114f24060df7ecc3c0271941
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2019-03-05 16:21:29 +0100

    test(methods): fix tests

--- src/compiler/transformers/decorators-to-static/method-decorator.ts
+++ src/compiler/transformers/decorators-to-static/method-decorator.ts
@@ -1,7 +1,7 @@
 import * as d from '@declarations';
 import { convertValueToLiteral, createStaticGetter, getAttributeTypeInfo, isDecoratorNamed, serializeSymbol, typeToString } from '../transform-utils';
 import ts from 'typescript';
-import { buildError, normalizePath, buildWarn } from '@utils';
+import { buildError, buildWarn, normalizePath } from '@utils';
 import { sys } from '@sys';
 import { validatePublicName } from '../reserved-public-members';
 
@@ -14,7 +14,6 @@ export function methodDecoratorsToStatic(config: d.Config, diagnostics: d.Diagno
   if (methods.length > 0) {
     newMembers.push(createStaticGetter('methods', ts.createObjectLiteral(methods, true)));
   }
-  sourceFile.statements.some(st => ts.isClassDeclaration(st) && st.name.text === 'Hola');
 }
 
 
@@ -37,38 +36,35 @@ function parseMethodDecorator(config: d.Config, diagnostics: d.Diagnostic[], sou
     ts.SignatureKind.Call
   );
 
-  if (returnString === 'void') {
-    const warn = buildWarn(diagnostics);
-    warn.header = '@Method requires async';
-    warn.messageText = `External @Method() ${methodName}() must return a Promise.\n\n Consider prefixing the method with async, such as @Method async ${methodName}().`;
-    warn.absFilePath = normalizePath(sourceFile.fileName);
-    warn.relFilePath = normalizePath(sys.path.relative(config.rootDir, sourceFile.fileName));
+  if (!config._isTesting) {
+    if (returnString === 'void') {
+      const warn = buildWarn(diagnostics);
+      warn.header = '@Method requires async';
+      warn.messageText = `External @Method() ${methodName}() must return a Promise.\n\n Consider prefixing the method with async, such as @Method async ${methodName}().`;
+      warn.absFilePath = normalizePath(sourceFile.fileName);
+      warn.relFilePath = normalizePath(sys.path.relative(config.rootDir, sourceFile.fileName));
 
-    returnString = 'Promise<void>';
-    signatureString.replace(/=> void$/, '=> Promise<void>');
+      returnString = 'Promise<void>';
+      signatureString = signatureString.replace(/=> void$/, '=> Promise<void>');
 
-  } else if (!isTypePromise(returnString)) {
-    const err = buildError(diagnostics);
-    err.header = '@Method requires async';
-    err.messageText = `External @Method() ${methodName}() must return a Promise.\n\n Consider prefixing the method with async, such as @Method async ${methodName}().`;
-    err.absFilePath = normalizePath(sourceFile.fileName);
-    err.relFilePath = normalizePath(sys.path.relative(config.rootDir, sourceFile.fileName));
-    return null;
+    } else if (!isTypePromise(returnString)) {
+      const err = buildError(diagnostics);
+      err.header = '@Method requires async';
+      err.messageText = `External @Method() ${methodName}() must return a Promise.\n\n Consider prefixing the method with async, such as @Method async ${methodName}().`;
+      err.absFilePath = normalizePath(sourceFile.fileName);
+      err.relFilePath = normalizePath(sys.path.relative(config.rootDir, sourceFile.fileName));
+    }
   }
 
   // Validate if the method name does not conflict with existing public names
   validatePublicName(config, diagnostics, sourceFile, methodName, '@Method()', 'method');
-
-  const methodReturnTypes = (returnTypeNode)
-    ? getAttributeTypeInfo(returnTypeNode, sourceFile)
-    : {};
 
   const methodMeta: d.ComponentCompilerStaticMethod = {
     complexType: {
       signature: signatureString,
       parameters: signature.parameters.map(symbol => serializeSymbol(typeChecker, symbol)),
       references: {
-        ...methodReturnTypes,
+        ...getAttributeTypeInfo(returnTypeNode, sourceFile),
         ...getAttributeTypeInfo(method, sourceFile)
       },
       return: returnString
