c9819f28705088c06e9ab962edfa5af6f5f7ae22
Adam Bradley <adambradley25@gmail.com>
2019-04-24 12:54:02 -0500

    chore(compiler): export config from compiler

--- src/compiler/index.ts
+++ src/compiler/index.ts
@@ -4,6 +4,7 @@ export { BuildContext } from './build/build-ctx';
 export { Cache } from './cache';
 export { Compiler } from './compiler';
 export { COMPILER_BUILD } from './build/compiler-build-id';
+export { Config } from '../declarations';
 export { formatComponentRuntimeMeta, formatLazyBundleRuntimeMeta } from './app-core/format-component-runtime-meta';
 export { getBuildFeatures } from './app-core/build-conditionals';
 export { transpileModule } from './transpile/transpile-module';
