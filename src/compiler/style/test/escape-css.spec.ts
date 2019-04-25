1ea49eb1034b6d57ff2582db46a0ac7e06b582a0
Adam Bradley <adambradley25@gmail.com>
2019-04-25 08:53:48 -0500

    test: fix escapeCssForJs tests

--- src/compiler/style/test/escape-css.spec.ts
+++ src/compiler/style/test/escape-css.spec.ts
@@ -8,39 +8,39 @@ describe('escapeCssForJs', () => {
 
   it(`should escape unicode characters`, () => {
     const escaped = escapeCssForJs(`p { content: '\\F113' }`);
-    expect(escaped).toBe(`p { content: '\\\\F113' }`);
+    expect(escaped).toBe(`p { content: \\'\\\\F113\\' }`);
   });
 
   it(`should octal escape sequences 0 to 7 (not 8 or 9)`, () => {
     let escaped = escapeCssForJs(`p { content: '\\0660' }`);
-    expect(escaped).toBe(`p { content: '\\\\0660' }`);
+    expect(escaped).toBe(`p { content: \\'\\\\0660\\' }`);
 
     escaped = escapeCssForJs(`p { content: '\\1660' }`);
-    expect(escaped).toBe(`p { content: '\\\\1660' }`);
+    expect(escaped).toBe(`p { content: \\'\\\\1660\\' }`);
 
     escaped = escapeCssForJs(`p { content: '\\2660' }`);
-    expect(escaped).toBe(`p { content: '\\\\2660' }`);
+    expect(escaped).toBe(`p { content: \\'\\\\2660\\' }`);
 
     escaped = escapeCssForJs(`p { content: '\\3660' }`);
-    expect(escaped).toBe(`p { content: '\\\\3660' }`);
+    expect(escaped).toBe(`p { content: \\'\\\\3660\\' }`);
 
     escaped = escapeCssForJs(`p { content: '\\4660' }`);
-    expect(escaped).toBe(`p { content: '\\\\4660' }`);
+    expect(escaped).toBe(`p { content: \\'\\\\4660\\' }`);
 
     escaped = escapeCssForJs(`p { content: '\\5660' }`);
-    expect(escaped).toBe(`p { content: '\\\\5660' }`);
+    expect(escaped).toBe(`p { content: \\'\\\\5660\\' }`);
 
     escaped = escapeCssForJs(`p { content: '\\6660' }`);
-    expect(escaped).toBe(`p { content: '\\\\6660' }`);
+    expect(escaped).toBe(`p { content: \\'\\\\6660\\' }`);
 
     escaped = escapeCssForJs(`p { content: '\\7660' }`);
-    expect(escaped).toBe(`p { content: '\\\\7660' }`);
+    expect(escaped).toBe(`p { content: \\'\\\\7660\\' }`);
 
     escaped = escapeCssForJs(`p { content: '\\8660' }`);
-    expect(escaped).toBe(`p { content: '\\8660' }`);
+    expect(escaped).toBe(`p { content: \\'\\8660\\' }`);
 
     escaped = escapeCssForJs(`p { content: '\\9660' }`);
-    expect(escaped).toBe(`p { content: '\\9660' }`);
+    expect(escaped).toBe(`p { content: \\'\\9660\\' }`);
   });
 
   it(`should escape double quotes`, () => {
