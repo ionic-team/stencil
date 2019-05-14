92acda9947671447a15a4664baee14c8b5b6ac7d
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2019-05-14 22:48:53 +0200

    fix(compiler): relax types of Diagnostic

--- src/declarations/diagnostics.ts
+++ src/declarations/diagnostics.ts
@@ -2,7 +2,7 @@
 
 export interface Diagnostic {
   level: 'error'|'warn'|'info'|'log'|'debug';
-  type: 'typescript'|'bundling'|'build'|'runtime'|'hydrate'|'css'|'config';
+  type: string; // 'typescript'|'bundling'|'build'|'runtime'|'hydrate'|'css'|'config'
   header?: string;
   language?: string;
   messageText: string;
