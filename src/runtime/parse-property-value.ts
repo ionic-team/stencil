d120f0cc12a5c78fa2458e7bab5902c3c43a3d98
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2019-05-19 19:27:22 +0200

    wip

--- src/runtime/parse-property-value.ts
+++ src/runtime/parse-property-value.ts
@@ -3,22 +3,22 @@ import { BUILD } from '@build-conditionals';
 
 
 
-export const parsePropertyValue = (propValue: any, expectedPropType: number) => {
+export const parsePropertyValue = (propValue: any, propType: number) => {
   // ensure this value is of the correct prop type
 
   if (propValue != null && !isComplexType(propValue)) {
-    if (BUILD.propBoolean && expectedPropType & MEMBER_FLAGS.Boolean) {
+    if (BUILD.propBoolean && propType & MEMBER_FLAGS.Boolean) {
       // per the HTML spec, any string value means it is a boolean true value
       // but we'll cheat here and say that the string "false" is the boolean false
       return (propValue === 'false' ? false : propValue === '' || !!propValue);
     }
 
-    if (BUILD.propNumber && expectedPropType & MEMBER_FLAGS.Number) {
+    if (BUILD.propNumber && propType & MEMBER_FLAGS.Number) {
       // force it to be a number
       return parseFloat(propValue);
     }
 
-    if (BUILD.propString && expectedPropType & MEMBER_FLAGS.String) {
+    if (BUILD.propString && propType & MEMBER_FLAGS.String) {
       // could have been passed as a number or boolean
       // but we still want it as a string
       return String(propValue);
