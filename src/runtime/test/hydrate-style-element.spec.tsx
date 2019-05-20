e67f98d9df4af4d1c3fb7179916444b8bca0d3cb
Adam Bradley <adambradley25@gmail.com>
2019-05-20 18:18:31 -0500

    test(dev-server): fix dev server tests

--- src/runtime/test/hydrate-style-element.spec.tsx
+++ src/runtime/test/hydrate-style-element.spec.tsx
@@ -20,7 +20,7 @@ describe('hydrate style element', () => {
       hydrateServerSide: true
     });
     expect(serverHydrated.root).toEqualHtml(`
-      <cmp-a s-id="1">
+      <cmp-a class="hydrated" s-id="1">
         <!--r.1-->
         <style c-id="1.0.0.0">div { color: red; }</style>
       </cmp-a>
@@ -33,7 +33,7 @@ describe('hydrate style element', () => {
     });
 
     expect(clientHydrated.root).toEqualHtml(`
-      <cmp-a>
+      <cmp-a class="hydrated">
         <!--r.1-->
         <style>div { color: red; }</style>
       </cmp-a>
