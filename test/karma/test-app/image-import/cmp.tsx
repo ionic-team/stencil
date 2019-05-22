410042b2f756a468526d97cdcfd4c84b8e0795f0
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2019-05-23 01:11:27 +0200

    wip

--- test/karma/test-app/image-import/cmp.tsx
+++ test/karma/test-app/image-import/cmp.tsx
@@ -1,16 +1,10 @@
-import { Component, State, h } from '@stencil/core';
+import { Component, h } from '@stencil/core';
 import icon from './icon.svg';
 
 @Component({
   tag: 'image-import'
 })
 export class ImageImport {
-
-  @State() isOpen = false;
-
-  testClick() {
-    this.isOpen = !this.isOpen;
-  }
 
   render() {
     return (
@@ -19,5 +13,4 @@ export class ImageImport {
       </div>
     );
   }
-
 }
