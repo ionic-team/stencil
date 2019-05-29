381ded9d08fec13c8c39fcc7015e358a5ee48e6d
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2019-05-29 18:28:23 +0200

    fix(): add polyfill for composedPath()

--- src/client/polyfills/dom.js
+++ src/client/polyfills/dom.js
@@ -52,6 +52,26 @@ if (typeof window.CustomEvent !== 'function') {
   window.CustomEvent = CustomEvent;
 }
 
+// Event.composedPath
+(function(E, d, w) {
+  if(!E.composedPath) {
+    E.composedPath = function() {
+      if (this.path) {
+        return this.path;
+      }
+    var target = this.target;
+
+    this.path = [];
+    while (target.parentNode !== null) {
+      this.path.push(target);
+      target = target.parentNode;
+    }
+    this.path.push(d, w);
+    return this.path;
+    }
+  }
+})(Event.prototype, document, window);
+
 /*!
 Element.closest and Element.matches
 https://github.com/jonathantneal/closest
