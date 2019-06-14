473588f5b8be696e1d737d7c65279031979f67a7
simonhaenisch <simonhaenisch@users.noreply.github.com>
2019-06-14 15:24:02 +0200

    format

--- src/compiler/rollup-plugins/in-memory-fs-read.ts
+++ src/compiler/rollup-plugins/in-memory-fs-read.ts
@@ -5,7 +5,6 @@ import { Plugin } from 'rollup';
 
 export function inMemoryFsRead(config: d.Config, compilerCtx: d.CompilerCtx): Plugin {
   const path = config.sys.path;
-
   return {
     name: 'inMemoryFsRead',
 
@@ -73,14 +72,12 @@ export function inMemoryFsRead(config: d.Config, compilerCtx: d.CompilerCtx): Pl
     async load(sourcePath: string) {
       if (/\.tsx?/i.test(sourcePath)) {
         this.warn({
-          message: `An import was resolved to a Tyepscript file (${sourcePath}) but Rollup treated it as Javascript. You should instead resolve to the absolute path of its transpiled Javascript equivalent (${path.resolve(
-            sourcePath.replace(/\.tsx?/i, '.js'),
-          )}).`,
+          message: `An import was resolved to a Tyepscript file (${sourcePath}) but Rollup treated it as Javascript. You should instead resolve to the absolute path of its transpiled Javascript equivalent (${path.resolve(sourcePath.replace(/\.tsx?/i, '.js'))}).`,
         });
       }
 
       return compilerCtx.fs.readFile(sourcePath);
-    },
+    }
   };
 }
 
