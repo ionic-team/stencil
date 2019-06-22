4f976dd2da7ffa091be954bf1e9c47499b52d047
Simon Hänisch <simonhaenisch@users.noreply.github.com>
2019-06-22 20:20:14 +0200

    fix(compiler): warning regex for .ts(x) imports (#1583)

--- src/compiler/rollup-plugins/in-memory-fs-read.ts
+++ src/compiler/rollup-plugins/in-memory-fs-read.ts
@@ -64,7 +64,7 @@ export function inMemoryFsRead(config: d.Config, compilerCtx: d.CompilerCtx): Pl
     },
 
     async load(sourcePath: string) {
-      if (/\.tsx?/i.test(sourcePath)) {
+      if (/\.tsx?$/i.test(sourcePath)) {
         this.warn({
           message: `An import was resolved to a Tyepscript file (${sourcePath}) but Rollup treated it as Javascript. You should instead resolve to the absolute path of its transpiled Javascript equivalent (${path.resolve(sourcePath.replace(/\.tsx?/i, '.js'))}).`,
         });
