7b4321382c3cb26dd79663e4c5bdfe42b4ea47e6
fab.sav <36304994+fabiolibre@users.noreply.github.com>
2019-07-04 12:53:48 +0100

    fix(dom.js): dom.js compliant with 'use strict;'

--- src/client/polyfills/dom.js
+++ src/client/polyfills/dom.js
@@ -40,7 +40,7 @@
 
 // Polyfill CustomEvent
 if (typeof window.CustomEvent !== 'function') {
-  function CustomEvent(event, params) {
+  CustomEvent = function(event, params) {
     params = params || { bubbles: false, cancelable: false, detail: undefined };
     var evt = document.createEvent( 'CustomEvent' );
     evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
