34c3b9066f63e219de7b50f4a1af4bed19311410
Adam Bradley <adambradley25@gmail.com>
2019-07-19 21:48:36 -0500

    wip

--- src/compiler/browser/index.ts
+++ src/compiler/browser/index.ts
@@ -8,16 +8,16 @@ export const dependencies = [
   {
     name: 'rollup',
     version: '__VERSION:ROLLUP__',
-    url: 'https://unpkg.com/rollup@__VERSION:ROLLUP__/dist/rollup.browser.js'
+    url: 'https://cdn.jsdelivr.net/npm/rollup@__VERSION:ROLLUP__/dist/rollup.browser.js'
   },
   {
     name: 'terser',
     version: '__VERSION:TERSER__',
-    url: 'https://unpkg.com/terser@__VERSION:TERSER__/dist/bundle.js'
+    url: 'https://cdn.jsdelivr.net/npm/terser@__VERSION:TERSER__/dist/bundle.js'
   },
   {
     name: 'typescript',
     version: '__VERSION:TYPESCRIPT__',
-    url: 'https://unpkg.com/typescript@__VERSION:TYPESCRIPT__/lib/typescript.js'
+    url: 'https://cdn.jsdelivr.net/npm/typescript@__VERSION:TYPESCRIPT__/lib/typescript.js'
   }
 ];
