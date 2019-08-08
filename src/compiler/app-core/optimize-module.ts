66571d07bda2796b950dc23effdc949325932c36
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2019-08-09 01:14:04 +0200

    perf(): better optimize esm build
    - Drops cssVarShim code
    - Drops supportsListenerOptions check

--- src/compiler/app-core/optimize-module.ts
+++ src/compiler/app-core/optimize-module.ts
@@ -10,6 +10,10 @@ export async function optimizeModule(config: d.Config, compilerCtx: d.CompilerCt
   if (sourceTarget !== 'es5' && isCore) {
     if (!isDebug) {
       opts.compress.passes = 3;
+      opts.compress.global_defs = {
+        supportsListenerOptions: true,
+        cssVarShim: false
+      };
       opts.compress.pure_funcs = ['getHostRef', ...opts.compress.pure_funcs];
     }
 
