134ac503b7d3918ea675e45f27156ba7ae714e09
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2019-08-09 01:15:28 +0200

    perf(www): create modulepreload for main entry-point

--- src/compiler/html/inline-esm-import.ts
+++ src/compiler/html/inline-esm-import.ts
@@ -1,6 +1,7 @@
 import * as d from '../../declarations';
 import { getAbsoluteBuildDir } from './utils';
 import { generateHashedCopy } from '../copy/hashed-copy';
+import { injectModulePreloads } from './inject-module-preloads';
 
 
 export async function optimizeEsmImport(config: d.Config, compilerCtx: d.CompilerCtx, doc: Document, outputTarget: d.OutputTargetWww) {
@@ -23,9 +24,12 @@ export async function optimizeEsmImport(config: d.Config, compilerCtx: d.Compile
   if (content.length > MAX_JS_INLINE_SIZE) {
     const hashedFile = await generateHashedCopy(config, compilerCtx, entryPath);
     if (hashedFile) {
-      script.setAttribute('src', config.sys.path.join(resourcesUrl, hashedFile));
+      const hashedPath = config.sys.path.join(resourcesUrl, hashedFile);
+      script.setAttribute('src', hashedPath);
       script.setAttribute('data-resources-url', resourcesUrl);
       script.setAttribute('data-namespace', config.fsNamespace);
+
+      injectModulePreloads(doc, [hashedPath]);
       return true;
     }
     return false;
