2072cd7dcec9633d466cd2cc6bf1e4f1de9edda5
simonhaenisch <simonhaenisch@users.noreply.github.com>
2019-08-14 19:41:33 +0200

    add test to test/end-to-end

---
+++ test/end-to-end/src/path-alias-cmp/path-alias-cmp.tsx
@@ -0,0 +1,11 @@
+import { Component, h } from '@stencil/core';
+import { sayHi } from '@path-alias';
+
+@Component({
+  tag: 'path-alias-cmp'
+})
+export class PathAliasCmp {
+  render() {
+    return <h1>{sayHi()}</h1>;
+  }
+}
