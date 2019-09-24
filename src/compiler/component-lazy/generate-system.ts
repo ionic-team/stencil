d9881d30fd3893f80c435105e868326037d60c06
Thomas Zilz <t.zilz@mofux.org>
2019-09-24 11:31:01 +0200

    fix(system): fix leaking variables (#1788)

--- src/compiler/component-lazy/generate-system.ts
+++ src/compiler/component-lazy/generate-system.ts
@@ -45,17 +45,21 @@ async function getSystemLoader(config: d.Config, corePath: string, includePolyfi
   const polyfills = includePolyfills ? await getAppBrowserCorePolyfills(config) : '';
   return `
 'use strict';
+(function () {
 ${polyfills}
 
 var doc = document;
-var allScripts = doc.querySelectorAll('script');
-var scriptElm;
-for (var x = allScripts.length - 1; x >= 0; x--) {
-  scriptElm = allScripts[x];
-  if (scriptElm.src || scriptElm.hasAttribute('data-resources-url')) {
-    break;
+var scriptElm = doc.querySelector('script[data-stencil-namespace="${config.fsNamespace}"]');
+if (!scriptElm) {
+  var allScripts = doc.querySelectorAll('script');
+  for (var x = allScripts.length - 1; x >= 0; x--) {
+    scriptElm = allScripts[x];
+    if (scriptElm.src || scriptElm.hasAttribute('data-resources-url')) {
+      break;
+    }
   }
 }
+
 var resourcesUrl = scriptElm ? scriptElm.getAttribute('data-resources-url') || scriptElm.src : '';
 var start = function() {
   var url = new URL('${corePath}', resourcesUrl);
@@ -67,5 +71,9 @@ if (win.__stencil_cssshim) {
 } else {
   start();
 }
+
+// Note: using .call(window) here because the self-executing function needs
+// to be scoped to the window object for the ES6Promise polyfill to work
+}).call(window);
 `;
 }
