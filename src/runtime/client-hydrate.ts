24b6ec3b257ad9f5f2b5f186b5c5d3b4b894b29c
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2019-10-10 12:25:25 +0200

    chore(): fix build

--- src/runtime/client-hydrate.ts
+++ src/runtime/client-hydrate.ts
@@ -3,7 +3,7 @@ import { BUILD } from '@build-conditionals';
 import { CONTENT_REF_ID, HYDRATE_CHILD_ID, HYDRATE_ID, NODE_TYPE, ORG_LOCATION_ID, SLOT_NODE_ID, TEXT_NODE_ID } from './runtime-constants';
 import { doc, plt } from '@platform';
 import { newVNode } from './vdom/h';
-import { createTime } from 'profile';
+import { createTime } from './profile';
 
 
 export const initializeClientHydrate = (hostElm: d.HostElement, tagName: string, hostId: string, hostRef: d.HostRef) => {
