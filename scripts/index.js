ac04ba5ed898afd7c62c6920da38bc1722752cb0
Adam Bradley <adambradley25@gmail.com>
2019-11-13 04:38:26 -0600

    scipts

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,17 @@
-const fs = require('fs');
+const fs = require('fs-extra');
 const path = require('path');
 
 
 function main() {
-  const rootDir = path.join(__dirname, '..');
-  const scriptsDir = path.join(rootDir, 'scripts');
+  const scriptsDir = __dirname;
+  const rootDir = path.join(scriptsDir, '..');
   const scriptsBuildDir = path.join(scriptsDir, 'build');
+  const scriptsBuildJs = path.join(scriptsBuildDir, 'build.js');
 
-  if (!fs.existsSync(scriptsBuildDir)) {
+  if (!fs.existsSync(scriptsBuildJs)) {
     // ensure we've transpiled the build scripts first
     console.log('🧩  transpiling build scripts');
+    fs.emptyDirSync(scriptsBuildDir);
     const tscPath = path.join(rootDir, 'node_modules', '.bin', 'tsc');
     const tsconfig = path.join(scriptsDir, 'tsconfig.json');
     const execa = require('execa');
@@ -19,7 +21,7 @@ function main() {
     ]);
   }
 
-  const build = require(path.join(scriptsBuildDir, 'build.js'));
+  const build = require(scriptsBuildJs);
   build.run(rootDir, process.argv.slice(2));
 }
 
