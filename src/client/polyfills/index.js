6a30a32fdc3f806f556c910daa81d5493f11befb
Adam Bradley <adambradley25@gmail.com>
2019-12-13 16:16:00 -0600

    fix(ssr): check window ref

--- src/client/polyfills/index.js
+++ src/client/polyfills/index.js
@@ -1,32 +1,35 @@
 export function applyPolyfills() {
-  var win = window;
+  if (typeof window !== 'undefined') {
+    var win = window;
 
-  var promises = [];
+    var promises = [];
 
-  if (!win.customElements || (win.Element && (!win.Element.prototype.closest || !win.Element.prototype.matches || !win.Element.prototype.remove))) {
-    promises.push(import('./dom.js'));
-  }
-
-  function checkIfURLIsSupported() {
-    try {
-      var u = new URL('b', 'http://a');
-      u.pathname = 'c%20d';
-      return (u.href === 'http://a/c%20d') && u.searchParams;
-    } catch(e) {
-      return false;
+    if (!win.customElements || (win.Element && (!win.Element.prototype.closest || !win.Element.prototype.matches || !win.Element.prototype.remove))) {
+      promises.push(import('./dom.js'));
     }
-  }
 
-  if (
-    'function' !== typeof Object.assign || !Object.entries ||
-    !Array.prototype.find || !Array.prototype.includes ||
-    !String.prototype.startsWith || !String.prototype.endsWith ||
-    (win.NodeList && !win.NodeList.prototype.forEach) ||
-    !win.fetch ||
-    !checkIfURLIsSupported() ||
-    typeof WeakMap == 'undefined'
-  ) {
-    promises.push(import('./core-js.js'));
+    function checkIfURLIsSupported() {
+      try {
+        var u = new URL('b', 'http://a');
+        u.pathname = 'c%20d';
+        return (u.href === 'http://a/c%20d') && u.searchParams;
+      } catch (e) {
+        return false;
+      }
+    }
+
+    if (
+      'function' !== typeof Object.assign || !Object.entries ||
+      !Array.prototype.find || !Array.prototype.includes ||
+      !String.prototype.startsWith || !String.prototype.endsWith ||
+      (win.NodeList && !win.NodeList.prototype.forEach) ||
+      !win.fetch ||
+      !checkIfURLIsSupported() ||
+      typeof WeakMap == 'undefined'
+    ) {
+      promises.push(import('./core-js.js'));
+    }
+    return Promise.all(promises);
   }
-  return Promise.all(promises);
+  return Promise.resolve();
 }
