d8e3b14a1411f50708746cb8e9cf49c0d4c43263
Manu MA <manu.mtza@gmail.com>
2019-12-17 16:09:25 +0100

    fix(runtime): cloneNode for non-shadow-dom components (#2052)
    fixes #1070
    fixes #1948

---
+++ test/karma/test-app/dom-reattach-clone/cmp-deep-slot.tsx
@@ -0,0 +1,19 @@
+import { Component, h } from '@stencil/core';
+
+@Component({
+  tag: 'dom-reattach-clone-deep-slot'
+})
+export class DomReattachCloneDeep {
+  render() {
+    return (
+      <div class='wrapper'>
+        <span class="component-mark-up">Component mark-up</span>
+        <div>
+          <section>
+            <slot></slot>
+          </section>
+        </div>
+      </div>
+    )
+  }
+}
---
+++ test/karma/test-app/dom-reattach-clone/cmp.tsx
@@ -0,0 +1,15 @@
+import { Component, h } from '@stencil/core';
+
+@Component({
+  tag: 'dom-reattach-clone'
+})
+export class DomReattachClone {
+  render() {
+    return (
+      <div class='wrapper'>
+        <span class="component-mark-up">Component mark-up</span>
+        <slot></slot>
+      </div>
+    )
+  }
+}
