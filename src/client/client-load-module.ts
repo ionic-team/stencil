d0eeec0fd4979f50b3d21e0ab9e5b21a856c2a3d
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2019-12-19 15:05:58 +0100

    fix(runtime): better error for unknown mode

--- src/client/client-load-module.ts
+++ src/client/client-load-module.ts
@@ -1,6 +1,6 @@
 import * as d from '../declarations';
 import { BUILD } from '@app-data';
-import { consoleError } from './client-log';
+import { consoleError, consoleDevError } from './client-log';
 
 export const cmpModules = /*@__PURE__*/new Map<string, {[exportName: string]: d.ComponentConstructor}>();
 
@@ -10,6 +10,10 @@ export const loadModule = (cmpMeta: d.ComponentRuntimeMeta, hostRef: d.HostRef, 
   const bundleId = ((BUILD.mode && typeof cmpMeta.$lazyBundleIds$ !== 'string')
     ? cmpMeta.$lazyBundleIds$[hostRef.$modeName$]
     : cmpMeta.$lazyBundleIds$) as string;
+  if (BUILD.isDev && typeof bundleId !== 'string') {
+    consoleDevError(`Trying to lazyly load component <${cmpMeta.$tagName$}> with style mode "${hostRef.$modeName$}", but it does not exist.`);
+    return undefined;
+  }
   const module = !BUILD.hotModuleReplacement ? cmpModules.get(bundleId) : false;
   if (module) {
     return module[exportName];
