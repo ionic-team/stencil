92a6015a77aff622ec4c43578e6f4623136b9045
Adam Bradley <adambradley25@gmail.com>
2020-01-07 23:31:49 -0600

    feat(hydrate): always build hydrate if it's an output

--- src/compiler_next/output-targets/dist-hydrate-script/index.ts
+++ src/compiler_next/output-targets/dist-hydrate-script/index.ts
@@ -4,18 +4,12 @@ import { isOutputTargetHydrate } from '../../../compiler/output-targets/output-u
 
 
 export const outputHydrateScript = async (config: d.Config, compilerCtx: d.CompilerCtx, buildCtx: d.BuildCtx) => {
-  if (config.devMode) {
-    return;
-  }
-
   const hydrateOutputTargets = config.outputTargets.filter(isOutputTargetHydrate);
-  if (hydrateOutputTargets.length === 0) {
-    return;
+  if (hydrateOutputTargets.length > 0) {
+    const timespan = buildCtx.createTimeSpan(`generate hydrate app started`);
+
+    await generateHydrateApp(config, compilerCtx, buildCtx, hydrateOutputTargets);
+
+    timespan.finish(`generate hydrate app finished`);
   }
-
-  const timespan = buildCtx.createTimeSpan(`generate hydrate app started`);
-
-  await generateHydrateApp(config, compilerCtx, buildCtx, hydrateOutputTargets);
-
-  timespan.finish(`generate hydrate app finished`);
 };
