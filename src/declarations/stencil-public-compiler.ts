32c456e292e0a5b6b9e39ea3a512e2af7e8e9852
Esben Nepper <en@mapspeople.com>
2020-01-17 14:14:08 +0100

    Added dependencies property to OutputTargetDocsReadme in stencil-public-compiler.ts

--- src/declarations/stencil-public-compiler.ts
+++ src/declarations/stencil-public-compiler.ts
@@ -1152,8 +1152,8 @@ export interface OutputTargetDocsVscode extends OutputTargetBase {
 
 export interface OutputTargetDocsReadme extends OutputTargetBase {
   type: 'docs-readme' | 'docs';
-
   dir?: string;
+  dependencies?: boolean;
   footer?: string;
   strict?: boolean;
 }
