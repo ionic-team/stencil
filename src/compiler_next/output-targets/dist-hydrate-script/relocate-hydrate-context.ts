d65b0cc93a20763cd5154715a4a5f4fbab9fa66f
Adam Bradley <adambradley25@gmail.com>
2020-02-08 01:19:52 -0600

    fix(next): fix hydrate build w/ global script, w/out Context

--- src/compiler_next/output-targets/dist-hydrate-script/relocate-hydrate-context.ts
+++ src/compiler_next/output-targets/dist-hydrate-script/relocate-hydrate-context.ts
@@ -13,12 +13,12 @@ export const relocateHydrateContextConst = (config: d.Config, compilerCtx: d.Com
   const globalScripts = getGlobalScriptData(config, compilerCtx);
   if (globalScripts.length > 0) {
     const startCode = code.indexOf('/*hydrate context start*/');
-    const endCode = code.indexOf('/*hydrate context end*/') + '/*hydrate context end*/'.length;
-
-    const hydrateContextCode = code.substring(startCode, endCode);
-    code = code.replace(hydrateContextCode, '');
-
-    return code.replace(HYDRATE_APP_CLOSURE_START, HYDRATE_APP_CLOSURE_START + '\n  ' + hydrateContextCode);
+    if (startCode > -1) {
+      const endCode = code.indexOf('/*hydrate context end*/') + '/*hydrate context end*/'.length;
+      const hydrateContextCode = code.substring(startCode, endCode);
+      code = code.replace(hydrateContextCode, '');
+      return code.replace(HYDRATE_APP_CLOSURE_START, HYDRATE_APP_CLOSURE_START + '\n  ' + hydrateContextCode);
+    }
   }
   return code;
 };
