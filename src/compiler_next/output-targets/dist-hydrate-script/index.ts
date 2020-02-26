01c70f61820efed3c5c7139a7616e1b7f19c8032
Adam Bradley <adambradley25@gmail.com>
2020-02-25 21:47:42 -0600

    chore(build): skip hydrate app output for dev watch

--- src/compiler_next/output-targets/dist-hydrate-script/index.ts
+++ src/compiler_next/output-targets/dist-hydrate-script/index.ts
@@ -6,6 +6,11 @@ import { isOutputTargetHydrate } from '../../../compiler/output-targets/output-u
 export const outputHydrateScript = async (config: d.Config, compilerCtx: d.CompilerCtx, buildCtx: d.BuildCtx) => {
   const hydrateOutputTargets = config.outputTargets.filter(isOutputTargetHydrate);
   if (hydrateOutputTargets.length > 0) {
+    if (config.devMode && config.watch) {
+      buildCtx.debug(`hydrate app output skipped during dev watch builds`);
+      return;
+    }
+
     const timespan = buildCtx.createTimeSpan(`generate hydrate app started`);
 
     await generateHydrateApp(config, compilerCtx, buildCtx, hydrateOutputTargets);
