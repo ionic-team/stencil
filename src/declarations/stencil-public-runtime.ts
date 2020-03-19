0f71c6c590624bbfe64c6038166daa0c90f74123
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2020-03-19 23:04:13 +0100

    fix(vdom): functional components always get object as props

--- src/declarations/stencil-public-runtime.ts
+++ src/declarations/stencil-public-runtime.ts
@@ -461,7 +461,7 @@ export interface FunctionalUtilities {
 }
 
 export interface FunctionalComponent<T = {}> {
-  (props: T | undefined, children: VNode[], utils: FunctionalUtilities): VNode | VNode[];
+  (props: T, children: VNode[], utils: FunctionalUtilities): VNode | VNode[];
 }
 
 export interface ChildNode {
