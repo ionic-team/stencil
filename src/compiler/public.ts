0de89ba922d34ecf74b004cead7af35a90e4e690
Adam Bradley <adamdbradley@users.noreply.github.com>
2020-05-08 17:46:20 -0500

    docs(compiler): add public compiler JSDocs

--- src/compiler/public.ts
+++ src/compiler/public.ts
@@ -19,7 +19,7 @@ import {
 
 /**
  * The `compile()` function inputs source code as a string, with various options
- * within the second argument. The function returns a Promise of the compile results, including
+ * within the second argument. The function returns a `Promise` of the compile results, including
  * diagnostics and the compiled code. The `compile()` function does not handle any bundling,
  * minifying, or precompiling any CSS preprocessing like Sass or Less. The `compileSync()`
  * equivalent is available so the same function it can be called synchronously, however,
@@ -39,8 +39,29 @@ export declare const compile: (code: string, opts?: CompileOptions) => Promise<C
  */
 export declare const compileSync: (code: string, opts?: CompileOptions) => CompileResults;
 
+/**
+ * The compiler is the utility that brings together many tools to build optimized components, such as a
+ * transpiler, bundler and minifier. When using the CLI, the `stencil build` command uses the compiler for
+ * the various builds, such as a production build, or watch mode during development. If only one file should
+ * be compiled then the `compiler()` function should be used instead.
+ *
+ * Given a Stencil config, this method asynchronously returns a `Compiler` instance. The config provided
+ * should already be created using the `loadConfig({...})` method.
+ */
 export declare const createCompiler: (config: Config) => Promise<Compiler>;
+
+/**
+ * The compiler uses a `CompilerSystem` instance to access any file system reads and writes. When used
+ * from the CLI, the CLI will provide its own system based on NodeJS. This method provide a compiler
+ * system is in-memory only and independent of any platform.
+ */
 export declare const createSystem: () => CompilerSystem;
+
+/**
+ * The `dependencies` array is only informational and provided to state which versions of dependencies
+ * the compiler was built and works with. For example, the version of TypeScript, Rollup and Terser used
+ * for this version of Stencil are listed here.
+ */
 export declare const dependencies: CompilerDependency[];
 export interface CompilerDependency {
   name: string;
@@ -48,10 +69,34 @@ export interface CompilerDependency {
   main: string;
   resources?: string[];
 }
-export declare const optimizeCss: (cssInput?: OptimizeCssInput) => Promise<OptimizeCssOutput>;
-export declare const optimizeJs: (jsInput?: OptimizeJsInput) => Promise<OptimizeJsOutput>;
-export declare const path: PlatformPath;
+
+/**
+ * The `loadConfig(init)` method is used to take raw config information and transform it into a
+ * usable config object for the compiler and dev-server. The `init` argument should be given
+ * an already created system and logger which can also be used by the compiler.
+ */
 export declare const loadConfig: (init?: LoadConfigInit) => Promise<LoadConfigResults>;
+
+/**
+ * Utility function used by the compiler to optimize CSS.
+ */
+export declare const optimizeCss: (cssInput?: OptimizeCssInput) => Promise<OptimizeCssOutput>;
+
+/**
+ * Utility function used by the compiler to optimize JavaScript. Knowing the JavaScript target
+ * will further apply minification optimizations beyond usual minification.
+ */
+export declare const optimizeJs: (jsInput?: OptimizeJsInput) => Promise<OptimizeJsOutput>;
+
+/**
+ * Utility of the `path` API providied by NodeJS, but capable of running in any environment.
+ * This `path` API is only the POSIX version: https://nodejs.org/api/path.html
+ */
+export declare const path: PlatformPath;
+
+/**
+ * Current version of `@stencil/core`.
+ */
 export declare const version: string;
 
 export {
