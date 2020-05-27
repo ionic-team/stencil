6c5b2695cc8d56f7f9f8667257a0fa1ae8004e0e
Adam Bradley <adamdbradley@users.noreply.github.com>
2020-05-27 11:41:54 -0500

    svg?format=dataurl

--- src/compiler/bundle/image-plugin.ts
+++ src/compiler/bundle/image-plugin.ts
@@ -11,26 +11,40 @@ export const imagePlugin = (config: d.Config, buildCtx: d.BuildCtx): Plugin => {
   return {
     name: 'image',
 
-    async transform(code, id) {
-      if (/\0/.test(id)) {
+    async transform(code, importPath) {
+      if (/\0/.test(importPath)) {
         return null;
       }
 
-      const mime = svgMimeTypes[extname(id)];
+      const pathParts = importPath.split('?');
+      const filePath = normalizePath(pathParts[0]);
+      const mime = svgMimeTypes[extname(filePath)];
       if (!mime) {
         return null;
       }
 
       try {
-        const varName = createJsVarName(basename(id));
-        const base64 = config.sys.encodeToBase64(code);
+        const varName = createJsVarName(basename(filePath));
 
-        if (config.devMode && base64.length > SVG_MAX_IMAGE_SIZE) {
-          const warn = buildWarn(buildCtx.diagnostics);
-          warn.messageText = 'Importing big images will bloat your bundle, please use assets instead.';
-          warn.absFilePath = normalizePath(id);
+        if (pathParts.length > 1) {
+          const urlParams = new URLSearchParams(pathParts[1]);
+          const format = urlParams.get('format');
+          if (format === 'dataurl') {
+            // ?format=dataurl
+            const base64 = config.sys.encodeToBase64(code);
+
+            if (config.devMode && base64.length > SVG_MAX_IMAGE_SIZE) {
+              const warn = buildWarn(buildCtx.diagnostics);
+              warn.messageText = 'Importing big images will bloat your bundle, please use assets instead.';
+              warn.absFilePath = filePath;
+            }
+
+            return `const ${varName} = 'data:${mime};base64,${base64}';export default ${varName};`;
+          }
         }
-        return `const ${varName} = 'data:${mime};base64,${base64}';export default ${varName};`;
+
+        // plain text as the default
+        return `const ${varName} = '${code}';export default ${varName};`;
       } catch (e) {
         catchError(buildCtx.diagnostics, e);
       }
