6b012a764f5bc8e21372796042970b1f3d77fbe8
Adam Bradley <adamdbradley@users.noreply.github.com>
2020-06-03 10:04:25 -0500

    chore: update node version requirement message
    - Minimum of Node v10.13.0
    - Recommend Node v12.17.0 or greater

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -83,25 +83,6 @@ function getCompilerExecutingPath() {
 }
 
 function setupNodeProcess(prcs: NodeJS.Process, logger: Logger) {
-  try {
-    const v = prcs.version.substring(1).split('.');
-    const major = parseInt(v[0], 10);
-    const minor = parseInt(v[1], 10);
-    if (major < 8 || (major === 8 && minor < 9)) {
-      logger.error(
-        '\nYour current version of Node is ' + prcs.version + " but Stencil needs at least v8.9. It's recommended to install latest Node (https://github.com/nodejs/Release).\n",
-      );
-      exit(1);
-    }
-    if (major < 10 || (major === 10 && minor < 13)) {
-      logger.warn(
-        '\nYour current version of Node is ' +
-          prcs.version +
-          ', however the recommendation is a minimum of Node LTS (https://github.com/nodejs/Release). Note that future versions of Stencil will eventually remove support for non-LTS Node versions.\n',
-      );
-    }
-  } catch (e) {}
-
   prcs.on(`unhandledRejection`, (e: any) => {
     if (!shouldIgnoreError(e)) {
       let msg = 'unhandledRejection';
