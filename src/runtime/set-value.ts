54965b9a87dd1a23c2e609c525c0c0a830459ef5
Adam Bradley <adamdbradley@users.noreply.github.com>
2020-06-04 14:03:41 -0500

    chore: clean up build statements

--- src/runtime/set-value.ts
+++ src/runtime/set-value.ts
@@ -48,7 +48,7 @@ export const setValue = (ref: d.RuntimeRef, propName: string, newVal: any, cmpMe
     if (!BUILD.lazyLoad || instance) {
       // get an array of method names of watch functions to call
       if (BUILD.watchCallback && cmpMeta.$watchers$) {
-        if (!BUILD.lazyLoad || (BUILD.lazyLoad && flags & HOST_FLAGS.isWatchReady)) {
+        if (!BUILD.lazyLoad || flags & HOST_FLAGS.isWatchReady) {
           const watchMethods = cmpMeta.$watchers$[propName];
 
           if (watchMethods) {
