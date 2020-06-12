4c3c8fbb9b37394a8b7a98579dec14abeaa88751
peterpeterparker <david.dalbusco@outlook.com>
2020-06-12 14:52:17 +0200

    feat(#2366): dynamic child component index

--- src/runtime/vdom/vdom-annotations.ts
+++ src/runtime/vdom/vdom-annotations.ts
@@ -127,15 +127,20 @@ const insertParentVNodeAnnotations = (hostElm: d.HostElement, vnode: d.VNode) =>
     return;
   }
 
-  const comment = Array.from(parent.childNodes).find(node => {
+  const parentChildNodes: ChildNode[] = Array.from(parent.childNodes);
+
+  const comment: d.RenderNode | undefined = parentChildNodes.find(node => {
     return node.nodeType === NODE_TYPE.CommentNode && (node as d.RenderNode)['s-sr'];
-  });
+  }) as d.RenderNode | undefined;
 
   if (!comment) {
     return;
   }
 
-  (vnode.$elm$ as d.RenderNode).setAttribute(HYDRATE_CHILD_ID, `${(comment as d.RenderNode)['s-host-id']}.${(comment as d.RenderNode)['s-node-id']}.0.1`);
+  // We have got at least the element plus a comment. First element should be indexed with 1, second element with 2, etc.
+  const index: number = parentChildNodes.indexOf(hostElm) - 1;
+
+  (vnode.$elm$ as d.RenderNode).setAttribute(HYDRATE_CHILD_ID, `${comment['s-host-id']}.${comment['s-node-id']}.0.${index}`);
 };
 
 const insertChildVNodeAnnotations = (doc: Document, vnodeChild: d.VNode, cmpData: CmpData, hostId: number, depth: number, index: number) => {
