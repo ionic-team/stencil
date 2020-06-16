438c6b310f24d93dc82aba1614e0f9d91dbccd7a
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2020-06-16 23:52:48 +0200

    perf(): reenable const class optimization
    https://github.com/microsoft/TypeScript/issues/38528

--- src/compiler/transformers/update-component-class.ts
+++ src/compiler/transformers/update-component-class.ts
@@ -20,6 +20,33 @@ export const updateComponentClass = (
     }
   }
 
-  // ESM with export
-  return ts.updateClassDeclaration(classNode, classNode.decorators, classModifiers, classNode.name, classNode.typeParameters, heritageClauses, members);
+  return createConstClass(transformOpts, classNode, heritageClauses, members);
+};
+
+const createConstClass = (
+  transformOpts: d.TransformOptions,
+  classNode: ts.ClassDeclaration,
+  heritageClauses: ts.HeritageClause[] | ts.NodeArray<ts.HeritageClause>,
+  members: ts.ClassElement[],
+) => {
+  const className = classNode.name;
+
+  const classModifiers = (Array.isArray(classNode.modifiers) ? classNode.modifiers : []).filter(m => {
+    // remove the export
+    return m.kind !== ts.SyntaxKind.ExportKeyword;
+  });
+
+  const constModifiers: ts.Modifier[] = [];
+
+  if (transformOpts.componentExport !== 'customelement') {
+    constModifiers.push(ts.createModifier(ts.SyntaxKind.ExportKeyword));
+  }
+
+  return ts.createVariableStatement(
+    constModifiers,
+    ts.createVariableDeclarationList(
+      [ts.createVariableDeclaration(className, undefined, ts.createClassExpression(classModifiers, undefined, classNode.typeParameters, heritageClauses, members))],
+      ts.NodeFlags.Const,
+    ),
+  );
 };
