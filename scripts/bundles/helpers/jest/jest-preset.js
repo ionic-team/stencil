bc7d4d197cffb2eeb74ab1dd75a27042ede340a0
Adam Bradley <adamdbradley@users.noreply.github.com>
2020-07-23 20:17:23 -0500

    revert(jest-preset): do not transform .js extension by default
    Reverting https://github.com/ionic-team/stencil/commit/781180d670e2729ae121d6d84fe479d4b8d4432d because for larger projects it makes a significant performance difference to also transform all .js files. This is only setting stencil's jest preset default, users should still be able to configure the jest transform: https://jestjs.io/docs/en/configuration

--- scripts/bundles/helpers/jest/jest-preset.js
+++ scripts/bundles/helpers/jest/jest-preset.js
@@ -27,6 +27,6 @@ module.exports = {
   maxConcurrency: 1,
   testRegex: '(/__tests__/.*|\\.?(test|spec))\\.' + moduleExtensionRegexp + '$',
   transform: {
-    ['^.+\\.' + moduleExtensionRegexp + '$']: path.join(testingDir, 'jest-preprocessor.js'),
+    '^.+\\.(ts|tsx|jsx|css)$': path.join(testingDir, 'jest-preprocessor.js'),
   },
 };
