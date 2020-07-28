987575221f667fde804a2f397356a3b826e5207c
Adam Bradley <adamdbradley@users.noreply.github.com>
2020-07-28 09:44:08 -0500

    chore(compiler): update compiler intro

--- scripts/bundles/helpers/compiler-cjs-intro.js
+++ scripts/bundles/helpers/compiler-cjs-intro.js
@@ -11,30 +11,25 @@ if (typeof globalThis === 'undefined') {
   }
 }
 
-(gbl => {
-  if (!gbl.Buffer) {
-    gbl.Buffer = {};
-  }
-
-  const process = gbl.process = (gbl.process || {});
-  if (!process.argv) {
-    process.argv = [''];
-  }
-  let cwd = '/';
-  if (!process.cwd) {
-    process.cwd = () => cwd;
-  }
-  if (!process.chdir) {
-    process.chdir = (v) => cwd = v;
-  }
-  if (!process.nextTick) {
-    const resolved = Promise.resolve();
-    process.nextTick = (cb) => resolved.then(cb);
-  }
-  if (!process.platform) {
-    process.platform = 'stencil';
-  }
-  if (!process.version) {
-    process.version = 'v12.0.0';
-  }
-})(globalThis);
+const Buffer = globalThis.Buffer || {};
+const process = globalThis.process || {};
+if (!process.argv) {
+  process.argv = [''];
+}
+let __cwd = '/';
+if (!process.cwd) {
+  process.cwd = () => __cwd;
+}
+if (!process.chdir) {
+  process.chdir = (v) => __cwd = v;
+}
+if (!process.nextTick) {
+  const resolved = Promise.resolve();
+  process.nextTick = (cb) => resolved.then(cb);
+}
+if (!process.platform) {
+  process.platform = 'stencil';
+}
+if (!process.version) {
+  process.version = 'v12.0.0';
+}
