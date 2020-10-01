69108759fd67a470a3481a9205ba27976a2bd0fa
Adam Bradley <adamdbradley@users.noreply.github.com>
2020-10-01 09:44:09 -0500

    feat(puppeteer): update to puppeteer 5.3.1

--- src/sys/node/node-sys.ts
+++ src/sys/node/node-sys.ts
@@ -546,7 +546,7 @@ export function createNodeSys(c: { process?: any } = {}) {
     'jest': ['24.9.0', '26.4.2'],
     'jest-cli': ['24.9.0', '26.4.2'],
     'pixelmatch': ['4.0.2', '4.0.2'],
-    'puppeteer': ['1.19.0', '5.3.0'],
+    'puppeteer': ['1.19.0', '5.3.1'],
     'puppeteer-core': ['1.19.0', '5.2.1'],
     'workbox-build': ['4.3.1', '4.3.1'],
   });
