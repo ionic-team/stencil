71990846ba39d3a604cca50ce5f58e8a6e2aaa00
Camden Narzt <heartwork@narzt.cam>
2020-10-01 18:02:29 -0600

    chore(test): add test for MockLocation behaviour

---
+++ src/mock-doc/test/location.spec.ts
@@ -0,0 +1,95 @@
+import { MockWindow } from '../window';
+
+describe('location.href', () => {
+  let win: MockWindow;
+  beforeEach(() => {
+    win = new MockWindow(`
+      <html>
+        <head>
+        </head>
+      </head>
+      `);
+    win.location.href = "http://stencil:secret@stenciljs.com:3000/path/to/page?var=var#hash"
+  });
+
+  it('window.location.href', () => {
+    expect(win.location.href).toBe("http://stencil:secret@stenciljs.com:3000/path/to/page?var=var#hash");
+  });
+  it('window.location.protocol', () => {
+    expect(win.location.protocol).toBe("http:");
+  });
+  it('window.location.host', () => {
+    expect(win.location.host).toBe("stenciljs.com:3000");
+  });
+  it('window.location.hostname', () => {
+    expect(win.location.hostname).toBe("stenciljs.com");
+  });
+  it('window.location.port', () => {
+    expect(win.location.port).toBe("3000");
+  });
+  it('window.location.pathname', () => {
+    expect(win.location.pathname).toBe("/path/to/page");
+  });
+  it('window.location.search', () => {
+    expect(win.location.search).toBe("?var=var");
+  });
+  it('window.location.hash', () => {
+    expect(win.location.hash).toBe("#hash");
+  });
+  it('window.location.username', () => {
+    expect(win.location.username).toBe("stencil");
+  });
+  it('window.location.password', () => {
+    expect(win.location.password).toBe("secret");
+  });
+  it('window.location.origin', () => {
+    expect(win.location.origin).toBe("http://stenciljs.com:3000");
+  });
+});
+
+describe('location', () => {
+  let win: MockWindow;
+  beforeEach(() => {
+    win = new MockWindow(`
+      <html>
+        <head>
+        </head>
+      </head>
+      `);
+    win.location = "http://stencil:secret@stenciljs.com:3000/path/to/page?var=var#hash"
+  });
+
+  it('window.location.href', () => {
+    expect(win.location.href).toBe("http://stencil:secret@stenciljs.com:3000/path/to/page?var=var#hash");
+  });
+  it('window.location.protocol', () => {
+    expect(win.location.protocol).toBe("http:");
+  });
+  it('window.location.host', () => {
+    expect(win.location.host).toBe("stenciljs.com:3000");
+  });
+  it('window.location.hostname', () => {
+    expect(win.location.hostname).toBe("stenciljs.com");
+  });
+  it('window.location.port', () => {
+    expect(win.location.port).toBe("3000");
+  });
+  it('window.location.pathname', () => {
+    expect(win.location.pathname).toBe("/path/to/page");
+  });
+  it('window.location.search', () => {
+    expect(win.location.search).toBe("?var=var");
+  });
+  it('window.location.hash', () => {
+    expect(win.location.hash).toBe("#hash");
+  });
+  it('window.location.username', () => {
+    expect(win.location.username).toBe("stencil");
+  });
+  it('window.location.password', () => {
+    expect(win.location.password).toBe("secret");
+  });
+  it('window.location.origin', () => {
+    expect(win.location.origin).toBe("http://stenciljs.com:3000");
+  });
+});
