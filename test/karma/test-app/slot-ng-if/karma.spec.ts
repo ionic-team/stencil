61a20605405ac07c5265f480590d48ee5394abd3
Justus Romijn <Justus.Romijn@rabobank.nl>
2020-10-14 16:30:46 +0200

    chore: add karma testcase for cloneNodeFix

---
+++ test/karma/test-app/slot-ng-if/karma.spec.ts
@@ -0,0 +1,17 @@
+import { setupDomTests } from '../util';
+
+describe('slot-ng-if', function() {
+  const { setupDom, tearDownDom } = setupDomTests(document);
+  let app: HTMLElement;
+
+  beforeEach(async () => {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
+    app = await setupDom('/slot-ng-if/index.html');
+  });
+  afterEach(tearDownDom);
+
+  it('renders bound values in slots within ng-if context', async () => {
+    const root = app.querySelector('slot-ng-if');
+    expect(root.textContent).toBe('Angular Bound Label');
+  });
+});
