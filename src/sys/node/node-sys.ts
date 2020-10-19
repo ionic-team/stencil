241c23c5ae38f6b973465dd7e0ca93f07329d5d0
Adam Bradley <adamdbradley@users.noreply.github.com>
2020-10-18 22:02:16 -0500

    feat(rollup): update to rollup 2.32.0 and jest 26.5.3
    Co-Authored-By: Justus Romijn <1901845+justusromijn@users.noreply.github.com>

--- src/sys/node/node-sys.ts
+++ src/sys/node/node-sys.ts
@@ -550,8 +550,8 @@ export function createNodeSys(c: { process?: any } = {}) {
     // [minimumVersion, recommendedVersion]
     '@types/jest': ['24.9.1', '26.0.14'],
     '@types/puppeteer': ['1.19.0', '3.0.1'],
-    'jest': ['24.9.0', '26.4.2'],
-    'jest-cli': ['24.9.0', '26.4.2'],
+    'jest': ['24.9.0', '26.5.3'],
+    'jest-cli': ['24.9.0', '26.5.3'],
     'pixelmatch': ['4.0.2', '4.0.2'],
     'puppeteer': ['1.19.0', '5.3.1'],
     'puppeteer-core': ['1.19.0', '5.2.1'],
