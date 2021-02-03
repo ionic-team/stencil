d764110c64b7f03da28075a1c72a5d48ebe9234d
Pavlo Mykhalov <git@mykhalov.com>
2021-02-03 14:38:58 +0200

    Sort selectors in generated style
    👋
    
    This sorts selectors by component name instead of component order in the dependency tree. The change won't affect the appearance of Stencil components, but will make it easier for external tools to generate a hash of injected style, which should help setting up CSP.
    
    Before:
    ```
    c-component,a-component,b-component{visibility:hidden}.hydrated{visibility:inherit} /* sha256-B/TVz4hlE0LPqYktS5z2XygEqJWw6XADCoGEXeUKoJs= */
    ```
    
    After:
    ```
    a-component,b-component,c-component{visibility:hidden}.hydrated{visibility:inherit} /* sha256-RbJGguhbDA2O26upcSoCXOydA0wWLSg/SI1HGTOG81k= */
    ```

--- src/runtime/bootstrap-lazy.ts
+++ src/runtime/bootstrap-lazy.ts
@@ -153,7 +153,7 @@ export const bootstrapLazy = (lazyBundles: d.LazyBundlesRuntimeData, options: d.
   );
 
   if (BUILD.hydratedClass || BUILD.hydratedAttribute) {
-    visibilityStyle.innerHTML = cmpTags + HYDRATED_CSS;
+    visibilityStyle.innerHTML = cmpTags.sort() + HYDRATED_CSS;
     visibilityStyle.setAttribute('data-styles', '');
     head.insertBefore(visibilityStyle, metaCharset ? metaCharset.nextSibling : head.firstChild);
   }
