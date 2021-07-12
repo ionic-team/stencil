d0898cf1218462b0659011a72f076b0819b017e4
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2021-07-12 16:43:32 -0400

    TEMP: break a test

--- test/karma/test-app/prerender-test/karma.spec.ts
+++ test/karma/test-app/prerender-test/karma.spec.ts
@@ -52,7 +52,7 @@ describe('prerender', () => {
     const shadowStyle = getComputedStyle(shadow.shadowRoot.querySelector('article'));
     expect(shadowStyle.color).toBe('rgb(0, 155, 0)');
 
-    const blueText = shadow.shadowRoot.querySelector('cmp-text-blue');
+    const blueText = shadow.shadowRoot.querySelector('temp-revert-me');
     const blueTextStyle = getComputedStyle(blueText.querySelector('text-blue'));
     expect(blueTextStyle.color).toBe('rgb(0, 0, 255)');
 
