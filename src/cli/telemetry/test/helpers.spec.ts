13994a6b22b03571a01b18a2d7e0a208fc8860bc
William M. Riley <splitinfinities@users.noreply.github.com>
2021-07-21 09:21:36 -0500

    Update src/cli/telemetry/test/helpers.spec.ts
    Co-authored-by: Ryan Waskiewicz <ryanwaskiewicz@gmail.com>

--- src/cli/telemetry/test/helpers.spec.ts
+++ src/cli/telemetry/test/helpers.spec.ts
@@ -2,7 +2,7 @@ import { isInteractive, TERMINAL_INFO, tryFn, uuidv4 } from '../helpers';
 
 describe('uuidv4', () => {
 
-	it('should output a UUID', async () => {
+	it('outputs a UUID', async () => {
 		const pattern = new RegExp(/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i);
 		const uuid = uuidv4()
 		expect(!!uuid.match(pattern)).toBe(true)
