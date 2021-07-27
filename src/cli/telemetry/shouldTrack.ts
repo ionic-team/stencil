c635329d2dcf319c29395b228baa29cbbe94e5b4
William M. Riley <splitinfinities@users.noreply.github.com>
2021-07-27 13:35:56 -0500

    Update src/cli/telemetry/shouldTrack.ts
    Co-authored-by: Ryan Waskiewicz <ryanwaskiewicz@gmail.com>

--- src/cli/telemetry/shouldTrack.ts
+++ src/cli/telemetry/shouldTrack.ts
@@ -3,8 +3,8 @@ import { checkTelemetry } from './telemetry';
 
 /**
  * Used to determine if tracking should occur.
- * @param ci boolean
- * @returns boolean
+ * @param ci whether or not the process is running in a Continuous Integration (CI) environment
+ * @returns true if telemetry should be sent, false otherwise
  */
 export async function shouldTrack(ci?: boolean) {
   return !ci && isInteractive() && (await checkTelemetry());
