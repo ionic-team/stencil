8bf176cede6d8243bdc56f8437e35b1c0f182913
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2021-08-30 09:57:44 -0400

    refactor(validate-tsconfig-tests): refactor tsconfig tests (#3036)
    this commit refactors & renames
    `src/compiler/config/test/validate-ts-sourcemap.spec.ts` to
    `src/compiler/config/test/validate-config-sourcemap.spec.ts`
    
    the file was renamed to align with the unit test file
    `validate-config.spec.ts`, found in the same directory. This file was
    not merged with `validate-config.spec.ts` to avoid more refactoring than
    necessary.
    
    the refactoring of the test file consists of:
    - renaming the suite & associated tests for clarity
    - removing unnecessary filesystem calls
    - refactoring generating a `LoadConfigInit` entity out to a helper fn
    - variable renaming

---
+++ src/compiler/config/test/validate-config-sourcemap.spec.ts
@@ -0,0 +1,63 @@
+import type * as d from '../../../declarations';
+import { createSystem } from '../../../compiler/sys/stencil-sys';
+import { loadConfig } from '../load-config';
+import path from 'path';
+
+describe('stencil config - sourceMap option', () => {
+  const configPath = require.resolve('./fixtures/stencil.config.ts');
+  const fixturesPath = path.dirname(configPath);
+  let sys: d.CompilerSystem;
+
+  /**
+   * Test helper for generating default `d.LoadConfigInit` objects.
+   *
+   * This function assumes the fields in the enclosing scope have ben initialized.
+   * @param overrides the properties on the default `d.LoadConfigInit` entity to manually override
+   * @returns the default configuration initialization object, with any overrides applied
+   */
+  const getLoadConfigForTests = (overrides?: Partial<d.LoadConfigInit>): d.LoadConfigInit => {
+    const defaults: d.LoadConfigInit = {
+      configPath: configPath,
+      sys,
+      config: {},
+      initTsConfig: true,
+    };
+    return { ...defaults, ...overrides };
+  };
+
+  beforeEach(() => {
+    sys = createSystem();
+    sys.writeFileSync(configPath, ``);
+    sys.createDirSync(fixturesPath);
+  });
+
+  it('sets sourceMap options to true in tsconfig', async () => {
+    const testConfig = getLoadConfigForTests({ config: { sourceMap: true } });
+
+    const loadConfigResults = await loadConfig(testConfig);
+
+    const { sourceMap, inlineSources } = loadConfigResults.config.tsCompilerOptions;
+    expect(sourceMap).toBe(true);
+    expect(inlineSources).toBe(true);
+  });
+
+  it('sets sourceMap options to false in tsconfig', async () => {
+    const testConfig = getLoadConfigForTests({ config: { sourceMap: false } });
+
+    const loadConfigResults = await loadConfig(testConfig);
+
+    const { sourceMap, inlineSources } = loadConfigResults.config.tsCompilerOptions;
+    expect(sourceMap).toBe(false);
+    expect(inlineSources).toBe(false);
+  });
+
+  it('sets the sourceMap options to false in tsconfig by default', async () => {
+    const testConfig = getLoadConfigForTests();
+
+    const loadConfigResults = await loadConfig(testConfig);
+
+    const { sourceMap, inlineSources } = loadConfigResults.config.tsCompilerOptions;
+    expect(sourceMap).toBe(false);
+    expect(inlineSources).toBe(false);
+  });
+});
