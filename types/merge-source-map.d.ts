2c33b1e2d14bbf4cd5ffefea43bd969abdad3988
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2021-08-31 09:26:16 -0400

    chore(types): updating typings for merge-source-map
    document the typings for merge-source-map

--- types/merge-source-map.d.ts
+++ types/merge-source-map.d.ts
@@ -1,6 +1,11 @@
-
+/**
+ * Ambient module declaration for the [merge-source-map library](https://github.com/keik/merge-source-map).
+ *
+ * At the time of this writing, no official nor third party type declaration files exist for v1.1.0 of library, and are
+ * recreated here for type safety purposes.
+ */
 declare module 'merge-source-map' {
-  interface SourceMap {
+  interface MergeSourceMap {
     file: string;
     mappings: string;
     names: string[];
@@ -10,8 +15,21 @@ declare module 'merge-source-map' {
     version: number;
   }
 
-  namespace merge {}
-  function merge(oldMap: SourceMap, newMap: SourceMap): SourceMap;
+  /**
+   * Merge an existing sourcemap with a new one
+   *
+   * This type declaration differs from the output of running `tsc -d` on the entrypoint of the merge-source-map
+   * module. It prefers concrete `MergeSourceMap` formal arguments over `(object | string)`.
+   *
+   * @param oldMap the original sourcemap to merge
+   * @param newMap the new sourcemap to merge with the `oldMap`
+   * @returns the merged sourcemap
+   */
+  function merge(oldMap: MergeSourceMap, newMap: MergeSourceMap): MergeSourceMap;
 
+  /**
+   * mark the `merge` function as the default export of the module, so that the JSDoc for `merge` is properly picked up
+   * by IntelliSense
+   */
   export = merge;
 }
