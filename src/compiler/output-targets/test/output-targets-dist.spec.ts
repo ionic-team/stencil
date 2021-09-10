d4a277f8b4727249c4dea2dc8b046a52a6125e8c
William Riley <splitinfinities@users.noreply.github.com>
2021-09-10 11:42:40 -0500

    output target dist

--- src/compiler/output-targets/test/output-targets-dist.spec.ts
+++ src/compiler/output-targets/test/output-targets-dist.spec.ts
@@ -1,12 +1,13 @@
 import { doNotExpectFiles, expectFiles } from '../../../testing/testing-utils';
-import { Compiler, Config } from '@stencil/core/compiler';
+import { createCompiler } from '@stencil/core/compiler';
+import * as d from '@stencil/core/declarations';
 import { mockConfig } from '@stencil/core/testing';
 import path from 'path';
 
-xdescribe('outputTarget, dist', () => {
+describe('outputTarget, dist', () => {
   jest.setTimeout(20000);
-  let compiler: Compiler;
-  let config: Config;
+  let compiler: d.Compiler;
+  let config: d.Config;
   const root = path.resolve('/');
 
   it('default dist files', async () => {
@@ -18,41 +19,59 @@ xdescribe('outputTarget, dist', () => {
     config.globalScript = path.join(root, 'User', 'testing', 'src', 'global.ts');
     config.outputTargets = [{ type: 'dist' }];
 
-    compiler = new Compiler(config);
+    // Renamed from new Compiler since Adam switch to primarily functional throughout stencil with the compiler rewrite.
+    compiler = await createCompiler(config);
 
-    await compiler.fs.writeFiles({
-      [path.join(config.sys.getClientPath('polyfills/index.js'))]: `/* polyfills */`,
-      [path.join(root, 'User', 'testing', 'package.json')]: `{
+    // The system doesn't allow for bulk writes of files - likely a symptom of adding support for mutiple systems like Deno?
+    await Promise.all([
+      await compiler.sys.writeFile(
+        path.join(config.sys.getCompilerExecutingPath() + 'polyfills/index.js'),
+        `/* polyfills */`
+      ),
+      await compiler.sys.writeFile(
+        path.join(root, 'User', 'testing', 'package.json'),
+        `{
         "module": "dist/index.mjs",
         "main": "dist/index.js",
         "collection": "dist/collection/collection-manifest.json",
         "types": "dist/types/components.d.ts"
-      }`,
-      [path.join(root, 'User', 'testing', 'src', 'index.html')]: `<cmp-a></cmp-a>`,
-      [path.join(root, 'User', 'testing', 'src', 'components', 'cmp-a.tsx')]: `
+      }`
+      ),
+      await compiler.sys.writeFile(path.join(root, 'User', 'testing', 'src', 'index.html'), `<cmp-a></cmp-a>`),
+      await compiler.sys.writeFile(
+        path.join(root, 'User', 'testing', 'src', 'components', 'cmp-a.tsx'),
+        `
         @Component({
           tag: 'cmp-a',
           styleUrls: {
             ios: 'cmp-a.ios.css',
             md: 'cmp-a.md.css'
           }
-        }) export class CmpA {}`,
-      [path.join(root, 'User', 'testing', 'src', 'components', 'cmp-a.ios.css')]: `cmp-a { color: blue; }`,
-      [path.join(root, 'User', 'testing', 'src', 'components', 'cmp-a.md.css')]: `cmp-a { color: green; }`,
-      [path.join(
-        root,
-        'User',
-        'testing',
-        'src',
-        'global.ts'
-      )]: `export default function() { console.log('my global'); }`,
-    });
-    await compiler.fs.commit();
+        }) export class CmpA {}`
+      ),
+      await compiler.sys.writeFile(
+        path.join(root, 'User', 'testing', 'src', 'components', 'cmp-a.ios.css'),
+        `cmp-a { color: blue; }`
+      ),
+      await compiler.sys.writeFile(
+        path.join(root, 'User', 'testing', 'src', 'components', 'cmp-a.md.css'),
+        `cmp-a { color: green; }`
+      ),
+      await compiler.sys.writeFile(
+        path.join(root, 'User', 'testing', 'src', 'global.ts'),
+        `export default function() { console.log('my global'); }`
+      ),
+    ]);
 
+    // Seems some config variable passed down within this command isn't passed down all the way
+    // Causing a TypeError: Cannot read property 'extendedDiagnostics' of undefined
     const r = await compiler.build();
     expect(r.diagnostics).toHaveLength(0);
 
-    expectFiles(compiler.fs, [
+    // I'm nervous that the mocked out compiler.sys doesn't ACTUALLY have the same in-memory file system anymore...
+    // unsure what could be done about that right now.
+    // unsure also where the assertion within this function lies
+    expectFiles(compiler.sys, [
       path.join(root, 'User', 'testing', 'dist', 'index.js'),
       path.join(root, 'User', 'testing', 'dist', 'index.mjs'),
 
@@ -75,7 +94,8 @@ xdescribe('outputTarget, dist', () => {
       path.join(root, 'User', 'testing', 'src', 'components.d.ts'),
     ]);
 
-    doNotExpectFiles(compiler.fs, [
+    // unsure where the assertion within this function lies
+    doNotExpectFiles(compiler.sys, [
       path.join(root, 'User', 'testing', 'build'),
       path.join(root, 'User', 'testing', 'esm'),
       path.join(root, 'User', 'testing', 'es5'),
