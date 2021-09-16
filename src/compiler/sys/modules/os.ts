f9e4d9dd5297bfc3e3c371313dfcbf1357ee5783
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2021-09-16 15:09:48 -0400

    format the world

--- src/compiler/sys/modules/os.ts
+++ src/compiler/sys/modules/os.ts
@@ -2,7 +2,7 @@ import { OS_PLATFORM } from '../environment';
 
 export const EOL = '\n';
 
-export const platform = () =>  OS_PLATFORM;
+export const platform = () => OS_PLATFORM;
 
 export default {
   EOL,
