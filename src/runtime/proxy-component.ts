4e59113fa5e5f221f56db140c7d5ce136bacbb01
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2021-10-15 16:52:58 -0400

    chore(code-cleanup)
    while working through this ticket, a lot of code was read. this commit
    fixes up small issues that are inconsequential to the implementation of
    this fix, and can be dropped if needed. this commit focuses on:
    - removing an unused tslint:disable pragma
    - fixing typos
    - adding curly braces where needed

--- src/runtime/proxy-component.ts
+++ src/runtime/proxy-component.ts
@@ -74,14 +74,14 @@ export const proxyComponent = (Cstr: d.ComponentConstructor, cmpMeta: d.Componen
         plt.jmp(() => {
           const propName = attrNameToPropName.get(attrName);
 
-          //  In a webcomponent lifecyle the attributeChangedCallback runs prior to connectedCallback
+          //  In a web component lifecycle the attributeChangedCallback runs prior to connectedCallback
           //  in the case where an attribute was set inline.
           //  ```html
           //    <my-component some-attribute="some-value"></my-component>
           //  ```
           //
-          //  There is an edge case where a developer sets the attribute inline on a custom element and then programatically
-          //  changes it before it has been upgraded as shown below:
+          //  There is an edge case where a developer sets the attribute inline on a custom element and then
+          //  programmatically changes it before it has been upgraded as shown below:
           //
           //  ```html
           //    <!-- this component has _not_ been upgraded yet -->
@@ -91,13 +91,13 @@ export const proxyComponent = (Cstr: d.ComponentConstructor, cmpMeta: d.Componen
           //      el = document.querySelector("#test");
           //      el.someAttribute = "another-value";
           //      // upgrade component
-          //      cutsomElements.define('my-component', MyComponent);
+          //      customElements.define('my-component', MyComponent);
           //    </script>
           //  ```
           //  In this case if we do not unshadow here and use the value of the shadowing property, attributeChangedCallback
           //  will be called with `newValue = "some-value"` and will set the shadowed property (this.someAttribute = "another-value")
           //  to the value that was set inline i.e. "some-value" from above example. When
-          //  the connectedCallback attempts to unshadow it will use "some-value" as the intial value rather than "another-value"
+          //  the connectedCallback attempts to unshadow it will use "some-value" as the initial value rather than "another-value"
           //
           //  The case where the attribute was NOT set inline but was not set programmatically shall be handled/unshadowed
           //  by connectedCallback as this attributeChangedCallback will not fire.
