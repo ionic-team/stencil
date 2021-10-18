d0ab6305b31317637a5b17c609f60123a616cb4f
Lars Mikkelsen <lars@ionic.io>
2021-10-18 10:26:30 -0400

    add additional test cases

--- src/compiler/transformers/test/transform-utils.spec.ts
+++ src/compiler/transformers/test/transform-utils.spec.ts
@@ -2,28 +2,40 @@ import { mapJSDocTagInfo } from '../transform-utils';
 
 describe('transform utils', () => {
   it('flattens TypeScript JSDocTagInfo to Stencil JSDocTagInfo', () => {
+    // tags corresponds to the following JSDoc
+    /*
+     * @param foo the first parameter
+     * @param bar
+     * @returns
+     * @see {@link https://example.com}
+     */
     const tags = [
       {
         name: 'param',
         text: [
-          { kind: 'text', text: 'foo' },
-          { kind: 'space', text: ' ' },
-          { kind: 'text', text: 'the first parameter' },
+          { text: 'foo', kind: 'parameterName' },
+          { text: ' ', kind: 'space' },
+          { text: 'the first parameter', kind: 'text' },
         ],
       },
+      { name: 'param', text: [{ text: 'bar', kind: 'text' }] },
+      { name: 'returns', text: undefined },
       {
-        name: 'param',
+        name: 'see',
         text: [
-          { kind: 'text', text: 'bar' },
-          { kind: 'space', text: ' ' },
-          { kind: 'text', text: 'the second parameter' },
+          { text: '', kind: 'text' },
+          { text: '{@link ', kind: 'link' },
+          { text: 'https://example.com', kind: 'linkText' },
+          { text: '}', kind: 'link' },
         ],
       },
     ];
 
     expect(mapJSDocTagInfo(tags)).toEqual([
       { name: 'param', text: 'foo the first parameter' },
-      { name: 'param', text: 'bar the second parameter' },
+      { name: 'param', text: 'bar' },
+      { name: 'returns', text: undefined },
+      { name: 'see', text: '{@link https://example.com}' },
     ]);
   });
 });
