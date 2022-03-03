d5d9ff440947abcb914963ed677ad0996458abe1
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2022-03-03 08:10:44 -0500

    test(runtime): add bool tests to property parsing
    this commit backfills unit tests for
    `parse-property-value#parsePropertyValue()`, specifically handling cases
    where boolean values are passed to the function

--- src/runtime/test/parse-property-value.spec.ts
+++ src/runtime/test/parse-property-value.spec.ts
@@ -3,6 +3,78 @@ import { MEMBER_FLAGS } from '../../utils';
 
 describe('parse-property-value', () => {
   describe('parsePropertyValue', () => {
+    describe('boolean coercion', () => {
+      it('coerces the string "false" to boolean false', () => {
+        const result = parsePropertyValue('false', MEMBER_FLAGS.Boolean);
+        expect(result).toBe(false);
+      });
+
+      it('coerces the string "False" to boolean true', () => {
+        const result = parsePropertyValue('False', MEMBER_FLAGS.Boolean);
+        expect(result).toBe(true);
+      });
+
+      it('coerces an empty string to boolean true', () => {
+        const result = parsePropertyValue('', MEMBER_FLAGS.Boolean);
+        expect(result).toBe(true);
+      });
+
+      it('coerces the string "true" to boolean true', () => {
+        const result = parsePropertyValue('true', MEMBER_FLAGS.Boolean);
+        expect(result).toBe(true);
+      });
+
+      it('coerces the number 0 to boolean false', () => {
+        const result = parsePropertyValue(0, MEMBER_FLAGS.Boolean);
+        expect(result).toBe(false);
+      });
+
+      it('coerces the string "0" to boolean true', () => {
+        const result = parsePropertyValue('0', MEMBER_FLAGS.Boolean);
+        expect(result).toBe(true);
+      });
+
+      it('coerces the number 1 to boolean true', () => {
+        const result = parsePropertyValue(1, MEMBER_FLAGS.Boolean);
+        expect(result).toBe(true);
+      });
+
+      it('coerces the string "1" to boolean true', () => {
+        const result = parsePropertyValue('1', MEMBER_FLAGS.Boolean);
+        expect(result).toBe(true);
+      });
+
+      it('does not coerce null to boolean', () => {
+        const result = parsePropertyValue(null, MEMBER_FLAGS.Boolean);
+        expect(result).toBe(null);
+      });
+
+      it('coerces the string "null" to boolean true', () => {
+        const result = parsePropertyValue('null', MEMBER_FLAGS.Boolean);
+        expect(result).toBe(true);
+      });
+
+      it('does not coerce undefined to boolean', () => {
+        const result = parsePropertyValue(undefined, MEMBER_FLAGS.Boolean);
+        expect(result).toBe(undefined);
+      });
+
+      it('coerces the string "undefined" to boolean true', () => {
+        const result = parsePropertyValue('undefined', MEMBER_FLAGS.Boolean);
+        expect(result).toBe(true);
+      });
+
+      it('coerces NaN to boolean false', () => {
+        const result = parsePropertyValue(NaN, MEMBER_FLAGS.Boolean);
+        expect(result).toBe(false);
+      });
+
+      it('coerces the string "NaN" to boolean true', () => {
+        const result = parsePropertyValue('NaN', MEMBER_FLAGS.Boolean);
+        expect(result).toBe(true);
+      });
+    });
+
     describe('number coercion', () => {
       it('coerces a number value to a number', () => {
         const result = parsePropertyValue(42, MEMBER_FLAGS.Number);
