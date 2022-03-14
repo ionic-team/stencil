ionic-team/stencil

commit cf42114edf04520c11421c60673e4f03be22df49

fix(testing): fix test regex for 'e2e.ts' (#3277)
this commit fixes an edge case where unit tests were not being properly
detected following https://github.com/ionic-team/stencil/commit/c6fda394f256eb3be68a1a6ff2f0c2ef0193958f

the ionic framework has tests named `e2e.ts` that we must account for in
this regex. because jest doesn't error when no tests are found, we
missed this in the original implementation.

tests of name 'spec.ts' and 'test.ts' (or any other acceptable
extension) are considered unit tests, and tests of the name 'e2e.ts' are
considered end-to-end tests

src/compiler/config/validate-testing.ts
@@ -125,7 +125,18 @@ export const validateTesting = (config: d.Config, diagnostics: d.Diagnostic[]) =
   }
 
   if (testing.testRegex === undefined) {
-    testing.testRegex = '(/__tests__/.*|\\.(test|spec|e2e))\\.(tsx|ts|jsx|js)$';
+    /**
+     * The test regex covers cases of:
+     * - files under a `__tests__` directory
+     * - the case where a test file has a name such as `test.ts`, `spec.ts` or `e2e.ts`.
+     *   - these files can use any of the following file extensions: .ts, .tsx, .js, .jsx.
+     *   - this regex only handles the entire path of a file, e.g. `/some/path/e2e.ts`
+     * - the case where a test file ends with `.test.ts`, `.spec.ts`, or `.e2e.ts`.
+     *   - these files can use any of the following file extensions: .ts, .tsx, .js, .jsx.
+     *   - this regex case shall match file names such as `my-cmp.spec.ts`, `test.spec.ts`
+     *   - this regex case shall not match file names such as `attest.ts`, `bespec.ts`
+     */
+    testing.testRegex = '(/__tests__/.*|(\\.|/)(test|spec|e2e))\\.[jt]sx?';
   }
 
   if (Array.isArray(testing.testMatch)) {

src/compiler/config/test/validate-testing.spec.ts
@@ -159,6 +159,22 @@ describe('validateTesting', () => {
         expect(testRegex.test('my-component.test.jsx')).toBe(true);
       });
 
+      it('matches the file "test.ts"', () => {
+        expect(testRegex.test('some/path/test.ts')).toBe(true);
+      });
+
+      it('matches the file "test.tsx"', () => {
+        expect(testRegex.test('some/path/test.tsx')).toBe(true);
+      });
+
+      it('matches the file "test.js"', () => {
+        expect(testRegex.test('some/path/test.js')).toBe(true);
+      });
+
+      it('matches the file "test.jsx"', () => {
+        expect(testRegex.test('some/path/test.jsx')).toBe(true);
+      });
+
       it("doesn't match files ending in test.ts", () => {
         expect(testRegex.test('my-component-test.ts')).toBe(false);
       });
@@ -201,6 +217,22 @@ describe('validateTesting', () => {
         expect(testRegex.test('my-component.spec.jsx')).toBe(true);
       });
 
+      it('matches the file "spec.ts"', () => {
+        expect(testRegex.test('some/path/spec.ts')).toBe(true);
+      });
+
+      it('matches the file "spec.tsx"', () => {
+        expect(testRegex.test('some/path/spec.tsx')).toBe(true);
+      });
+
+      it('matches the file "spec.js"', () => {
+        expect(testRegex.test('some/path/spec.js')).toBe(true);
+      });
+
+      it('matches the file "spec.jsx"', () => {
+        expect(testRegex.test('some/path/spec.jsx')).toBe(true);
+      });
+
       it("doesn't match files ending in spec.ts", () => {
         expect(testRegex.test('my-component-spec.ts')).toBe(false);
       });
@@ -241,6 +273,22 @@ describe('validateTesting', () => {
 
       it('matches files ending in .e2e.jsx', () => {
         expect(testRegex.test('my-component.e2e.jsx')).toBe(true);
+      });
+
+      it('matches the file "e2e.ts"', () => {
+        expect(testRegex.test('some/path/e2e.ts')).toBe(true);
+      });
+
+      it('matches the file "e2e.tsx"', () => {
+        expect(testRegex.test('some/path/e2e.tsx')).toBe(true);
+      });
+
+      it('matches the file "e2e.js"', () => {
+        expect(testRegex.test('some/path/e2e.js')).toBe(true);
+      });
+
+      it('matches the file "e2e.jsx"', () => {
+        expect(testRegex.test('some/path/e2e.jsx')).toBe(true);
       });
 
       it("doesn't match files ending in e2e.ts", () => {

commit 34242c17535a83ffe13928af8f436863178d4cff

fix(testing): handle snapshot filepaths
this commit updates the testing regex in our jest base configuration to
handle files such as testing snapshots - those that contain 'spec.ts'
(or a similarly acceptable file extension for tests), but do not end
with that extension. For example, `my-component.test.ts.snap`

this fixes a regression introduced in https://github.com/ionic-team/stencil/commit/cf42114edf04520c11421c60673e4f03be22df49

src/compiler/config/validate-testing.ts
@@ -136,7 +136,7 @@ export const validateTesting = (config: d.Config, diagnostics: d.Diagnostic[]) =
      *   - this regex case shall match file names such as `my-cmp.spec.ts`, `test.spec.ts`
      *   - this regex case shall not match file names such as `attest.ts`, `bespec.ts`
      */
-    testing.testRegex = '(/__tests__/.*|(\\.|/)(test|spec|e2e))\\.[jt]sx?';
+    testing.testRegex = '(/__tests__/.*|(\\.|/)(test|spec|e2e))\\.[jt]sx?$';
   }
 
   if (Array.isArray(testing.testMatch)) {

src/compiler/config/test/validate-testing.spec.ts
@@ -175,6 +175,22 @@ describe('validateTesting', () => {
         expect(testRegex.test('some/path/test.jsx')).toBe(true);
       });
 
+      it("doesn't match snap files ending in test.ts.snap", () => {
+        expect(testRegex.test('my-component.test.ts.snap')).toBe(false);
+      });
+
+      it("doesn't match snap files ending in test.tsx.snap", () => {
+        expect(testRegex.test('my-component.test.tsx.snap')).toBe(false);
+      });
+
+      it("doesn't match snap files ending in test.js.snap", () => {
+        expect(testRegex.test('my-component.test.js.snap')).toBe(false);
+      });
+
+      it("doesn't match snap files ending in test.jsx.snap", () => {
+        expect(testRegex.test('my-component.test.jsx.snap')).toBe(false);
+      });
+
       it("doesn't match files ending in test.ts", () => {
         expect(testRegex.test('my-component-test.ts')).toBe(false);
       });
@@ -233,6 +249,22 @@ describe('validateTesting', () => {
         expect(testRegex.test('some/path/spec.jsx')).toBe(true);
       });
 
+      it("doesn't match snap files ending in spec.ts.snap", () => {
+        expect(testRegex.test('my-component.spec.ts.snap')).toBe(false);
+      });
+
+      it("doesn't match snap files ending in spec.tsx.snap", () => {
+        expect(testRegex.test('my-component.spec.tsx.snap')).toBe(false);
+      });
+
+      it("doesn't match snap files ending in spec.js.snap", () => {
+        expect(testRegex.test('my-component.spec.js.snap')).toBe(false);
+      });
+
+      it("doesn't match snap files ending in spec.jsx.snap", () => {
+        expect(testRegex.test('my-component.spec.jsx.snap')).toBe(false);
+      });
+
       it("doesn't match files ending in spec.ts", () => {
         expect(testRegex.test('my-component-spec.ts')).toBe(false);
       });
@@ -289,6 +321,22 @@ describe('validateTesting', () => {
 
       it('matches the file "e2e.jsx"', () => {
         expect(testRegex.test('some/path/e2e.jsx')).toBe(true);
+      });
+
+      it("doesn't match snap files ending in e2e.ts.snap", () => {
+        expect(testRegex.test('my-component.e2e.ts.snap')).toBe(false);
+      });
+
+      it("doesn't match snap files ending in e2e.tsx.snap", () => {
+        expect(testRegex.test('my-component.e2e.tsx.snap')).toBe(false);
+      });
+
+      it("doesn't match snap files ending in e2e.js.snap", () => {
+        expect(testRegex.test('my-component.e2e.js.snap')).toBe(false);
+      });
+
+      it("doesn't match snap files ending in e2e.jsx.snap", () => {
+        expect(testRegex.test('my-component.e2e.jsx.snap')).toBe(false);
       });
 
       it("doesn't match files ending in e2e.ts", () => {