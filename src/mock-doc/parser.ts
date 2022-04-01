eb2090efd2bb10ba25272c383d3a9cac99df5497
Camden Narzt <heartwork@narzt.cam>
2022-04-01 09:50:35 -0600

    formatted with prettier

--- src/mock-doc/parser.ts
+++ src/mock-doc/parser.ts
@@ -2,15 +2,15 @@ import { MockDocument } from './document';
 import { parseHtmlToDocument } from './parse-html';
 
 export type DOMParserSupportedType =
-    "text/html" |
-    "text/xml" |
-    "application/xml" |
-    "application/xhtml+xml" |
-    "image/svg+xml";
+  | 'text/html'
+  | 'text/xml'
+  | 'application/xml'
+  | 'application/xhtml+xml'
+  | 'image/svg+xml';
 
 export class MockDOMParser {
-    parseFromString(string: string, type: DOMParserSupportedType): MockDocument {
-        if (type !== 'text/html') console.error('XML parsing not implemented yet, continuing as html');
-        return parseHtmlToDocument(string);
-    }
+  parseFromString(string: string, type: DOMParserSupportedType): MockDocument {
+    if (type !== 'text/html') console.error('XML parsing not implemented yet, continuing as html');
+    return parseHtmlToDocument(string);
+  }
 }
