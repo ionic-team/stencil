d0b6016d9cb2185552e6ad1e6f574d0d5dc6c005
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2022-04-04 16:33:20 -0400

    test(events): add unit tests to event type generation
    this commit adds unit testing to the generation of `@Event` decorated
    class members - specifically how we generate type information that will
    be used in a type declaration file (`d.ts` file).
    
    this commit changes the interface of the internal `generateEventTypes`
    function, requiring that `ComponentCompilerMeta` be passed to the
    function rather than just the `ComponentCompilerMethod` metadata found
    on the former object. this change is for a future effort where we will
    be using additional information from the component metadata to avoid
    naming collisions in type declaration file generation. although the
    author would have liked to have made this interface change then, adding
    the change now simplifies the future pr.

--- src/compiler/types/generate-event-types.ts
+++ src/compiler/types/generate-event-types.ts
@@ -1,19 +1,26 @@
 import type * as d from '../../declarations';
 import { getTextDocs, toTitleCase } from '@utils';
 
-export const generateEventTypes = (cmpEvents: d.ComponentCompilerEvent[]): d.TypeInfo => {
-  return cmpEvents.map((cmpEvent) => {
-    const name = `on${toTitleCase(cmpEvent.name)}`;
-    const type = cmpEvent.complexType.original
-      ? `(event: CustomEvent<${cmpEvent.complexType.original}>) => void`
-      : `CustomEvent`;
-    return {
-      name,
-      type,
-      optional: false,
-      required: false,
-      internal: cmpEvent.internal,
-      jsdoc: getTextDocs(cmpEvent.docs),
-    };
-  });
+/**
+ * Generates the individual event types for all @Event() decorated events in a component
+ * @param cmpMeta component runtime metadata for a single component
+ * @returns the generated type metadata
+ */
+export const generateEventTypes = (cmpMeta: d.ComponentCompilerMeta): d.TypeInfo => {
+  return [
+    ...cmpMeta.events.map((cmpEvent) => {
+      const name = `on${toTitleCase(cmpEvent.name)}`;
+      const type = cmpEvent.complexType.original
+        ? `(event: CustomEvent<${cmpEvent.complexType.original}>) => void`
+        : `CustomEvent`;
+      return {
+        name,
+        type,
+        optional: false,
+        required: false,
+        internal: cmpEvent.internal,
+        jsdoc: getTextDocs(cmpEvent.docs),
+      };
+    }),
+  ];
 };
