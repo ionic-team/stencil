cf5f7748dc56d822e84bed499853e0a3e83c9f6e
Alice Pote <alice.writes.wrongs@gmail.com>
2022-04-22 12:24:56 -0400

    basic eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    "@typescript-eslint/no-unused-vars": [2]
+  }
+};
