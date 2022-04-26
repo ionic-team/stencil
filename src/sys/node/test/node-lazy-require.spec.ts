382bb75d244b1f7eff3c7f79483b1c3248527ce0
Alice Pote <alice.writes.wrongs@gmail.com>
2022-04-26 15:26:58 -0400

    another describe block

--- src/sys/node/test/node-lazy-require.spec.ts
+++ src/sys/node/test/node-lazy-require.spec.ts
@@ -10,48 +10,50 @@ const mockPackageJson = (version: string) =>
 
 describe('node-lazy-require', () => {
   describe('NodeLazyRequire', () => {
-    function setup() {
-      const resolveModule = new NodeResolveModule();
-      const readFSMock = jest.spyOn(fs, 'readFileSync').mockReturnValue(mockPackageJson('10.10.10'));
+    describe('ensure', () => {
+      function setup() {
+        const resolveModule = new NodeResolveModule();
+        const readFSMock = jest.spyOn(fs, 'readFileSync').mockReturnValue(mockPackageJson('10.10.10'));
 
-      const nodeLazyRequire = new NodeLazyRequire(resolveModule, {
-        jest: ['2.0.7', '38.0.1'],
-      });
-      return {
-        nodeLazyRequire,
-        readFSMock,
-      };
-    }
-
-    it.each(['2.0.7', '10.10.10', '38.0.1'])(
-      'should not error if a package of suitable version (%p) is installed',
-      async (testVersion) => {
-        const { nodeLazyRequire, readFSMock } = setup();
-        readFSMock.mockReturnValue(mockPackageJson(testVersion));
-        let diagnostics = await nodeLazyRequire.ensure('.', ['jest']);
-        expect(diagnostics.length).toBe(0);
+        const nodeLazyRequire = new NodeLazyRequire(resolveModule, {
+          jest: ['2.0.7', '38.0.1'],
+        });
+        return {
+          nodeLazyRequire,
+          readFSMock,
+        };
       }
-    );
 
-    it('should error if the installed version of a package is too low', async () => {
-      const { nodeLazyRequire, readFSMock } = setup();
-      readFSMock.mockReturnValue(mockPackageJson('1.1.1'));
-      let [error] = await nodeLazyRequire.ensure('.', ['jest']);
-      expect(error).toEqual({
-        ...buildError([]),
-        header: 'Please install supported versions of dev dependencies with either npm or yarn.',
-        messageText: 'npm install --save-dev jest@38.0.1',
+      it.each(['2.0.7', '10.10.10', '38.0.1'])(
+        'should not error if a package of suitable version (%p) is installed',
+        async (testVersion) => {
+          const { nodeLazyRequire, readFSMock } = setup();
+          readFSMock.mockReturnValue(mockPackageJson(testVersion));
+          let diagnostics = await nodeLazyRequire.ensure('.', ['jest']);
+          expect(diagnostics.length).toBe(0);
+        }
+      );
+
+      it('should error if the installed version of a package is too low', async () => {
+        const { nodeLazyRequire, readFSMock } = setup();
+        readFSMock.mockReturnValue(mockPackageJson('1.1.1'));
+        let [error] = await nodeLazyRequire.ensure('.', ['jest']);
+        expect(error).toEqual({
+          ...buildError([]),
+          header: 'Please install supported versions of dev dependencies with either npm or yarn.',
+          messageText: 'npm install --save-dev jest@38.0.1',
+        });
       });
-    });
 
-    it('should error if the installed version of a package is too high', async () => {
-      const { nodeLazyRequire, readFSMock } = setup();
-      readFSMock.mockReturnValue(mockPackageJson('100.1.1'));
-      let [error] = await nodeLazyRequire.ensure('.', ['jest']);
-      expect(error).toEqual({
-        ...buildError([]),
-        header: 'Please install supported versions of dev dependencies with either npm or yarn.',
-        messageText: 'npm install --save-dev jest@38.0.1',
+      it('should error if the installed version of a package is too high', async () => {
+        const { nodeLazyRequire, readFSMock } = setup();
+        readFSMock.mockReturnValue(mockPackageJson('100.1.1'));
+        let [error] = await nodeLazyRequire.ensure('.', ['jest']);
+        expect(error).toEqual({
+          ...buildError([]),
+          header: 'Please install supported versions of dev dependencies with either npm or yarn.',
+          messageText: 'npm install --save-dev jest@38.0.1',
+        });
       });
     });
   });
