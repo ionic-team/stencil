9bfef1ad4637fe82c349eadbd9153e04417b1337
Alice <alice.writes.wrongs@gmail.com>
2022-04-28 14:17:55 -0400

    fix(sys): tweak NodeLazyRequire logic around too-high-versions errors (#3347)
    this changes the logic that we use in the `NodeLazyRequire` class to
    error out in fewer situations. the changes are:
    
    - the data structure for specifying our version ranges is changed from
      `[string, string]` (`[minVersion, recommendedVersion]`) to an object
      with `minVersion` `recommendedVersion` required properties and an
      optional `maxVersion` property.
    - in the `ensure` method on `NodeLazyRequire` we check if `maxVersion`
      is defined on a given version range requirement.
        - If so, we check that the installed version is greater than or
          equal to `minVersion` and less than the major version of
          `maxVersion`.
        - If not, we just check that `minVersion <= installedVersion`
    
    this should give us the flexibility to mark certain versions of packages
    as incompatible (for instance jest@28) without having to do that for all
    packages that we lazy require. this is helpful because for most of them
    we just want to set a minimum version and don't have a need for an
    enforced maximum version.

--- src/sys/node/test/node-lazy-require.spec.ts
+++ src/sys/node/test/node-lazy-require.spec.ts
@@ -1,4 +1,4 @@
-import { NodeLazyRequire } from '../node-lazy-require';
+import { LazyDependencies, NodeLazyRequire } from '../node-lazy-require';
 import { buildError } from '@utils';
 import { NodeResolveModule } from '../node-resolve-module';
 import fs from 'graceful-fs';
@@ -21,45 +21,63 @@ describe('node-lazy-require', () => {
         readFSMock.mockClear();
       });
 
-      function setup() {
+      const jestTestRange = (maxVersion = '38.0.1'): LazyDependencies => ({
+        jest: {
+          minVersion: '2.0.7',
+          recommendedVersion: '36.0.1',
+          maxVersion,
+        },
+      });
+
+      function setup(versionRange: LazyDependencies) {
         const resolveModule = new NodeResolveModule();
-        const nodeLazyRequire = new NodeLazyRequire(resolveModule, {
-          jest: ['2.0.7', '38.0.1'],
-        });
+        const nodeLazyRequire = new NodeLazyRequire(resolveModule, versionRange);
         return nodeLazyRequire;
       }
 
       it.each(['2.0.7', '10.10.10', '38.0.1', '38.0.2', '38.5.17'])(
         'should not error if installed package has a suitable major version (%p)',
         async (testVersion) => {
-          const nodeLazyRequire = setup();
+          const nodeLazyRequire = setup(jestTestRange());
           readFSMock.mockReturnValue(mockPackageJson(testVersion));
           let diagnostics = await nodeLazyRequire.ensure('.', ['jest']);
           expect(diagnostics.length).toBe(0);
         }
       );
 
-      it('should error if the installed version of a package is too low', async () => {
-        const nodeLazyRequire = setup();
+      it.each(['2.0.7', '10.10.10', '36.0.1', '38.0.2', '38.5.17'])(
+        'should never error with versions above minVersion if there is no maxVersion supplied (%p)',
+        async (testVersion) => {
+          const nodeLazyRequire = setup(jestTestRange(undefined));
+          readFSMock.mockReturnValue(mockPackageJson(testVersion));
+          let diagnostics = await nodeLazyRequire.ensure('.', ['jest']);
+          expect(diagnostics.length).toBe(0);
+        }
+      );
+
+      it.each(['38', undefined])('should error w/ installed version too low and maxVersion=%p', async (maxVersion) => {
+        const range = jestTestRange(maxVersion);
+        const nodeLazyRequire = setup(range);
         readFSMock.mockReturnValue(mockPackageJson('1.1.1'));
         let [error] = await nodeLazyRequire.ensure('.', ['jest']);
         expect(error).toEqual({
           ...buildError([]),
           header: 'Please install supported versions of dev dependencies with either npm or yarn.',
-          messageText: 'npm install --save-dev jest@38.0.1',
+          messageText: `npm install --save-dev jest@${range.jest.recommendedVersion}`,
         });
       });
 
       it.each(['100.1.1', '38.0.1-alpha.0'])(
         'should error if the installed version of a package is too high (%p)',
         async (version) => {
-          const nodeLazyRequire = setup();
+          const range = jestTestRange();
+          const nodeLazyRequire = setup(range);
           readFSMock.mockReturnValue(mockPackageJson(version));
           let [error] = await nodeLazyRequire.ensure('.', ['jest']);
           expect(error).toEqual({
             ...buildError([]),
             header: 'Please install supported versions of dev dependencies with either npm or yarn.',
-            messageText: 'npm install --save-dev jest@38.0.1',
+            messageText: `npm install --save-dev jest@${range.jest.recommendedVersion}`,
           });
         }
       );
