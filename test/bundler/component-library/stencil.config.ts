b66b8534530ce9d5453bc7cfa031883af67485a0
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2022-04-29 16:24:17 -0400

    test(bundler): add component library for testing
    add a stencil component library to be used in tests that verify
    applications that consume the library and are bundled with vite, parcel,
    etc.
    
    this commit does not introduce any tests themselves, but rather is
    boilerplate code that will be used in future commits (the intent is to
    stack multiple pull requests atop this one)

---
+++ test/bundler/component-library/stencil.config.ts
@@ -0,0 +1,18 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'component-library',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+};
