b6dbe642c0f40093aa1ea4dffe2daa0b4dab7226
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2022-05-03 09:10:27 -0400

    update script test

--- test/bundler/parcel-bundle-test/restructure.sh
+++ test/bundler/parcel-bundle-test/restructure.sh
@@ -15,10 +15,10 @@ pushd dist
 mkdir -p parcel-assets
 # use '--' to mark the end of options for `mv`, so that a filename is not mistaken for an option to the command
 # https://github.com/koalaman/shellcheck/wiki/SC2035
-mv *.js parcel-assets
+mv -- *.js parcel-assets
 
 # rewrite index.html's src attributes to use the new sub-directory
-awk '{gsub("src=\"/index", "src=\"/.parcel-assets/index", $0); print $0;}' index.html > new_index.html
+awk '{gsub("src=\"/index", "src=\"./parcel-assets/index", $0); print $0;}' index.html > new_index.html
 mv new_index.html index.html
 
 popd
