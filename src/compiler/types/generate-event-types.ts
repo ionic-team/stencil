6b4ee36eb0226a937d7406e52e5306eb8a47bba6
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2022-05-09 15:58:10 -0400

    fix(): update jsdocs

--- src/compiler/types/generate-event-types.ts
+++ src/compiler/types/generate-event-types.ts
@@ -6,7 +6,7 @@ import { updateTypeIdentifierNames } from './stencil-types';
  * Generates the individual event types for all @Event() decorated events in a component
  * @param cmpMeta component runtime metadata for a single component
  * @param typeImportData locally/imported/globally used type names, which may be used to prevent naming collisions
- * @param cmpClassName The pascal cas name of the component class
+ * @param cmpClassName The pascal cased name of the component class
  * @returns the generated type metadata
  */
 export const generateEventTypes = (
@@ -34,6 +34,7 @@ export const generateEventTypes = (
 /**
  * Determine the correct type name for all type(s) used by a class member annotated with `@Event()`
  * @param cmpEvent the compiler metadata for a single `@Event()`
+ * @param cmpEventDetailInterface the name of the custom event type to use in the generated type
  * @param typeImportData locally/imported/globally used type names, which may be used to prevent naming collisions
  * @param componentSourcePath the path to the component on disk
  * @returns the type associated with a `@Event()`
