0f33f2265f86f871a8f3067145c7fa9af1ede3f3
Alice Pote <alice.writes.wrongs@gmail.com>
2022-05-12 10:57:12 -0400

    more feedback

--- scripts/strict-null-checks-ci.ts
+++ scripts/strict-null-checks-ci.ts
@@ -76,6 +76,7 @@ const errorsOnPR = prData.length;
 /**
  * Build up an object which maps error codes (like `TS2339`) to
  * a `Set` of all the messages we see for that code.
+ * @returns a map from error codes to the messages for that code
  */
 const getErrorCodeMessages = (): Record<string, Set<string>> => {
   const errorCodeMessageMap = {};
@@ -97,12 +98,15 @@ const errorCodeMessages = getErrorCodeMessages();
 
 /**
  * Build a map which just counts the entries in an array
+ *
+ * @param arr the array whose entries we're going to count
+ * @returns a map of counts for `arr`
  */
-const countArrayEntries = <T>(xs: Array<T>): Map<T, number> => {
+const countArrayEntries = <T>(arr: Array<T>): Map<T, number> => {
   let counts = new Map<T, number>();
 
-  xs.forEach((x) => {
-    counts.set(x, (counts.get(x) ?? 0) + 1);
+  arr.forEach((entry) => {
+    counts.set(entry, (counts.get(entry) ?? 0) + 1);
   });
 
   return counts;
@@ -117,8 +121,13 @@ const errorCodeCounts = countArrayEntries(prData.map((error) => error.value.tsEr
 /**
  * This makes it straightforward to create a collapsible section in GFM
  * Just pass in a callback which expects an array to push lines onto
+ *
+ * @param title the title for this collapsible section
+ * @param contentCb a callback which is used by the caller to define the section's contents
+ * @param lineBreak an (optional) param for setting the linebreak
+ * @returns the collapsible section, ready for inclusion in a larger markdown context
  */
-const collapsible = (title: string, contentCb: (out: string[]) => void, lineBreak = '\n') => {
+const collapsible = (title: string, contentCb: (out: string[]) => void, lineBreak = '\n'): string => {
   let out = [`<details><summary>${title}</summary>`, ''];
   contentCb(out);
   out.push('');
@@ -128,6 +137,9 @@ const collapsible = (title: string, contentCb: (out: string[]) => void, lineBrea
 
 /**
  * Format a basic table header for GFM
+ *
+ * @param colNames column names to be formatted into the table
+ * @returns a formatted table header
  */
 const tableHeader = (...colNames: string[]) => [tableRow(...colNames), tableRow(...colNames.map((_) => '---'))].join('\n');
 
@@ -135,12 +147,18 @@ const tableHeader = (...colNames: string[]) => [tableRow(...colNames), tableRow(
  * Format a GFM table row
  *
  * this looks like `| cell | cell |`
+ *
+ * @param entries for this row
+ * @returns a formatted row
  */
 const tableRow = (...entries: string[]) => '| ' + entries.join(' | ') + ' |';
 
 /**
  * Helper function get a reverse-sort of the entries in our
  * 'count' maps (the return value of `countArrayEntries`)
+ *
+ * @param countMap is a map of counts (which we plan to sort)
+ * @returns the entries of `countMap`, sorted in descending order by the count
  */
 const sortEntries = (countMap: Map<string, number>): [string, number][] =>
   [...countMap.entries()].sort((a, b) => {
@@ -197,7 +215,7 @@ lines.push(
 lines.push('');
 
 // then we can do something similar with errors, building a table with the
-// TypeScript error code, the number of occurences, and a collapsed section
+// TypeScript error code, the number of occurrences, and a collapsed section
 // with the actual concrete errors messages for that error code.
 lines.push(
   collapsible('Our most common errors', (out) => {
