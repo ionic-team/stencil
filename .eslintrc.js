84fdb451c1708d0fae27e7d20a0a8ca4803db186
Alice Pote <alice.writes.wrongs@gmail.com>
2022-05-18 17:11:11 -0400

    some eslint config changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,14 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    // including prettier here ensures that we don't set any rules which will conflict
+    // with Prettier's formatting. Keep it last in the list so that nothing else messes
+    // with it!
+    'prettier',
   ],
   rules: {
-    "@typescript-eslint/no-unused-vars": [2]
-  }
+    '@typescript-eslint/no-unused-vars': ['error', { "argsIgnorePattern": "^_" }]
+  },
 };
