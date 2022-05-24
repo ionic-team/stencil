ba205c305a883673cb0503bc94ffdf58964a8d57
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2022-05-24 17:21:36 -0400

    cleanup url paths tests

--- src/utils/url-paths.ts
+++ src/utils/url-paths.ts
@@ -1,6 +1,11 @@
 import { isString } from './helpers';
 
-export const isRemoteUrl = (p: string) => {
+/**
+ * Determines whether a string should be considered a remote url or not
+ * @param p the string to evaluate
+ * @returns `true` if the provided string is a remote url, `false` otherwise
+ */
+export const isRemoteUrl = (p: string): boolean => {
   if (isString(p)) {
     p = p.toLowerCase();
     return p.startsWith('https://') || p.startsWith('http://');
