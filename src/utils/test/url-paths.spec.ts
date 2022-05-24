edc0e72d12fad504abda96367c9cba9f5befc76a
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2022-05-24 17:10:45 -0400

    refactor(url-paths): remove unused path helpers
    this commit removes the following unused helper functions:
    - `convertPathToFileProtocol`
    - `isFileUrl`
    
    and their associated tests

--- src/utils/test/url-paths.spec.ts
+++ src/utils/test/url-paths.spec.ts
@@ -1,20 +1,6 @@
-import { convertPathToFileProtocol, isFileUrl, isRemoteUrl } from '../url-paths';
+import { isRemoteUrl } from '../url-paths';
 
 describe('url-paths', () => {
-  it('isFileUrl', () => {
-    expect(isFileUrl('file://domain.com/file.txt')).toBe(true);
-    expect(isFileUrl('FILE://domain.com/file.txt')).toBe(true);
-    expect(isFileUrl('file:/domain.com/file.txt')).toBe(true);
-    expect(isFileUrl('file:///domain.com/file.txt')).toBe(true);
-    expect(isFileUrl('file:/domain.com/file.txt')).toBe(true);
-    expect(isFileUrl('file:///c:/domain.com/file.txt')).toBe(true);
-    expect(isFileUrl('C:/file.txt')).toBe(false);
-    expect(isFileUrl('C:\\file.txt')).toBe(false);
-    expect(isFileUrl('/User/file.txt')).toBe(false);
-    expect(isFileUrl('')).toBe(false);
-    expect(isFileUrl(null)).toBe(false);
-  });
-
   it('isRemoteUrl', () => {
     expect(isRemoteUrl('https://domain.com/file.txt')).toBe(true);
     expect(isRemoteUrl('http://domain.com/file.txt')).toBe(true);
@@ -25,18 +11,5 @@ describe('url-paths', () => {
     expect(isRemoteUrl('/User/file.txt')).toBe(false);
     expect(isRemoteUrl('')).toBe(false);
     expect(isRemoteUrl(null)).toBe(false);
-  });
-
-  it('convertPathToFileProtocol', () => {
-    // https://en.wikipedia.org/wiki/File_URI_scheme
-    expect(convertPathToFileProtocol('/User/file.txt')).toBe('file:///User/file.txt');
-    expect(convertPathToFileProtocol('C:/User/file.txt')).toBe('file:///C:/User/file.txt');
-    expect(convertPathToFileProtocol('C:\\User\\file.txt')).toBe('file:///C:/User/file.txt');
-    expect(convertPathToFileProtocol('file:/dir/file.txt')).toBe('file:/dir/file.txt');
-    expect(convertPathToFileProtocol('file://dir/file.txt')).toBe('file://dir/file.txt');
-    expect(convertPathToFileProtocol('file:///dir/file.txt')).toBe('file:///dir/file.txt');
-    expect(convertPathToFileProtocol('https://domain.com/file.txt')).toBe('https://domain.com/file.txt');
-    expect(convertPathToFileProtocol('http://domain.com/file.txt')).toBe('http://domain.com/file.txt');
-    expect(convertPathToFileProtocol(null)).toBe(null);
   });
 });
--- src/utils/url-paths.ts
+++ src/utils/url-paths.ts
@@ -1,5 +1,4 @@
 import { isString } from './helpers';
-import { normalizePath } from './normalize-path';
 
 export const isRemoteUrl = (p: string) => {
   if (isString(p)) {
@@ -7,22 +6,4 @@ export const isRemoteUrl = (p: string) => {
     return p.startsWith('https://') || p.startsWith('http://');
   }
   return false;
-};
-
-export const isFileUrl = (p: string) => (isString(p) ? p.toLowerCase().startsWith('file:/') : false);
-
-export const convertPathToFileProtocol = (p: string) => {
-  if (isString(p) && !isRemoteUrl(p) && !isFileUrl(p)) {
-    if (isFileUrl(p)) {
-      p = new URL(p).href;
-    } else {
-      p = normalizePath(p);
-      if (p.startsWith('/')) {
-        p = 'file://' + p;
-      } else {
-        p = 'file:///' + p;
-      }
-    }
-  }
-  return p;
 };
