f11135b20f3fc1ce239a91eef1e63332c814293d
Alice Pote <alice.writes.wrongs@gmail.com>
2022-05-24 10:22:52 -0400

    update test names to include output target param

--- src/compiler/output-targets/test/output-utils.spec.ts
+++ src/compiler/output-targets/test/output-utils.spec.ts
@@ -2,12 +2,12 @@ import { DIST_TYPES, isValidConfigOutputTarget, VALID_CONFIG_OUTPUT_TARGETS } fr
 
 describe('output-utils tests', () => {
   describe('isValidConfigOutputTarget', () => {
-    it.each(VALID_CONFIG_OUTPUT_TARGETS)('should return true for valid output types', (outputTargetType) => {
+    it.each(VALID_CONFIG_OUTPUT_TARGETS)('should return true for valid output type "%s"', (outputTargetType) => {
       expect(isValidConfigOutputTarget(outputTargetType)).toBe(true);
     });
 
     it.each(['', 'my-target-that-i-made-up', DIST_TYPES])(
-      'should return false for invalid config output types',
+      'should return false for invalid config output type "%s"',
       (outputTargetType) => {
         expect(isValidConfigOutputTarget(outputTargetType)).toBe(false);
       }
