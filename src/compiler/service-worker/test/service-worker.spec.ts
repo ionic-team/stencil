e8fe8d95f210201003977771c72aa9b0901cf7af
Alice Pote <alice.writes.wrongs@gmail.com>
2022-05-27 09:29:17 -0400

    add note for STENCIL-462

--- src/compiler/service-worker/test/service-worker.spec.ts
+++ src/compiler/service-worker/test/service-worker.spec.ts
@@ -1,9 +1,11 @@
 // @ts-nocheck
+// TODO(STENCIL-462): investigate getting this file to pass (remove ts-nocheck)
 import type * as d from '@stencil/core/declarations';
 import { Compiler, Config } from '@stencil/core/compiler';
 import { mockConfig } from '@stencil/core/testing';
 import path from 'path';
 
+// TODO(STENCIL-462): investigate getting this file to pass
 xdescribe('service worker', () => {
   jest.setTimeout(20000);
   let compiler: Compiler;
