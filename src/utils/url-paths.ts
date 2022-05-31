2f0a3634c3decfb47d947c397ed4dc2242c3b04f
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2022-05-31 16:29:14 -0400

    review(ap): add a comment about schems

--- src/utils/url-paths.ts
+++ src/utils/url-paths.ts
@@ -1,7 +1,11 @@
 import { isString } from './helpers';
 
 /**
- * Determines whether a string should be considered a remote url or not
+ * Determines whether a string should be considered a remote url or not.
+ *
+ * This helper only checks the provided string to evaluate is one of a few pre-defined schemes, and should not be
+ * considered all-encompassing
+ *
  * @param p the string to evaluate
  * @returns `true` if the provided string is a remote url, `false` otherwise
  */
