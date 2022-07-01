757ede8e57d39fd21d9a595c70a0aaa80af5579e
Alice Pote <alice.writes.wrongs@gmail.com>
2022-07-01 09:24:36 -0400

    move jest/recommended above prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,11 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'jsdoc', 'jest'],
   extends: [
+    'plugin:jest/recommended',
     // including prettier here ensures that we don't set any rules which will conflict
     // with Prettier's formatting. Keep it last in the list so that nothing else messes
     // with it!
     'prettier',
-    'plugin:jest/recommended'
   ],
   rules: {
     '@typescript-eslint/no-unused-vars': ['error', {
