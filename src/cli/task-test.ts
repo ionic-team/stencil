40a8e4d9dddef3062ad545071b4df8a8e52ea39b
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2022-07-01 10:11:52 -0400

    review(ap): add jsdoc to `taskTest`

--- src/cli/task-test.ts
+++ src/cli/task-test.ts
@@ -1,7 +1,11 @@
 import type { InternalStrictConfig, TestingRunOptions } from '../declarations';
 import { IS_NODE_ENV } from '../compiler/sys/environment';
 
-export const taskTest = async (config: InternalStrictConfig) => {
+/**
+ * Entrypoint for any Stencil tests
+ * @param config a validated Stencil configuration entity
+ */
+export const taskTest = async (config: InternalStrictConfig): Promise<void> => {
   if (!IS_NODE_ENV) {
     config.logger.error(`"test" command is currently only implemented for a NodeJS environment`);
     return config.sys.exit(1);
