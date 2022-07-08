ac2a8e3756c7863463f6149e4c81867382880d85
Alice Pote <alice.writes.wrongs@gmail.com>
2022-07-08 16:17:17 -0400

    chore(unused exports): remove unused export from validate-collection.ts
    We had a function called `getCollectionDistDir` defined in
    `src/compiler/config/outputs/validate-collection.ts` which does not
    appear to be used anywhere in the codebase.

--- src/compiler/config/outputs/validate-collection.ts
+++ src/compiler/config/outputs/validate-collection.ts
@@ -1,22 +1,20 @@
 import type * as d from '../../../declarations';
 import { getAbsolutePath } from '../config-utils';
 import { isOutputTargetDistCollection } from '../../output-targets/output-utils';
-import { join } from 'path';
-import { normalizePath } from '@utils';
 
-export const validateCollection = (config: d.Config, userOutputs: d.OutputTarget[]) => {
-  return userOutputs.filter(isOutputTargetDistCollection).map((o) => {
+/**
+ * Validate and return DIST_COLLECTION output targets, ensuring that the `dir`
+ * property is set on them.
+ *
+ * @param config the user-supplied configuration object
+ * @param userOutputs an array of output targets
+ * @returns an array of validated DIST_COLLECTION output targets
+ */
+export const validateCollection = (config: d.Config, userOutputs: d.OutputTarget[]): d.OutputTargetDistCollection[] => {
+  return userOutputs.filter(isOutputTargetDistCollection).map((outputTarget) => {
     return {
-      ...o,
-      dir: getAbsolutePath(config, o.dir || 'dist/collection'),
+      ...outputTarget,
+      dir: getAbsolutePath(config, outputTarget.dir || 'dist/collection'),
     };
   });
-};
-
-export const getCollectionDistDir = (config: d.Config) => {
-  const collectionOutputTarget = config.outputTargets.find(isOutputTargetDistCollection);
-  if (collectionOutputTarget) {
-    return normalizePath(collectionOutputTarget.dir);
-  }
-  return normalizePath(join(config.rootDir, 'dist', 'collection'));
 };
