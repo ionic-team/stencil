454b8280347c253ebdc189d27dd1287594d2cce7
Alice <alice.writes.wrongs@gmail.com>
2022-07-27 08:58:56 -0400

    chore(cli): remove sys param from `parseFlags` (#3489)
    This removes the `sys` parameter from the `parseFlags` function in
    `src/cli/parse-flags.ts`. This parameter was more or less deprecated
    in #3486 but was not removed at the time because `parseFlags` is part of
    Stencil's public API, so we need to wait for a major version change to
    change the type signature of the function.
    
    STENCIL-509: remove _sys param from cli/parse_flags.ts::parseFlags

--- src/cli/parse-flags.ts
+++ src/cli/parse-flags.ts
@@ -1,6 +1,6 @@
 import { readOnlyArrayHasStringMember, toCamelCase } from '@utils';
 
-import { CompilerSystem, LOG_LEVELS, LogLevel, TaskCommand } from '../declarations';
+import { LOG_LEVELS, LogLevel, TaskCommand } from '../declarations';
 import {
   BOOLEAN_CLI_FLAGS,
   CLI_FLAG_ALIASES,
@@ -18,11 +18,9 @@ import {
  * Parse command line arguments into a structured `ConfigFlags` object
  *
  * @param args an array of CLI flags
- * @param _sys an optional compiler system
  * @returns a structured ConfigFlags object
  */
-export const parseFlags = (args: string[], _sys?: CompilerSystem): ConfigFlags => {
-  // TODO(STENCIL-509): remove the _sys parameter here ^^ (for v3)
+export const parseFlags = (args: string[]): ConfigFlags => {
   const flags: ConfigFlags = createConfigFlags();
 
   // cmd line has more priority over npm scripts cmd
