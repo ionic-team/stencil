f8bdc9c77a94b4c8b49911e3b0d25f1f6e0bf061
John Jenkins <john.jenkins@nanoporetech.com>
2022-08-12 13:58:07 +0100

    fix typescript not having access to sourceFile during test

--- src/compiler/transformers/static-to-meta/formassociated.ts
+++ src/compiler/transformers/static-to-meta/formassociated.ts
@@ -3,16 +3,20 @@ import ts from 'typescript';
 export const hasFormAssociated = (statements: ts.NodeArray<ts.Statement>, classNode: ts.ClassDeclaration) => {
   const className = classNode.name.getText();
 
-  return !!statements.find(
-    (st: ts.ExpressionStatement) =>
-      st.kind === ts.SyntaxKind.ExpressionStatement &&
-      st.expression &&
-      st.expression.kind === ts.SyntaxKind.BinaryExpression &&
-      (st.expression as ts.BinaryExpression).left.kind === ts.SyntaxKind.PropertyAccessExpression &&
-      ((st.expression as ts.BinaryExpression).left as ts.PropertyAccessExpression).expression?.getText() ===
-        className &&
-      ((st.expression as ts.BinaryExpression).left as ts.PropertyAccessExpression).name?.getText() ===
-        'formAssociated' &&
-      (st.expression as ts.BinaryExpression).right.kind === ts.SyntaxKind.TrueKeyword
-  );
+  try {
+    return !!statements.find(
+      (st: ts.ExpressionStatement) =>
+        st.kind === ts.SyntaxKind.ExpressionStatement &&
+        st.expression &&
+        st.expression.kind === ts.SyntaxKind.BinaryExpression &&
+        (st.expression as ts.BinaryExpression).left.kind === ts.SyntaxKind.PropertyAccessExpression &&
+        ((st.expression as ts.BinaryExpression).left as ts.PropertyAccessExpression).expression?.getText() ===
+          className &&
+        ((st.expression as ts.BinaryExpression).left as ts.PropertyAccessExpression).name?.getText() ===
+          'formAssociated' &&
+        (st.expression as ts.BinaryExpression).right.kind === ts.SyntaxKind.TrueKeyword
+    );
+  } catch (e) {
+    return false;
+  }
 };
