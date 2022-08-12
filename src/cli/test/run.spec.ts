ionic-team/stencil

commit a6a9171a5e8aebd0070e3d6a89e41b1e9199de82

chore(config): make sys required on ValidatedConfig (#3491)
this commit makes the `sys` property required on `ValidatedConfig`. this
work decreases the number of `strictNullCheck` violations in the
codebase. it does not fix every violation pertaining to `config.sys`
usage - rather, it seeks to be the minimal work necessary to

1. make the `sys` property required on `ValidatedConfig`
2. prevent introducing breaking compilation errors

src/cli/run.ts
@@ -113,18 +113,27 @@ export const run = async (init: d.CliInitOptions) => {
   }
 };
 
+/**
+ * Run a specified task
+ * @param coreCompiler an instance of a minimal, bootstrap compiler for running the specified task
+ * @param config a configuration for the Stencil project to apply to the task run
+ * @param task the task to run
+ * @param sys the {@link CompilerSystem} for interacting with the operating system
+ * @public
+ */
 export const runTask = async (
   coreCompiler: CoreCompiler,
   config: d.Config,
   task: d.TaskCommand,
   sys?: d.CompilerSystem
-) => {
+): Promise<void> => {
   const logger = config.logger ?? createLogger();
   const strictConfig: ValidatedConfig = {
     ...config,
     flags: createConfigFlags(config.flags ?? { task }),
     logger,
     outputTargets: config.outputTargets ?? [],
+    sys: sys ?? coreCompiler.createSystem({ logger }),
   };
 
   switch (task) {

src/cli/telemetry/test/telemetry.spec.ts
@@ -2,7 +2,7 @@ import type * as d from '../../../declarations';
 import * as telemetry from '../telemetry';
 import * as shouldTrack from '../shouldTrack';
 import { createSystem } from '../../../compiler/sys/stencil-sys';
-import { mockLogger, mockValidatedConfig } from '@stencil/core/testing';
+import { mockValidatedConfig } from '@stencil/core/testing';
 import * as coreCompiler from '@stencil/core/compiler';
 import { anonymizeConfigForTelemetry } from '../telemetry';
 import { DIST, DIST_CUSTOM_ELEMENTS, DIST_HYDRATE_SCRIPT, WWW } from '../../../compiler/output-targets/output-utils';
@@ -148,25 +148,21 @@ describe('prepareData', () => {
   let sys: d.CompilerSystem;
 
   beforeEach(() => {
-    config = {
-      outputTargets: [],
-      flags: createConfigFlags(),
-      logger: mockLogger(),
-    };
-
-    sys = createSystem();
+    config = mockValidatedConfig();
+    sys = config.sys;
+    // set static name + versions, otherwise tests will pull in the dev build's data (which changes per build)
+    sys.name = 'in-memory';
+    sys.version = '__VERSION:STENCIL__';
   });
 
-  it('provides an object', async () => {
+  it('prepares an object to send to ionic', async () => {
     const data = await telemetry.prepareData(coreCompiler, config, sys, 1000);
     expect(data).toEqual({
       arguments: [],
       build: coreCompiler.buildId,
       component_count: undefined,
-      config: {
-        flags: createConfigFlags(),
-        outputTargets: [],
-      },
+      // the configuration generation is tested elsewhere, just verify we're sending something under this flag
+      config: expect.any(Object),
       cpu_model: '',
       duration_ms: 1000,
       has_app_pwa_config: false,
@@ -185,100 +181,38 @@ describe('prepareData', () => {
     });
   });
 
-  it('updates when there is a PWA config', async () => {
-    const config: d.ValidatedConfig = {
-      flags: createConfigFlags(),
-      logger: mockLogger(),
-      outputTargets: [{ type: 'www', baseUrl: 'https://example.com', serviceWorker: { swDest: './tmp' } }],
-    };
+  describe('has_app_pwa_config property', () => {
+    it('sets `has_app_pwa_config` to true when there is a service worker', async () => {
+      const config = mockValidatedConfig({
+        outputTargets: [{ type: 'www', baseUrl: 'https://example.com' }],
+      });
 
-    const data = await telemetry.prepareData(coreCompiler, config, sys, 1000);
+      const data = await telemetry.prepareData(coreCompiler, config, sys, 1000);
 
-    expect(data).toEqual({
-      arguments: [],
-      build: coreCompiler.buildId,
-      component_count: undefined,
-      config: {
-        flags: {
-          args: [],
-          knownArgs: [],
-          task: null,
-          unknownArgs: [],
-        },
-        outputTargets: [
-          {
-            baseUrl: 'omitted',
-            serviceWorker: {
-              swDest: 'omitted',
-            },
-            type: 'www',
-          },
-        ],
-      },
-      cpu_model: '',
-      duration_ms: 1000,
-      has_app_pwa_config: true,
-      os_name: '',
-      os_version: '',
-      packages: [],
-      packages_no_versions: [],
-      rollup: coreCompiler.versions.rollup,
-      stencil: coreCompiler.versions.stencil,
-      system: 'in-memory __VERSION:STENCIL__',
-      system_major: 'in-memory __VERSION:STENCIL__',
-      targets: ['www'],
-      task: null,
-      typescript: coreCompiler.versions.typescript,
-      yarn: false,
+      expect(data.has_app_pwa_config).toBe(true);
+    });
+
+    it("sets `has_app_pwa_config` to true for a non '/' baseUrl", async () => {
+      const config = mockValidatedConfig({
+        outputTargets: [{ type: 'www', serviceWorker: { swDest: './tmp' } }],
+      });
+
+      const data = await telemetry.prepareData(coreCompiler, config, sys, 1000);
+
+      expect(data.has_app_pwa_config).toBe(true);
     });
   });
 
-  it('updates when there is a component count passed in', async () => {
-    const config: d.ValidatedConfig = {
-      flags: createConfigFlags(),
-      logger: mockLogger(),
-      outputTargets: [{ type: 'www', baseUrl: 'https://example.com', serviceWorker: { swDest: './tmp' } }],
-    };
+  it('sends a component count when one is provided', async () => {
+    const COMPONENT_COUNT = 12;
 
-    const data = await telemetry.prepareData(coreCompiler, config, sys, 1000, 12);
-
-    expect(data).toEqual({
-      arguments: [],
-      build: coreCompiler.buildId,
-      component_count: 12,
-      config: {
-        flags: {
-          args: [],
-          knownArgs: [],
-          task: null,
-          unknownArgs: [],
-        },
-        outputTargets: [
-          {
-            baseUrl: 'omitted',
-            serviceWorker: {
-              swDest: 'omitted',
-            },
-            type: WWW,
-          },
-        ],
-      },
-      cpu_model: '',
-      duration_ms: 1000,
-      has_app_pwa_config: true,
-      os_name: '',
-      os_version: '',
-      packages: [],
-      packages_no_versions: [],
-      rollup: coreCompiler.versions.rollup,
-      stencil: coreCompiler.versions.stencil,
-      system: 'in-memory __VERSION:STENCIL__',
-      system_major: 'in-memory __VERSION:STENCIL__',
-      targets: ['www'],
-      task: null,
-      typescript: coreCompiler.versions.typescript,
-      yarn: false,
+    const config = mockValidatedConfig({
+      outputTargets: [{ type: 'www' }],
     });
+
+    const data = await telemetry.prepareData(coreCompiler, config, sys, 1000, COMPONENT_COUNT);
+
+    expect(data.component_count).toEqual(COMPONENT_COUNT);
   });
 });
 

src/compiler/compiler.ts
@@ -1,4 +1,4 @@
-import type { Compiler, Config, Diagnostic } from '../declarations';
+import type { Compiler, Config, Diagnostic, ValidatedConfig } from '../declarations';
 import { Cache } from './cache';
 import { CompilerContext } from './build/compiler-ctx';
 import { createFullBuild } from './build/full-build';
@@ -22,7 +22,7 @@ export const createCompiler = async (userConfig: Config): Promise<Compiler> => {
   // actual compiler code
   // could be in a web worker on the browser
   // or the main thread in node
-  const config = getConfig(userConfig);
+  const config: ValidatedConfig = getConfig(userConfig);
   const diagnostics: Diagnostic[] = [];
   const sys = config.sys;
   const compilerCtx = new CompilerContext();

src/compiler/config/load-config.ts
@@ -46,9 +46,13 @@ export const loadConfig = async (init: LoadConfigInit = {}): Promise<LoadConfigR
   const unknownConfig: UnvalidatedConfig = {};
 
   try {
-    const sys = init.sys || createSystem();
     const config = init.config || {};
     let configPath = init.configPath || config.configPath;
+
+    // Pull the {@link CompilerSystem} out of the initialization object, or create one if it does not exist.
+    // This entity is needed to load the project's configuration (and therefore needs to be created before it can be
+    // attached to a configuration entity, validated or otherwise)
+    const sys = init.sys ?? createSystem();
 
     const loadedConfigFile = await loadConfigFile(sys, results.diagnostics, configPath);
     if (hasError(results.diagnostics)) {

src/compiler/config/validate-config.ts
@@ -12,6 +12,7 @@ import { validateRollupConfig } from './validate-rollup-config';
 import { validateTesting } from './validate-testing';
 import { validateWorkers } from './validate-workers';
 import { createLogger } from '../sys/logger/console-logger';
+import { createSystem } from '../sys/stencil-sys';
 
 /**
  * Represents the results of validating a previously unvalidated configuration
@@ -41,7 +42,7 @@ export const validateConfig = (
   userConfig: UnvalidatedConfig = {},
   bootstrapConfig: LoadConfigInit
 ): ConfigValidationResults => {
-  const config = Object.assign({}, userConfig || {}); // not positive it's json safe
+  const config = Object.assign({}, userConfig); // not positive it's json safe
   const diagnostics: Diagnostic[] = [];
 
   const logger = bootstrapConfig.logger || config.logger || createLogger();
@@ -52,6 +53,7 @@ export const validateConfig = (
     flags: JSON.parse(JSON.stringify(config.flags || {})),
     logger,
     outputTargets: config.outputTargets ?? [],
+    sys: config.sys ?? bootstrapConfig.sys ?? createSystem({ logger }),
   };
 
   // default devMode false

src/compiler/sys/config.ts
@@ -11,11 +11,8 @@ export const getConfig = (userConfig: d.Config): d.ValidatedConfig => {
     flags: createConfigFlags(userConfig.flags ?? {}),
     logger,
     outputTargets: userConfig.outputTargets ?? [],
+    sys: userConfig.sys ?? createSystem({ logger }),
   };
-
-  if (!config.sys) {
-    config.sys = createSystem({ logger: config.logger });
-  }
 
   setPlatformPath(config.sys.platformPath);
 

src/declarations/stencil-public-compiler.ts
@@ -409,7 +409,7 @@ type RequireFields<T, K extends keyof T> = T & { [P in K]-?: T[P] };
 /**
  * Fields in {@link Config} to make required for {@link ValidatedConfig}
  */
-type StrictConfigFields = 'flags' | 'logger' | 'outputTargets';
+type StrictConfigFields = 'flags' | 'logger' | 'outputTargets' | 'sys';
 
 /**
  * A version of {@link Config} that makes certain fields required. This type represents a valid configuration entity.

src/testing/mocks.ts
@@ -35,6 +35,7 @@ export function mockValidatedConfig(overrides: Partial<ValidatedConfig> = {}): V
     flags: createConfigFlags(),
     logger: mockLogger(),
     outputTargets: baseConfig.outputTargets ?? [],
+    sys: createTestingSystem(),
     ...overrides,
   };
 }

commit 7ef0bfd956dc0a247922e4a07234afe3e175bc73

fix(task): consider config sys in task runner
this commit updates the generation of a `ValidatedConfig` entity to take
a `sys` property on a user provided `Config` in the context of
`runTask`.

when generating the validated configuration, the `sys` entity takes
precedence over all other configurations, with the belief that in the
event argument was provided, it is the intent of the user to use it. in
the event `sys` is not expressly provided, use the one on the provided
configuration entity. this is useful in the case that a user used
stencil's external facing apis in a flow that:
1. loads a configuration
2. validates that configuration
3. uses that configuration when calling `runTask` directly

this commit fixes a bug introduced making `sys` a required field on
`ValidatedConfig`, where `config.sys` was not properly accounted for.
the bug was initially introduced in
a6a9171a5e8aebd0070e3d6a89e41b1e9199de82 (#3491)

src/cli/run.ts
@@ -133,7 +133,7 @@ export const runTask = async (
     flags: createConfigFlags(config.flags ?? { task }),
     logger,
     outputTargets: config.outputTargets ?? [],
-    sys: sys ?? coreCompiler.createSystem({ logger }),
+    sys: sys ?? config.sys ?? coreCompiler.createSystem({ logger }),
     testing: config.testing ?? {},
   };
 

src/cli/test/run.spec.ts
@@ -177,6 +177,38 @@ describe('run', () => {
       taskTestSpy.mockRestore();
     });
 
+    describe('default configuration', () => {
+      describe('sys property', () => {
+        it('uses the sys argument if one is provided', async () => {
+          // remove the `CompilerSystem` on the config, just to be sure we don't accidentally use it
+          unvalidatedConfig.sys = undefined;
+
+          validatedConfig = mockValidatedConfig({ sys });
+
+          await runTask(coreCompiler, unvalidatedConfig, 'build', sys);
+
+          // first validate there was one call, and that call had two arguments
+          expect(taskBuildSpy).toHaveBeenCalledTimes(1);
+          expect(taskBuildSpy).toHaveBeenCalledWith(coreCompiler, validatedConfig);
+
+          const compilerSystemUsed: d.CompilerSystem = taskBuildSpy.mock.calls[0][1].sys;
+          expect(compilerSystemUsed).toBe(sys);
+        });
+
+        it('uses the sys field on the config if no sys argument is provided', async () => {
+          // if the optional `sys` argument isn't provided, attempt to default to the one on the config
+          await runTask(coreCompiler, unvalidatedConfig, 'build');
+
+          // first validate there was one call, and that call had two arguments
+          expect(taskBuildSpy).toHaveBeenCalledTimes(1);
+          expect(taskBuildSpy).toHaveBeenCalledWith(coreCompiler, validatedConfig);
+
+          const compilerSystemUsed: d.CompilerSystem = taskBuildSpy.mock.calls[0][1].sys;
+          expect(compilerSystemUsed).toBe(unvalidatedConfig.sys);
+        });
+      });
+    });
+
     it('calls the build task', async () => {
       await runTask(coreCompiler, unvalidatedConfig, 'build', sys);
 