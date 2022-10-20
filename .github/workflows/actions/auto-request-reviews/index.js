8d0aa087f713736609d72d4b79732e1b789f9a81
Tanner Reits <tanner@ionic.io>
2022-10-20 16:28:26 -0400

    wip

--- .github/workflows/actions/auto-request-reviews/index.js
+++ .github/workflows/actions/auto-request-reviews/index.js
@@ -1,5 +1,5 @@
-import * as core from '@actions/core';
-import * as github from '@actions/github';
+const core = require('@actions/core');
+const github = require('@actions/github');
 
 const ORG = 'ionic-team';
 const STENCIL_TEAM_SLUG = 'stencil';
