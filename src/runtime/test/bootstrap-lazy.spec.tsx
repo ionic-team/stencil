b07140c62efcfbf9cb63a6f1fefc485e74218880
Robin Aldenhoven <raldenhoven@bol.com>
2022-10-28 17:27:26 +0200

    Added example

---
+++ src/runtime/test/bootstrap-lazy.spec.tsx
@@ -0,0 +1,27 @@
+import { bootstrapLazy } from '../bootstrap-lazy';
+import { doc } from '@platform';
+
+
+describe('assets', () => {
+
+
+  it('should not append broken css', async () => {
+    const spy = jest.spyOn(doc.head, 'insertBefore');
+
+    /**
+     * To make the test shorter I called bootstrapLazy without any bundles
+     * When a user call defineCustomElements multiple times it will prevent defining the same elements within
+     * the bootstrapLazy method. Although this works it does still add the broken css.
+     */
+    bootstrapLazy([]);
+
+    expect(spy).not.toHaveBeenCalledWith(expect.objectContaining({
+      sheet: expect.objectContaining({
+        cssRules: [expect.objectContaining({
+          // This html is not valid since it does not start with a selector for the visibility hidden block
+          cssText: '{visibility:hidden}.hydrated{visibility:inherit}'
+        })]
+      })
+    }), null);
+  });
+});
