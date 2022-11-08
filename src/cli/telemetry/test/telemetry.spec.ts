ec53d63308f0f46ba8e360fbcd556a1cdcc229a6
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2022-11-08 16:33:29 -0500

    chore(): alphabetize keys to delete
    this commit alphabetizes the collection of strings that represents the
    keys to delete

--- src/cli/telemetry/test/telemetry.spec.ts
+++ src/cli/telemetry/test/telemetry.spec.ts
@@ -265,7 +265,7 @@ describe('anonymizeConfigForTelemetry', () => {
     expect(anonymizedConfig.outputTargets).toEqual([]);
   });
 
-  it.each<keyof d.ValidatedConfig>(['commonjs', 'env', 'rollupConfig', 'sys', 'testing', 'logger', 'devServer', 'tsCompilerOptions'])(
+  it.each<keyof d.ValidatedConfig>(['commonjs','devServer', 'env', 'logger', 'rollupConfig', 'sys', 'testing', 'tsCompilerOptions'])(
     "should remove objects under prop '%s'",
     (prop: keyof d.ValidatedConfig) => {
       const anonymizedConfig = anonymizeConfigForTelemetry({ ...config, [prop]: {}, outputTargets: [] });
