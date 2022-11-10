5c83c57d6c1ee76c85bd9931b19adb8aa8c756ff
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2022-11-10 10:17:52 -0500

    chore(scripts): remove try/catch from entrypoint
    this commit removes the try/catch from the entrypoint of the scripts
    project, with the intent of moving the require statement to an import
    one - thus removing the need for try/catch

--- scripts/index.js
+++ scripts/index.js
@@ -7,14 +7,7 @@ function main() {
   const scriptsBuildDir = path.join(scriptsDir, 'build');
   const scriptsBuildJs = path.join(scriptsBuildDir, 'build.js');
   const args = process.argv.slice(2);
-
-  let build;
-  try {
-    build = require(scriptsBuildJs);
-  } catch (requireError) {
-    console.warn(`Unable to load build scripts: ${requireError}. Attempting to recompile them.`);
-    process.exit(1);
-  }
+  const build = require(scriptsBuildJs);
   build.run(rootDir, args);
 }
 
