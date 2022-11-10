d2db01138baed004a2a7dd5d98cc2f346f867fe0
Tanner Reits <tanner@ionic.io>
2022-11-10 14:00:18 -0500

    wip(auto-assign-action): update API calls used

--- .github/workflows/actions/auto-request-reviews/index.js
+++ .github/workflows/actions/auto-request-reviews/index.js
@@ -9723,27 +9723,25 @@ async function run() {
   // PR author
   const username = github.context.payload.pull_request.user.login;
 
-  const { data } = await octokit.rest.orgs.listMembers({
+  // Check if the PR author is a member of the organization
+  const { status } = await octokit.rest.orgs.checkMembershipForUser({
     org: organization,
+    username,
   });
 
-  console.log('USERS', data);
-
-  const member = data.find((member) => member.login === username);
-
-  console.log('member', member);
-
-  // If the user is a member of the org, we will directly assign the members of the Stencil team
-  // Otherwise, we will assign the team itself as a reviewer
-  if (member != null) {
-    const stencilTeamMembers = await octokit.rest.teams.listMembersInOrg({
+  // 204 means that the both the requester (the action's authorization) and the PR author
+  // are org members, so we should assign the PR accordingly
+  if (status === 204) {
+    const { data: stencilTeamMembers } = await octokit.rest.teams.listMembersInOrg({
       org: organization,
       // Adding the team will only return the members in the org
       // that are in the team
       team_slug: teamSlug,
     });
-    console.log('STENCIL MEMBERS', stencilTeamMembers);
-    const reviewers = stencilTeamMembers.filter((ref) => ref.login !== username);
+
+    const reviewers = stencilTeamMembers.filter((ref) => ref.login !== username).map((ref) => ref.login);
+
+    console.log('REVIEWERS', reviewers);
 
     await octokit.rest.pulls.requestReviewers({
       owner: github.context.repo.owner,
