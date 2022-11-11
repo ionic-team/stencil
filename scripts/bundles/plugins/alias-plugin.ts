ff9fa7f1fb702cfbe1d9fa280d565ab55a8c6911
Alice Pote <alice.writes.wrongs@gmail.com>
2022-11-11 11:48:22 -0500

    refactor(build): use 'paths' set in TS config in alias plugin
    This refactors our 'alias' rollup plugin to determine what paths to
    resolve things to based on the settings we have in `tsconfig.json`.
    
    This is relevant for the mappings that we have set in `paths`, where we
    map things like `@utils` to `src/utils/index.ts`. TypeScript, because
    reasons, does not provide an option to automatically resolve those paths
    to normal relative imports when compiling.
    
    Our solution to this at present is to maintain a list of modules which
    should be rewritten in this `alias` plugin. However, the information
    about which module names map to which files is already set in our
    `tsconfig.json`! So it would be nicer if we didn't have to maintain that
    information in two places.
    
    This change modifies the `alias` plugin so that it looks up module names
    in the `paths` object in `tsconfig` and then uses TypeScript's module
    resolution helper function to ensure it has the right path for the
    module, and then re-writes the path to point to the right compiled file
    in the built output.
    
    Note that (unfortunately) this change cannot do the same thing for our
    `.d.ts` files :( We'll need to figure out another solution to hopefully
    copy those automatically and re-write import paths within them to point
    to the right places.

--- scripts/bundles/plugins/alias-plugin.ts
+++ scripts/bundles/plugins/alias-plugin.ts
@@ -1,5 +1,6 @@
 import { join } from 'path';
 import type { PartialResolvedId, Plugin } from 'rollup';
+import { CompilerOptions, nodeModuleNameResolver, parseConfigFileTextToJson, sys } from 'typescript';
 
 import type { BuildOptions } from '../../utils/options';
 
@@ -30,40 +31,101 @@ export function aliasPlugin(opts: BuildOptions): Plugin {
     'chalk',
   ]);
 
+  const compilerOptions = getTsCompilerOptions();
+  const { outDir, paths } = compilerOptions;
+
   return {
     name: 'aliasPlugin',
     /**
-     * A rollup build hook for resolving identifiers. [Source](https://rollupjs.org/guide/en/#resolveid)
-     * @param id the importee exactly as it is written in an import statement in the source code
-     * @returns a resolution to an import to a different id, potentially externalizing it from the bundle simultaneously
+     * A rollup build hook for resolving identifiers. See Rollup's
+     * documentation here: https://rollupjs.org/guide/en/#resolveid
+     *
+     * This plugin takes care of:
+     *
+     * 1. Marking some modules as external so imports from them aren't bundled
+     * 2. Ensuring that for some modules we always import the same one
+     * 3. Resolving the module aliases we set in `tsconfig.json` to the
+     * correct path to the corresponding compiled output
+     *
+     * @param importee the importee exactly as it is written in an import
+     * statement in the source code
+     * @param importer the path to the module which imports the importee
+     * @returns a resolution to an import to a different id, potentially
+     * externalizing it from the bundle simultaneously
      */
-    resolveId(id: string): PartialResolvedId | string | null {
-      const externalId = alias.get(id);
+    resolveId(importee: string, importer: string): PartialResolvedId | string | null {
+      // this indicates it's a 'virtual' module created by Rollup
+      if (typeof importer === 'undefined' || importee.startsWith('\0')) {
+        return null;
+      }
+
+      // if a module is on our list of 'blessed' external modules we mark it as such
+      const externalId = alias.get(importee);
       if (externalId) {
         return {
           id: externalId,
           external: true,
         };
       }
-      if (id === '@runtime') {
-        return join(opts.buildDir, 'runtime', 'index.js');
+
+      if (helperResolvers.has(importee)) {
+        return join(opts.bundleHelpersDir, `${importee}.js`);
       }
-      if (id === '@utils') {
-        return join(opts.buildDir, 'utils', 'index.js');
-      }
-      if (id === '@environment') {
-        return join(opts.buildDir, 'compiler', 'sys', 'environment.js');
-      }
-      if (helperResolvers.has(id)) {
-        return join(opts.bundleHelpersDir, `${id}.js`);
-      }
-      if (empty.has(id)) {
+      if (empty.has(importee)) {
         return join(opts.bundleHelpersDir, 'empty.js');
       }
-      if (nodeResolvers.has(id)) {
-        return nodeResolvers.get(id);
+      if (nodeResolvers.has(importee)) {
+        return nodeResolvers.get(importee);
       }
-      return null;
+
+      const hasMatchingPath = Object.keys(paths).some((path) => importee.includes(path));
+
+      // starting with '.' indicates this is a normal relative import, we don't
+      // need to do anything with that, and if we didn't match any of the paths
+      // defined in `tsconfig.json` we likewise don't need to do anything
+      if (importee.startsWith('.') || !hasMatchingPath) {
+        return null;
+      }
+
+      const { resolvedModule } = nodeModuleNameResolver(importee, importer, compilerOptions, sys);
+
+      let { resolvedFileName } = resolvedModule;
+
+      if (!resolvedFileName || resolvedFileName.endsWith('.d.ts')) {
+        // we don't resolve `.d.ts` files because that could result in a JS file trying to import one...
+        return null;
+      }
+
+      // something like src/foodir/dir.ts will be the resolved filename, so we
+      // slice the `src/` off the beginning
+      if (LEADING_SRC_REGEX.test(resolvedFileName)) {
+        resolvedFileName = resolvedFileName.replace(LEADING_SRC_REGEX, '');
+      }
+
+      const pathToCompiledImportee = join(outDir, resolvedFileName.replace(/\.tsx?$/i, '.js'));
+
+      return sys.resolvePath(pathToCompiledImportee);
     },
   };
 }
+
+/**
+ * A regular expression helpful for matching relative paths that start with
+ * 'src/', like 'src/foo/bar.ts'.
+ */
+const LEADING_SRC_REGEX = /^src\//;
+
+/**
+ * Read the project's `tsconfig.json` and return the {@link CompilerOptions}
+ * that we define therein.
+ *
+ * @returns the compiler options that we set for typescript
+ */
+const getTsCompilerOptions = (): CompilerOptions => {
+  const configJson = sys.readFile('./tsconfig.json');
+
+  const { config } = parseConfigFileTextToJson('./tsconfig.json', configJson);
+  const { compilerOptions } = config;
+
+  return compilerOptions;
+};
