266e50561e94021fe0f8fd1e9eda4c6f1f8c2ee4
Tanner Reits <tanner@ionic.io>
2022-11-15 15:41:21 -0500

    fix(): pr feedback
    This commit updates the auto-assign action to use TS, have its own `package.json`, and add null/empty checks for action params

--- .github/workflows/actions/auto-request-reviews/main.ts
+++ .github/workflows/actions/auto-request-reviews/main.ts
@@ -1,15 +1,25 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import * as core from '@actions/core';
+import * as github from '@actions/github';
 
 async function run() {
   const token = core.getInput('token');
+  if (token == null || !token.length) {
+    throw new Error('No Github token provided');
+  }
   const octokit = github.getOctokit(token);
 
   const organization = core.getInput('org');
+  if (organization == null || !organization.length) {
+    throw new Error('No Github organization specified');
+  }
+
   const teamSlug = core.getInput('team');
+  if (teamSlug == null || !teamSlug.length) {
+    throw new Error('No Github team specified');
+  }
 
   // PR author
-  const username = github.context.payload.pull_request.user.login;
+  const username = github.context.payload.pull_request?.user.login as string;
 
   // Check if the PR author is a member of the organization
   const { status } = await octokit.rest.orgs.checkMembershipForUser({
@@ -19,15 +29,16 @@ async function run() {
 
   // 204 means that both the requester (the action's authorization) and the PR author
   // are org members, so we should assign the PR to team members accordingly
-  if (status === 204) {
-    const { data: stencilTeamMembers } = await octokit.rest.teams.listMembersInOrg({
+  // Need to cast to a number because the octokit type generics assume this will only return a 302 which is not true
+  if ((status as number) === 204) {
+    const { data: teamMembers } = await octokit.rest.teams.listMembersInOrg({
       org: organization,
       // Adding the team will only return the members in the org
-      // that are in the team
+      // that are also in the team
       team_slug: teamSlug,
     });
 
-    const reviewers = stencilTeamMembers.filter((ref) => ref.login !== username).map((ref) => ref.login);
+    const reviewers = teamMembers.filter((ref) => ref.login !== username).map((ref) => ref.login);
 
     await octokit.rest.pulls.requestReviewers({
       owner: github.context.repo.owner,
@@ -38,8 +49,7 @@ async function run() {
   }
 }
 
-try {
-  run();
-} catch (error) {
+run().catch((error) => {
+  console.error(error);
   core.setFailed(error.message);
-}
+});
