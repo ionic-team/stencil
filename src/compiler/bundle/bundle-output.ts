87554b3b6683555a3f734abb0c12241dcdb10288
Alice Pote <alice.writes.wrongs@gmail.com>
2022-11-15 09:58:09 -0500

    handler?

--- src/compiler/bundle/bundle-output.ts
+++ src/compiler/bundle/bundle-output.ts
@@ -81,7 +81,7 @@ export const getRollupOptions = (
   const orgNodeResolveId2 = (nodeResolvePlugin.resolveId = async function (importee: string, importer: string) {
     const [realImportee, query] = importee.split('?');
     // @ts-ignore
-    const resolved = await orgNodeResolveId.call(nodeResolvePlugin, realImportee, importer);
+    const resolved = await orgNodeResolveId.handler.call(nodeResolvePlugin, realImportee, importer);
     if (resolved) {
       if (isString(resolved)) {
         return query ? resolved + '?' + query : resolved;
