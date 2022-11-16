edf2c3c1721baeabd214a04b11c9ab4cfeb3fcb5
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2022-11-16 10:28:54 -0500

    review(ap): fix grammar error ('of')
    Co-authored-by: Alice <alice.writes.wrongs@gmail.com>

--- src/compiler/config/validate-copy.ts
+++ src/compiler/config/validate-copy.ts
@@ -3,7 +3,7 @@ import { unique } from '@utils';
 import type * as d from '../../declarations';
 
 /**
- * Validate a series {@link d.CopyTask}s
+ * Validate a series of {@link d.CopyTask}s
  * @param copy the copy tasks to validate, or a boolean to specify if copy tasks are enabled
  * @param defaultCopy default copy tasks to add to the returned validated list if not present in the first argument
  * @returns the validated copy tasks. this function _may_ return an empty list if the first argument is:
