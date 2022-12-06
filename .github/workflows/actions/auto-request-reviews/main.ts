2deef93358adba427cee3be19269cef8c53d352b
Tanner Reits <tanner@ionic.io>
2022-12-06 09:26:02 -0500

    fix(): update to use separate read/write tokens
    This commit updates the PR auto-assign action to use separate Github tokens for the read and write operations. We don't have a single token with all necessary permissions, so we need to accept two tokens

--- .github/workflows/actions/auto-request-reviews/main.ts
+++ .github/workflows/actions/auto-request-reviews/main.ts
@@ -2,11 +2,15 @@ import { getInput, setFailed } from '@actions/core';
 import { context, getOctokit } from '@actions/github';
 
 async function run() {
-  const token = getInput('token');
-  if (token == null || !token.length) {
-    throw new Error('No Github token provided');
+  const writeToken = getInput('writeToken');
+  if (writeToken == null || !writeToken.length) {
+    throw new Error('No Github write token provided');
   }
-  const octokit = getOctokit(token);
+
+  const readToken = getInput('orgReadToken');
+  if (readToken == null || !readToken.length) {
+    throw new Error('No Github read token provided');
+  }
 
   const organization = getInput('org');
   if (organization == null || !organization.length) {
@@ -18,11 +22,14 @@ async function run() {
     throw new Error('No Github team specified');
   }
 
+  const writeOctokit = getOctokit(writeToken);
+  const readOctokit = getOctokit(readToken);
+
   // PR author
   const username = context.payload.pull_request?.user.login as string;
 
   // Check if the PR author is a member of the organization
-  const { status } = await octokit.rest.orgs.checkMembershipForUser({
+  const { status } = await readOctokit.rest.orgs.checkMembershipForUser({
     org: organization,
     username,
   });
@@ -31,7 +38,7 @@ async function run() {
   // are org members, so we should assign the PR to team members accordingly
   // Need to cast to a number because the octokit type generics assume this will only return a 302 which is not true
   if ((status as number) === 204) {
-    const { data: teamMembers } = await octokit.rest.teams.listMembersInOrg({
+    const { data: teamMembers } = await readOctokit.rest.teams.listMembersInOrg({
       org: organization,
       // Adding the team will only return the members in the org
       // that are also in the team
@@ -40,7 +47,7 @@ async function run() {
 
     const reviewers = teamMembers.filter((ref) => ref.login !== username).map((ref) => ref.login);
 
-    await octokit.rest.pulls.requestReviewers({
+    await writeOctokit.rest.pulls.requestReviewers({
       owner: context.repo.owner,
       repo: context.repo.repo,
       pull_number: context.payload.number,
