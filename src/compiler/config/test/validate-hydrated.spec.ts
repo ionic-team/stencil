dc0f3df642ac46ef59ecc0f3a180a310f418b51c
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2022-12-06 15:10:11 -0500

    review(): fix validate-hydrated

--- src/compiler/config/test/validate-hydrated.spec.ts
+++ src/compiler/config/test/validate-hydrated.spec.ts
@@ -14,11 +14,24 @@ describe('validate-hydrated', () => {
       };
     });
 
-    it.each([null, [], true, false])('returns undefined for hydratedFlag=%s', (badValue) => {
+    it.each([null, false])('returns undefined for hydratedFlag=%s', (badValue) => {
       // this test explicitly checks for a bad value in the stencil.config file, hence the type assertion
       (inputConfig.hydratedFlag as any) = badValue;
       const actual = validateHydrated(inputConfig);
       expect(actual).toBeUndefined();
+    });
+
+    it.each([[], true])('returns a default value when hydratedFlag=%s', (badValue) => {
+      // this test explicitly checks for a bad value in the stencil.config file, hence the type assertion
+      (inputConfig.hydratedFlag as any) = badValue;
+      const actual = validateHydrated(inputConfig);
+      expect(actual).toEqual<d.HydratedFlag>({
+        hydratedValue: 'inherit',
+        initialValue: 'hidden',
+        name: 'hydrated',
+        property: 'visibility',
+        selector: 'class',
+      });
     });
   });
 });
