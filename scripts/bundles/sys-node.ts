ionic-team/stencil

commit 6702eaee806f11bfb1b0993d37306d7cd3bf18a5

chore(deps): upgrade to webpack v5 (#4073)
this commit updates stencil version of webpack to v5. webpack is used in
stencil for the purpose of bundling external dependencies (said deps can
be found in `scripts/bundles/sys-node-bundle-cache` after a build).

in order to perform the migration the steps contained in
https://webpack.js.org/migrate/5/ were followed. specifically:
- old typings were removed
- the webpack configuration was un-lined to validate the types of the
  object
- old node types, `process` and `Buffer` were removed, as they are no
  longer supported in v5 (and were unused to start with)
- checks against the `request` object being falsy in the `externals`
  function were added

scripts/bundles/sys-node.ts
@@ -3,7 +3,7 @@ import rollupResolve from '@rollup/plugin-node-resolve';
 import fs from 'fs-extra';
 import { join } from 'path';
 import type { RollupOptions } from 'rollup';
-import webpack from 'webpack';
+import webpack, { Configuration } from 'webpack';
 
 import { getBanner } from '../utils/banner';
 import type { BuildOptions } from '../utils/options';
@@ -132,83 +132,80 @@ function bundleExternal(opts: BuildOptions, outputDir: string, cachedDir: string
     }
 
     const whitelist = new Set(['child_process', 'os', 'typescript']);
-
-    webpack(
-      {
-        entry: join(opts.srcDir, 'sys', 'node', 'bundles', entryFileName),
-        output: {
-          path: outputDir,
-          filename: entryFileName,
-          libraryTarget: 'commonjs',
-        },
-        target: 'node',
-        node: {
-          __dirname: false,
-          __filename: false,
-          process: false,
-          Buffer: false,
-        },
-        externals(_context, request, callback) {
-          if (request.match(/^(\.{0,2})\//)) {
-            // absolute and relative paths are not externals
-            return callback(null, undefined);
-          }
-
-          if (request === '@stencil/core/mock-doc') {
-            return callback(null, '../../mock-doc');
-          }
-
-          if (whitelist.has(request)) {
-            // we specifically do not want to bundle these imports
-            require.resolve(request);
-            return callback(null, request);
-          }
-
-          // bundle this import
-          callback(undefined, undefined);
-        },
-        resolve: {
-          alias: {
-            '@utils': join(opts.buildDir, 'utils', 'index.js'),
-            postcss: join(opts.nodeModulesDir, 'postcss'),
-            'source-map': join(opts.nodeModulesDir, 'source-map'),
-            chalk: join(opts.bundleHelpersDir, 'empty.js'),
-          },
-        },
-        optimization: {
-          minimize: false,
-        },
-        mode: 'production',
+    const webpackConfig: Configuration = {
+      entry: join(opts.srcDir, 'sys', 'node', 'bundles', entryFileName),
+      output: {
+        path: outputDir,
+        filename: entryFileName,
+        libraryTarget: 'commonjs',
       },
-      async (err, stats) => {
-        const { minify } = await import('terser');
+      target: 'node',
+      node: {
+        __dirname: false,
+        __filename: false,
+      },
+      externals(data, callback) {
+        const { request } = data;
 
-        if (err && err.message) {
-          rejectBundle(err);
+        if (request?.match(/^(\.{0,2})\//)) {
+          // absolute and relative paths are not externals
+          return callback(null, undefined);
+        }
+
+        if (request === '@stencil/core/mock-doc') {
+          return callback(null, '../../mock-doc');
+        }
+
+        if (whitelist.has(request)) {
+          // we specifically do not want to bundle these imports
+          require.resolve(request);
+          return callback(null, request);
+        }
+
+        // bundle this import
+        callback(undefined, undefined);
+      },
+      resolve: {
+        alias: {
+          '@utils': join(opts.buildDir, 'utils', 'index.js'),
+          postcss: join(opts.nodeModulesDir, 'postcss'),
+          'source-map': join(opts.nodeModulesDir, 'source-map'),
+          chalk: join(opts.bundleHelpersDir, 'empty.js'),
+        },
+      },
+      optimization: {
+        minimize: false,
+      },
+      mode: 'production',
+    };
+
+    webpack(webpackConfig, async (err, stats) => {
+      const { minify } = await import('terser');
+      if (err && err.message) {
+        rejectBundle(err);
+      } else {
+        const info = stats.toJson({ errors: true });
+        if (stats.hasErrors()) {
+          const webpackError = info.errors.join('\n');
+          rejectBundle(webpackError);
         } else {
-          const info = stats.toJson({ errors: true });
-          if (stats.hasErrors()) {
-            const webpackError = info.errors.join('\n');
-            rejectBundle(webpackError);
-          } else {
-            let code = await fs.readFile(outputFile, 'utf8');
+          let code = await fs.readFile(outputFile, 'utf8');
 
-            if (opts.isProd) {
-              try {
-                const minifyResults = await minify(code);
-                code = minifyResults.code;
-              } catch (e) {
-                rejectBundle(e);
-                return;
-              }
+          if (opts.isProd) {
+            try {
+              const minifyResults = await minify(code);
+              code = minifyResults.code;
+            } catch (e) {
+              rejectBundle(e);
+              return;
             }
-            await fs.writeFile(cachedFile, code);
-            await fs.writeFile(outputFile, code);
-
-            resolveBundle();
           }
+          await fs.writeFile(cachedFile, code);
+          await fs.writeFile(outputFile, code);
+
+          resolveBundle();
         }
       }
-    );
+    });
   });
 }

commit 27258aba05c6f16bc18bdb3b7df6f62ed7f99146

fix(browser): polyfill assert, process
this commit fixes a regression that was introduced in 6702eae (#4073),
where webpack 5 no longer polyfills certain node modules that stencil
had assumed would exist. because these polyfills no longer exist in the
form we previously expected them to be in, `require` statements are
generated for them. however, `require` cannot run in the browser,
causing scripts that import stencil to run their compilation in the
browser to fail.

this commit is inspired by the documentation for `resolve.fallback`,
which redcirects module requests when normal resolution fails. the
documentation provides a list of polyfills that webpack v4 would provide
previously. this commit restores the two that we need.

ref: https://webpack.js.org/configuration/resolve/#resolvefallback

scripts/bundles/sys-node.ts
@@ -172,6 +172,10 @@ function bundleExternal(opts: BuildOptions, outputDir: string, cachedDir: string
           'source-map': join(opts.nodeModulesDir, 'source-map'),
           chalk: join(opts.bundleHelpersDir, 'empty.js'),
         },
+        fallback: {
+          assert: require.resolve('assert'),
+          process: require.resolve('process/browser'),
+        },
       },
       optimization: {
         minimize: false,

scripts/license.ts
@@ -13,6 +13,7 @@ const entryDeps = [
   '@rollup/plugin-node-resolve',
   '@yarnpkg/lockfile',
   'ansi-colors',
+  'assert',
   'autoprefixer',
   'exit',
   'glob',
@@ -26,6 +27,7 @@ const entryDeps = [
   'pixelmatch',
   'pngjs',
   'postcss',
+  'process',
   'prompts',
   'rollup',
   'semver',