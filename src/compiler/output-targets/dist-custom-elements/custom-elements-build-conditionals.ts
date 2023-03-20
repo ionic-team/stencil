68a28c798439035b700bdc7b959948cf30bfc6b8
Alice Pote <alice.writes.wrongs@gmail.com>
2023-03-20 09:55:18 -0400

    add a JSDoc while we're in here

--- src/compiler/output-targets/dist-custom-elements/custom-elements-build-conditionals.ts
+++ src/compiler/output-targets/dist-custom-elements/custom-elements-build-conditionals.ts
@@ -1,7 +1,18 @@
 import type * as d from '../../../declarations';
 import { getBuildFeatures, updateBuildConditionals } from '../../app-core/app-data';
 
-export const getCustomElementsBuildConditionals = (config: d.ValidatedConfig, cmps: d.ComponentCompilerMeta[]) => {
+/**
+ * Get build conditions appropriate for the `dist-custom-elements` Output
+ * Target, including disabling lazy loading and hydration.
+ *
+ * @param config a validated user-supplied config
+ * @param cmps metadata about the components currently being compiled
+ * @returns build conditionals appropriate for the `dist-custom-elements` OT
+ */
+export const getCustomElementsBuildConditionals = (
+  config: d.ValidatedConfig,
+  cmps: d.ComponentCompilerMeta[]
+): d.BuildConditionals => {
   // because custom elements bundling does not customize the build conditionals by default
   // then the default in "import { BUILD, NAMESPACE } from '@stencil/core/internal/app-data'"
   // needs to have the static build conditionals set for the custom elements build
