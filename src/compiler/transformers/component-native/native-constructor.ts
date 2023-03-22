e8f5b8799da509ed18b713cfea5ee3e8fe74b112
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2023-03-22 15:46:57 -0400

    finish that comment

--- src/compiler/transformers/component-native/native-constructor.ts
+++ src/compiler/transformers/component-native/native-constructor.ts
@@ -7,8 +7,14 @@ import { addLegacyProps } from '../legacy-props';
 import { retrieveTsModifiers } from '../transform-utils';
 
 /**
- * Updates a constructor to include
- * Modifies a list of provided {@link ts.ClassElement}s in place
+ * Updates a constructor to include:
+ * - a `super()` call
+ * - function calls to initialize the component
+ * - function calls to create custom event emitters
+ * If a constructor does not exist, one will be created
+ *
+ * The constructor will be added to the provided list of {@link ts.ClassElement}s in place
+ *
  * @param classMembers the class elements to modify
  * @param moduleFile the Stencil module representation of the component class
  * @param cmp the component metadata generated for the component
