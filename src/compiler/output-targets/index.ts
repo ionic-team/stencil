52dabc3f0e16c503a961cba5fda992faacffd93b
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2023-03-24 16:13:42 -0400

    restore fixed ot order

--- src/compiler/output-targets/index.ts
+++ src/compiler/output-targets/index.ts
@@ -1,18 +1,15 @@
 import type { RollupCache } from 'rollup';
 
 import type * as d from '../../declarations';
-// import { outputCopy } from './copy/output-copy';
-// import { outputCollection } from './dist-collection';
+import { outputCopy } from './copy/output-copy';
+import { outputCollection } from './dist-collection';
 import { outputCustomElements } from './dist-custom-elements';
 import { outputHydrateScript } from './dist-hydrate-script';
-// import { outputCustomElementsBundle } from './dist-custom-elements-bundle';
-// import { outputHydrateScript } from './dist-hydrate-script';
 import { outputLazy } from './dist-lazy/lazy-output';
-// import { outputAngular } from './output-angular';
-// import { outputDocs } from './output-docs';
-// import { outputLazyLoader } from './output-lazy-loader';
-// import { outputTypes } from './output-types';
-// import { outputWww } from './output-www';
+import { outputDocs } from './output-docs';
+import { outputLazyLoader } from './output-lazy-loader';
+import { outputTypes } from './output-types';
+import { outputWww } from './output-www';
 
 export const generateOutputTargets = async (
   config: d.ValidatedConfig,
@@ -21,32 +18,28 @@ export const generateOutputTargets = async (
 ) => {
   const timeSpan = buildCtx.createTimeSpan('generate outputs started', true);
 
-  // const changedModuleFiles = Array.from(compilerCtx.changedModules)
-  //   .map((filename) => compilerCtx.moduleMap.get(filename))
-  //   .filter((mod) => mod && !mod.isCollectionDependency);
+  const changedModuleFiles = Array.from(compilerCtx.changedModules)
+    .map((filename) => compilerCtx.moduleMap.get(filename))
+    .filter((mod) => mod && !mod.isCollectionDependency);
 
   compilerCtx.changedModules.clear();
 
   invalidateRollupCaches(compilerCtx);
 
   await Promise.all([
-    // outputAngular(config, compilerCtx, buildCtx),
-    // outputCopy(config, compilerCtx, buildCtx),
-    // outputCollection(config, compilerCtx, buildCtx, changedModuleFiles),
-    // // outputCustomElements(config, compilerCtx, buildCtx),
-    // outputCustomElementsBundle(config, compilerCtx, buildCtx),
-    // outputHydrateScript(config, compilerCtx, buildCtx),
-    // outputLazyLoader(config, compilerCtx),
-    // outputWww(config, compilerCtx, buildCtx),
+    outputCopy(config, compilerCtx, buildCtx),
+    outputCollection(config, compilerCtx, buildCtx, changedModuleFiles),
+    outputCustomElements(config, compilerCtx, buildCtx),
+    outputHydrateScript(config, compilerCtx, buildCtx),
+    outputLazyLoader(config, compilerCtx),
+    outputLazy(config, compilerCtx, buildCtx),
+    outputWww(config, compilerCtx, buildCtx),
   ]);
 
-  await outputHydrateScript(config, compilerCtx, buildCtx);
-  await outputLazy(config, compilerCtx, buildCtx);
-  await outputCustomElements(config, compilerCtx, buildCtx);
   // must run after all the other outputs
   // since it validates files were created
-  // await outputDocs(config, compilerCtx, buildCtx);
-  // await outputTypes(config, compilerCtx, buildCtx);
+  await outputDocs(config, compilerCtx, buildCtx);
+  await outputTypes(config, compilerCtx, buildCtx);
 
   timeSpan.finish('generate outputs finished');
 };
