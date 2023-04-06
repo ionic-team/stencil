5775154bdd4689cd70f22af85be1f93a51ee57df
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2023-04-06 09:52:39 -0400

    remove release tests from release-prepare-prompts

--- scripts/test/release-prepare-prompts.spec.ts
+++ scripts/test/release-prepare-prompts.spec.ts
@@ -1,9 +1,4 @@
-import {
-  determineAnsweredTagToUse,
-  determineAnsweredVersionToUse,
-  PrepareReleasePromptAnswers,
-  ReleasePromptAnswers,
-} from '../release-prepare-prompts';
+import { determineAnsweredVersionToUse, PrepareReleasePromptAnswers } from '../release-prepare-prompts';
 
 describe('determineAnsweredVersionToUse', () => {
   it.each<[PrepareReleasePromptAnswers, string]>([
@@ -16,19 +11,5 @@ describe('determineAnsweredVersionToUse', () => {
     [{ confirm: true }, 'UNKNOWN'],
   ])('%s returns "%s"', (answers, expected) => {
     expect(determineAnsweredVersionToUse(answers)).toBe(expected);
-  });
-});
-
-describe('determineAnsweredTagToUse', () => {
-  it.each<[ReleasePromptAnswers, string]>([
-    [{ tag: '1.0.0', specifiedTag: '2.0.0', confirm: true, otp: '' }, '1.0.0'],
-    [{ tag: '1.0.0', confirm: true, otp: '' }, '1.0.0'],
-    [{ specifiedTag: '2.0.0', confirm: true, otp: '' }, '2.0.0'],
-    [{ tag: '', specifiedTag: '', confirm: true, otp: '' }, 'UNKNOWN'],
-    [{ tag: '', confirm: true, otp: '' }, 'UNKNOWN'],
-    [{ specifiedTag: '', confirm: true, otp: '' }, 'UNKNOWN'],
-    [{ confirm: true, otp: '' }, 'UNKNOWN'],
-  ])('%s returns "%s"', (answers, expected) => {
-    expect(determineAnsweredTagToUse(answers)).toBe(expected);
   });
 });
