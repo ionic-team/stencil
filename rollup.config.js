d260bb22111a1817c59546e64bc51d1b6486ec30
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2023-04-27 15:33:14 -0400

    temp

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,7 @@ module.exports = function (args) {
   const opts = getOptions(__dirname, {
     isProd: !!args['config-prod'],
     isCI: !!args['config-ci'],
+
   });
   return createBuild(opts);
 };
