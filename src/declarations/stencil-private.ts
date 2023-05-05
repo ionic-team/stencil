3811a4c4714f9fb0f88dc1f798959196721548e5
Alice <alice.writes.wrongs@gmail.com>
2023-05-05 11:43:17 -0400

    refactor(compiler): add a Result union type for polymorphic returns (#4281)
    This adds a new way to handle return types like those of the
    `generateBuildStatus` function. See here for some previous conversation
    about this:
    https://github.com/ionic-team/stencil/pull/3383#discussion_r882905056
    
    At present in `main` this function returns `d.CompilerBuildStats | {
    diagnostics: d.Diagnostic[] }`:
    
    https://github.com/ionic-team/stencil/blob/b5dc6d533f7961641e4876fe3411834884748b2f/src/compiler/build/build-stats.ts#L11-L14
    
    We use this sort of pattern in a few places throughout the codebase,
    where basically if a function could generate diagnostic messages it will
    return either it's 'normal' return value _or_ an object holding those
    diagnostics. We then typically have some manual checking to see which of
    the two it is, or perhaps just a type assertion.
    
    This pattern works alright, but in looking at this code before we
    identified it as something we'd like to clean up in the future.
    
    To do so, this commit introduces a new `Result` union type which is
    either an `Ok` or an `Err`. It's quite obviously strongly inspired by
    the `Result` enum in Rust's standard library
    (https://doc.rust-lang.org/std/result/) and it lets us handle two
    possible return values, one for the 'happy path' and one indicating
    something didn't go right, in a principled way.
    
    Using it looks something like this:
    
    ```ts
    import { result } from "@utils";
    
    const myFunc = (): result.Result<string, string> {
      if (failing) {
        return result.err("I failed!");
      } else {
        return result.ok("I succeeded!");
      }
    }
    ```
    
    Once you have a value of type `Result` there are some helper methods
    similar to what you might be familiar with in Rust like a `result.map`.

--- src/declarations/stencil-private.ts
+++ src/declarations/stencil-private.ts
@@ -1,3 +1,5 @@
+import { result } from '@utils';
+
 import type { InMemoryFileSystem } from '../compiler/sys/in-memory-fs';
 import type {
   BuildEvents,
@@ -219,7 +221,7 @@ export interface UpdatedLazyBuildCtx {
 export interface BuildCtx {
   buildId: number;
   buildResults: CompilerBuildResults;
-  buildStats?: CompilerBuildStats | { diagnostics: Diagnostic[] };
+  buildStats?: result.Result<CompilerBuildStats, { diagnostics: Diagnostic[] }>;
   buildMessages: string[];
   bundleBuildCount: number;
   collections: Collection[];
