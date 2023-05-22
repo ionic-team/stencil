4fc57fa1d2a6319b95a83802cd30389c0c9176bf
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2023-05-22 08:08:35 -0400

    Revert "Revert "refactor(compiler): add a Result union type for polymorphic returns (#4281)""
    This reverts commit c85df2b4fc8a882c116cae7cd98943992bbd2946.

--- scripts/build.ts
+++ scripts/build.ts
@@ -10,6 +10,7 @@ import { mockDoc } from './bundles/mock-doc';
 import { screenshot } from './bundles/screenshot';
 import { sysNode, sysNodeExternalBundles } from './bundles/sys-node';
 import { testing } from './bundles/testing';
+import { utils } from './bundles/utils';
 import { createLicense } from './license';
 import { release } from './release';
 import { validateBuild } from './test/validate-build';
@@ -66,6 +67,7 @@ export async function createBuild(opts: BuildOptions): Promise<readonly RollupOp
     screenshot(opts),
     testing(opts),
     sysNode(opts),
+    utils(opts),
   ]);
 
   return bundles.flat();
