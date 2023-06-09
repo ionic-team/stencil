d4f454eaf00d3d5b25f486f3a1935b0653020a30
Alice Pote <alice.writes.wrongs@gmail.com>
2023-06-09 10:27:31 -0400

    iron out some test flakiness

--- src/compiler/sys/config.ts
+++ src/compiler/sys/config.ts
@@ -1,8 +1,10 @@
+import { createNodeLogger } from '@sys-api-node';
 import { createConfigFlags } from '../../cli/config-flags';
 import type * as d from '../../declarations';
 import { validateConfig } from '../config/validate-config';
 
 export const getConfig = (userConfig: d.Config): d.ValidatedConfig => {
+  userConfig.logger = userConfig.logger ?? createNodeLogger();
   const flags = createConfigFlags(userConfig.flags ?? {});
   userConfig.flags = flags;
   const config: d.ValidatedConfig = validateConfig(userConfig, {}).config;
