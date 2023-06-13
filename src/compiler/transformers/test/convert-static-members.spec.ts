dc3925e86d27d4f7360d0b7d398a251f60042265
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2023-06-13 06:31:35 -0700

    fix(compiler): handle static members with stencil decorators (#4463)
    this commit fixes an edge case to the fix introduced in 6dbe9a5 (#4447).
    the mechanism for checking if there is a statically intiialized class
    member has been updated in order to exclude such members that are
    decorated with Stencil's `@Prop()` and `@State()` decorators.

--- src/compiler/transformers/test/convert-static-members.spec.ts
+++ src/compiler/transformers/test/convert-static-members.spec.ts
@@ -42,6 +42,65 @@ describe('convert-static-members', () => {
       expect(classWithStaticMembers.members.some(hasStaticInitializerInClass)).toBe(true);
     });
 
+    it('returns true for a decorated (non-Stencil) static property with an initializer', () => {
+      const classWithStaticMembers = ts.factory.createClassDeclaration(
+        [ts.factory.createToken(ts.SyntaxKind.ExportKeyword)],
+        ts.factory.createIdentifier('ClassWithStaticMember'),
+        undefined,
+        undefined,
+        [
+          ts.factory.createPropertyDeclaration(
+            [
+              ts.factory.createDecorator(
+                ts.factory.createCallExpression(
+                  ts.factory.createIdentifier('SomeDecorator'), // Imaginary decorator
+                  undefined,
+                  []
+                )
+              ),
+              ts.factory.createToken(ts.SyntaxKind.StaticKeyword),
+            ],
+            ts.factory.createIdentifier('propertyName'),
+            undefined,
+            undefined,
+            ts.factory.createStringLiteral('initial value')
+          ),
+        ]
+      );
+      expect(classWithStaticMembers.members.some(hasStaticInitializerInClass)).toBe(true);
+    });
+
+    it.each(['Prop', 'State'])(
+      'returns false for a static property decorated with @%s with an initializer',
+      (decoratorName) => {
+        const classWithStaticMembers = ts.factory.createClassDeclaration(
+          [ts.factory.createToken(ts.SyntaxKind.ExportKeyword)],
+          ts.factory.createIdentifier('ClassWithStaticMember'),
+          undefined,
+          undefined,
+          [
+            ts.factory.createPropertyDeclaration(
+              [
+                ts.factory.createDecorator(
+                  ts.factory.createCallExpression(
+                    ts.factory.createIdentifier(decoratorName), // Stencil decorator
+                    undefined,
+                    []
+                  )
+                ),
+                ts.factory.createToken(ts.SyntaxKind.StaticKeyword),
+              ],
+              ts.factory.createIdentifier('propertyName'),
+              undefined,
+              undefined,
+              ts.factory.createStringLiteral('initial value')
+            ),
+          ]
+        );
+        expect(classWithStaticMembers.members.some(hasStaticInitializerInClass)).toBe(false);
+      }
+    );
+
     it('returns true for a static property with an initializer with multiple members', () => {
       const classWithStaticMembers = ts.factory.createClassDeclaration(
         [ts.factory.createToken(ts.SyntaxKind.ExportKeyword)],
