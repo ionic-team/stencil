d4a3fe6827bfe477c95258f801fcec2f60facb56
Tanner Reits <tanner@ionic.io>
2023-06-27 14:52:29 -0400

    add tests for collection value

--- src/compiler/types/tests/validate-package-json.spec.ts
+++ src/compiler/types/tests/validate-package-json.spec.ts
@@ -99,9 +99,30 @@ describe('validate-package-json', () => {
   });
 
   describe('collection', () => {
-    it('should error when missing collection property', async () => {
+    it('should produce a warning when missing collection property', async () => {
       v.validateCollection(config, compilerCtx, buildCtx, collectionOutputTarget);
+
       expect(buildCtx.diagnostics[0].messageText).toMatch(/package.json "collection" property is required/);
+      expect(buildCtx.diagnostics[0].level).toBe('warn');
+    });
+
+    it('should produce a warning if the supplied path does not match the recommended path', () => {
+      buildCtx.packageJson.collection = 'bad/path';
+
+      v.validateCollection(config, compilerCtx, buildCtx, collectionOutputTarget);
+
+      expect(buildCtx.diagnostics[0].messageText).toBe(
+        `package.json "collection" property is required when generating a distribution and must be set to: dist/collection/collection-manifest.json`
+      );
+      expect(buildCtx.diagnostics[0].level).toBe('warn');
+    });
+
+    it('should not produce a warning if the normalized paths are the same', () => {
+      buildCtx.packageJson.collection = './dist/collection/collection-manifest.json';
+
+      v.validateCollection(config, compilerCtx, buildCtx, collectionOutputTarget);
+
+      expect(buildCtx.diagnostics.length).toEqual(0);
     });
   });
 });
