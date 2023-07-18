b37afd25b43146af3d957f33b867b39efb669e76
Alice Pote <alice.writes.wrongs@gmail.com>
2023-07-18 10:07:38 -0500

    fix(lazy): adjust the type of `defineCustomElements`
    This fixes an issue with the type for `defineCustomElements` that we set
    in output-lazy-loader.ts. In #4419 we changed the return type of the
    function to be `void` (at runtime) but we didn't make a corresponding
    adjustment to the type declaration for the function that we generate
    here:
    
    https://github.com/ionic-team/stencil/blob/7d5dc6cf5e0d2020c513cc87b6b2e5b93eece9bc/src/compiler/output-targets/output-lazy-loader.ts#L88
    
    This change adjusts that type declaration to have a `void` return value,
    instead of `Promise<void>`.
    
    See #4589 for more context

--- src/compiler/output-targets/output-lazy-loader.ts
+++ src/compiler/output-targets/output-lazy-loader.ts
@@ -85,7 +85,7 @@ export interface CustomElementsDefineOptions {
   ael?: (el: EventTarget, eventName: string, listener: EventListenerOrEventListenerObject, options: boolean | AddEventListenerOptions) => void;
   rel?: (el: EventTarget, eventName: string, listener: EventListenerOrEventListenerObject, options: boolean | AddEventListenerOptions) => void;
 }
-export declare function defineCustomElements(win?: Window, opts?: CustomElementsDefineOptions): Promise<void>;
+export declare function defineCustomElements(win?: Window, opts?: CustomElementsDefineOptions): void;
 export declare function applyPolyfills(): Promise<void>;
 
 /**
