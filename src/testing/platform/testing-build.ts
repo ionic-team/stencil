06ae53369fc25dd90c3e5e080cf9e2774722b29a
Tanner Reits <tanner@ionic.io>
2023-07-18 14:55:57 -0400

    update testing platform build conditionals

--- src/testing/platform/testing-build.ts
+++ src/testing/platform/testing-build.ts
@@ -2,7 +2,7 @@ import type * as d from '@stencil/core/internal';
 
 export const Build: d.UserBuildConditionals = {
   isDev: true,
-  isBrowser: false,
-  isServer: true,
+  isBrowser: true,
+  isServer: false,
   isTesting: true,
 };
