878d18a7cb49c2a5dfad48565f22ee4331f6770d
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2023-07-26 11:05:49 -0400

    chore(jsdoc): add docs to component flag values (#4620)
    this commit adds jsdoc to stencil's `CMP_FLAGS` enumeration, to help
    clarify the usage of each variant in the enum. this is intended to help
    guide the discussion around slots, scoped components, etc. as we work to
    improve various bugs related to this part of the code over the next few
    weeks

--- src/compiler/transformers/static-to-meta/encapsulation.ts
+++ src/compiler/transformers/static-to-meta/encapsulation.ts
@@ -1,8 +1,19 @@
 import ts from 'typescript';
 
+import type * as d from '../../../declarations';
 import { getStaticValue } from '../transform-utils';
 
-export const parseStaticEncapsulation = (staticMembers: ts.ClassElement[]) => {
+/**
+ * Find and return the type of encapsulation that a component has based on the return value of its static getter of the
+ * same name.
+ *
+ * If no encapsulation static getter is found, or if the found encapsulation getter return value is not an accepted
+ * value, 'none' is returned.
+ *
+ * @param staticMembers a collection of static getters to search
+ * @returns the encapsulation mode to use for a component
+ */
+export const parseStaticEncapsulation = (staticMembers: ts.ClassElement[]): d.Encapsulation => {
   let encapsulation: string = getStaticValue(staticMembers, 'encapsulation');
 
   if (typeof encapsulation === 'string') {
@@ -15,7 +26,20 @@ export const parseStaticEncapsulation = (staticMembers: ts.ClassElement[]) => {
   return 'none';
 };
 
-export const parseStaticShadowDelegatesFocus = (encapsulation: string, staticMembers: ts.ClassElement[]) => {
+/**
+ * Find and return if `delegatesFocus` is enabled for a component based on the return value of its static getter of the
+ * same name.
+ *
+ * @param encapsulation the encapsulation mode to use for a component
+ * @param staticMembers a collection of static getters to search
+ * @returns when `encapsulation` is 'shadow', return `true` if the static getter returns true. If the static getter
+ * returns `false` or does not exist, return `false`. If `encapsulation` is not 'shadow', return `null`, regardless of
+ * the static getter's existence/return value.
+ */
+export const parseStaticShadowDelegatesFocus = (
+  encapsulation: string,
+  staticMembers: ts.ClassElement[],
+): boolean | null => {
   if (encapsulation === 'shadow') {
     const delegatesFocus: boolean = getStaticValue(staticMembers, 'delegatesFocus');
     return !!delegatesFocus;
