1df736e0c4b27c402099f59d13ceb7ff1ed29a49
Alice Pote <alice.writes.wrongs@gmail.com>
2023-08-18 13:33:59 -0400

    feat(runtime): automatically insert `key` attrs during compilation
    This adds a new transformer, `performAutomaticKeyInsertion`, which will
    add `key` attributes into certain JSX nodes in the `render()` method of
    a Stencil component. This will allow the Stencil runtime to make more
    accurate decisions about when to create and destroy child nodes during
    re-rendering, especially in circumstances where nodes are changing order
    and so on.
    
    There are some limitations on when we can safely insert keys without
    possibly introducing incorrect behavior. In particular, we don't want to
    insert keys in the following situations:
    
    - when a `render` method has more than one return statement
    - when a `render` method has a conditional (ternary) expression in it
    - when a JSX node is located inside of a JSX expression (i.e. within
      curly braces like `<div>{ ... }</div>`)
    
    In each of these cases we don't have the static analysis chops to know
    when a given JSX syntax tree node is supposed to correspond to the same
    HTML element at runtime, whereas in the 'single return, JSX children
    case' like:
    
    ```tsx
    class Component {
      render () {
        return <div><img /></div>
      }
    }
    ```
    
    We know without a doubt in this case that the `div` and `img` JSX nodes
    are always supposed to correspond to the same HTML elements at runtime,
    so it's no problem to add keys to them.
    
    The key insertion transformer also does not transform JSX nodes which
    are not part of Stencil components, so if a project had, for some
    reason, a React component or something in it we will leave it alone.
    
    fixes #1968
    fixes #5263
    STENCIL-893

---
+++ src/compiler/transformers/automatic-key-insertion/utils.ts
@@ -0,0 +1,36 @@
+import { createHash } from 'node:crypto';
+
+import ts from 'typescript';
+
+/**
+ * An incrementing-number generator, just as a little extra 'uniqueness'
+ * insurance for {@link deriveJSXKey}
+ */
+const incrementer = (function* () {
+  let val = 0;
+  while (true) {
+    yield val++;
+  }
+})();
+
+/**
+ * Generate a unique key for a given JSX element. The key is creating by
+ * concatenating and then hashing (w/ SHA1) the following:
+ *
+ * - an incrementing value
+ * - the element's tag name
+ * - the start position for the element's token in the original source file
+ * - the end position for the element's token in the original source file
+ *
+ * It is hoped this provides enough uniqueness that a collision won't occur.
+ *
+ * @param jsxElement a typescript JSX syntax tree node which needs a key
+ * @returns a computed unique key for that element
+ */
+export function deriveJSXKey(jsxElement: ts.JsxOpeningElement | ts.JsxSelfClosingElement): string {
+  const hash = createHash('sha1')
+    .update(`${incrementer.next().value}__${jsxElement.tagName}__${jsxElement.pos}_${jsxElement.end}`)
+    .digest('hex')
+    .toLowerCase();
+  return hash;
+}
