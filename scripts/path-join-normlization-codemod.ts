68e7af609c96638ac2cb647224f7e0c0eafcd7cf
Alice Pote <alice.writes.wrongs@gmail.com>
2023-10-26 12:27:03 -0400

    ok

--- scripts/path-join-normlization-codemod.ts
+++ scripts/path-join-normlization-codemod.ts
@@ -8,28 +8,41 @@ function isFsImport(importDecl: ImportDeclaration): boolean {
   return importDecl.getModuleSpecifier().getText() === "fs";
 }
 
-// function 
-
 
 for (const sourceFile of project.getSourceFiles()) {
   console.log(`checking ${sourceFile.getFilePath()}`);
-  const fsImport = sourceFile.getImportDeclaration("path");
+  const pathImport = sourceFile.getImportDeclaration("path");
 
-  if (!fsImport) {
+  if (!pathImport) {
     continue;
   }
 
-  const imports = fsImport.getNamedImports();
+  const imports = pathImport.getNamedImports();
   const joinImport = imports.find(importSpecifier => importSpecifier.getName() === "join");
+  const relativeImport = imports.find(importSpecifier => importSpecifier.getName() === "relative");
+
+  const utilImports = []
 
   if (joinImport) {
     joinImport.remove();
-
-    sourceFile.addImportDeclaration({
-      moduleSpecifier: "@utils",
-      namedImports: ["join"]
-    })
+    utilImports.push("join");
   }
+
+  if (relativeImport) {
+    relativeImport.remove();
+    utilImports.push("relative");
+  }
+
+  if (pathImport.getNamedImports().length === 0) {
+    pathImport.remove();
+  }
+
+  sourceFile.addImportDeclaration({
+    moduleSpecifier: "@utils",
+    namedImports: utilImports
+  })
+
+  sourceFile.fixUnusedIdentifiers();
 }
 
 project.save();
