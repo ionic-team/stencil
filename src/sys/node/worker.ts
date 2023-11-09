1dd59a5e0a57ec5234a5d96a94121cd5cd2bc5fd
Alice Pote <alice.writes.wrongs@gmail.com>
2023-11-08 21:16:17 -0500

    chore(esbuild): build sys-node bundle with esbuild (#5010)
    This adds a new esbuild-based bundle for the sys-node bundle, which
    includes the `CompilerSystem` implementation for node.js (currently our
    only full implementation).

--- src/sys/node/worker.ts
+++ src/sys/node/worker.ts
@@ -1,10 +1,8 @@
-import '@stencil/core/compiler';
-
+import * as coreCompiler from '@stencil/core/compiler';
 import * as nodeApi from '@sys-api-node';
 
 import { initNodeWorkerThread } from './node-worker-thread';
 
-const coreCompiler = (global as any).stencil as typeof import('@stencil/core/compiler');
 const nodeSys = nodeApi.createNodeSys({ process: process });
 const msgHandler = coreCompiler.createWorkerMessageHandler(nodeSys);
 
