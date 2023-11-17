912bd811a038b2825bc39985fec14928b8373e86
Christian Bromann <git@bromann.dev>
2023-11-16 19:41:01 -0800

    chore(build): build the dev-server module with esbuild
    STENCIL-989
    
    fixes after testing
    
    revert changes in sys-node
    
    PR feedback

--- scripts/bundles/plugins/content-types-plugin.ts
+++ scripts/bundles/plugins/content-types-plugin.ts
@@ -22,7 +22,7 @@ export function contentTypesPlugin(opts: BuildOptions): Plugin {
   };
 }
 
-async function createContentTypeData(opts: BuildOptions) {
+export async function createContentTypeData(opts: BuildOptions) {
   // create a focused content-type lookup object from
   // the mime db json file
   const mimeDbSrcPath = join(opts.nodeModulesDir, 'mime-db', 'db.json');
