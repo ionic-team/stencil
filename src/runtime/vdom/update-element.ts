a9cc562ec862c7f3254b74d43559d40156370242
Alice Pote <alice.writes.wrongs@gmail.com>
2024-01-30 13:31:58 -0500

    fix(runtime): prevent ref callbacks from being called too early
    Sort attribute names before calling `setAccessor` with their changed
    values in order to ensure that the `ref` attribute is handled by
    `setAccessor` after all other attributes have been handled. This
    prevents the execution order for `ref` callbacks from being dependency
    on the order in which attributes are declared in the JSX.
    
    STENCIL-737
    fixes #4074

--- src/runtime/vdom/update-element.ts
+++ src/runtime/vdom/update-element.ts
@@ -23,7 +23,7 @@ export const updateElement = (
 
   if (BUILD.updatable) {
     // remove attributes no longer present on the vnode by setting them to undefined
-    for (memberName in oldVnodeAttrs) {
+    for (memberName of sortedAttrNames(Object.keys(oldVnodeAttrs))) {
       if (!(memberName in newVnodeAttrs)) {
         setAccessor(elm, memberName, oldVnodeAttrs[memberName], undefined, isSvgMode, newVnode.$flags$);
       }
@@ -31,7 +31,26 @@ export const updateElement = (
   }
 
   // add new & update changed attributes
-  for (memberName in newVnodeAttrs) {
+  for (memberName of sortedAttrNames(Object.keys(newVnodeAttrs))) {
     setAccessor(elm, memberName, oldVnodeAttrs[memberName], newVnodeAttrs[memberName], isSvgMode, newVnode.$flags$);
   }
 };
+
+/**
+ * Sort a list of attribute names to ensure that all the attribute names which
+ * are _not_ `"ref"` come before `"ref"`. Preserve the order of the non-ref
+ * attributes.
+ *
+ * **Note**: if the supplied attributes do not include `'ref'` then the same
+ * (by reference) array will be returned without modification.
+ *
+ * @param attrNames attribute names to sort
+ * @returns a list of attribute names, sorted if they include `"ref"`
+ */
+function sortedAttrNames(attrNames: string[]): string[] {
+  return attrNames.includes('ref')
+    ? // we need to sort these to ensure that `'ref'` is the last attr
+      [...attrNames.filter((attr) => attr !== 'ref'), 'ref']
+    : // no need to sort, return the original array
+      attrNames;
+}
