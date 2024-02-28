0da22382909151bf2de3658a26f4eb7db31613c9
Christian Bromann <git@bromann.dev>
2024-02-27 17:12:23 -0800

    fix(compiler): fix generated import statement
    STENCIL-855

---
+++ src/compiler/output-targets/dist-custom-elements/test/dist-custom-elements.spec.ts
@@ -0,0 +1,24 @@
+import { addCustomElementInputs } from '../index';
+
+describe('dist-custom-elements', () => {
+  it('should add custom element inputs', () => {
+    const buildCtx: any = {
+      components: [{
+        isPlain: true,
+        sourceFilePath: './foo/bar.tsx',
+        tagName: 'my-tag'
+      }]
+    };
+    const bundleOpts: any = {
+      inputs: {},
+      loader: {}
+    }
+    const outputTarget: any = {
+      type: 'dist-custom-elements',
+      customElementsExportBehavior: 'single-export-module'
+    };
+    addCustomElementInputs(buildCtx, bundleOpts, outputTarget);
+    expect(bundleOpts.loader['\x00core']).toContain(
+      `export { MyTag } from '\x00MyTag';\n`)
+  });
+});
