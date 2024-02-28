5508af4a237055bb1b6725c53331c6b54e53c2d9
Christian Bromann <git@bromann.dev>
2024-02-28 11:23:43 -0800

    apply provided patch

--- src/compiler/output-targets/dist-custom-elements/test/dist-custom-elements.spec.ts
+++ src/compiler/output-targets/dist-custom-elements/test/dist-custom-elements.spec.ts
@@ -1,21 +1,22 @@
+import type * as d from '@stencil/core/declarations';
+import { mockBuildCtx } from '@stencil/core/testing';
+
+import { BundleOptions } from '../../../bundle/bundle-interface';
+import { stubComponentCompilerMeta } from '../../../types/tests/ComponentCompilerMeta.stub';
 import { addCustomElementInputs } from '../index';
 
 describe('dist-custom-elements', () => {
   it('should add custom element inputs', () => {
-    const buildCtx: any = {
-      components: [
-        {
-          isPlain: true,
-          sourceFilePath: './foo/bar.tsx',
-          tagName: 'my-tag',
-        },
-      ],
-    };
-    const bundleOpts: any = {
+    const cmpMeta = stubComponentCompilerMeta({ isPlain: true, sourceFilePath: './foo/bar.tsx', tagName: 'my-tag' });
+    const buildCtx = mockBuildCtx();
+    buildCtx.components = [cmpMeta];
+    const bundleOpts: BundleOptions = {
+      id: 'customElements',
+      platform: 'client',
       inputs: {},
       loader: {},
     };
-    const outputTarget: any = {
+    const outputTarget: d.OutputTargetDistCustomElements = {
       type: 'dist-custom-elements',
       customElementsExportBehavior: 'single-export-module',
     };
