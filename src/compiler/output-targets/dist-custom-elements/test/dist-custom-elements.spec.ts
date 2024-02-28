6d5d7fbcc8eebd930afd3126042d37e8c7c627a1
Christian Bromann <git@bromann.dev>
2024-02-27 17:53:20 -0800

    prettier

--- src/compiler/output-targets/dist-custom-elements/test/dist-custom-elements.spec.ts
+++ src/compiler/output-targets/dist-custom-elements/test/dist-custom-elements.spec.ts
@@ -3,22 +3,23 @@ import { addCustomElementInputs } from '../index';
 describe('dist-custom-elements', () => {
   it('should add custom element inputs', () => {
     const buildCtx: any = {
-      components: [{
-        isPlain: true,
-        sourceFilePath: './foo/bar.tsx',
-        tagName: 'my-tag'
-      }]
+      components: [
+        {
+          isPlain: true,
+          sourceFilePath: './foo/bar.tsx',
+          tagName: 'my-tag',
+        },
+      ],
     };
     const bundleOpts: any = {
       inputs: {},
-      loader: {}
-    }
+      loader: {},
+    };
     const outputTarget: any = {
       type: 'dist-custom-elements',
-      customElementsExportBehavior: 'single-export-module'
+      customElementsExportBehavior: 'single-export-module',
     };
     addCustomElementInputs(buildCtx, bundleOpts, outputTarget);
-    expect(bundleOpts.loader['\x00core']).toContain(
-      `export { MyTag } from '\x00MyTag';\n`)
+    expect(bundleOpts.loader['\x00core']).toContain(`export { MyTag } from '\x00MyTag';\n`);
   });
 });
