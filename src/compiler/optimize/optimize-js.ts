b36f295fbdb7e8b8627a103f1ce6d8b07389279a
Alice Pote <alice.writes.wrongs@gmail.com>
2024-03-11 12:12:33 -0400

    chore(docs): add JSDoc to optimizeJs (#5447)
    We already have JSDoc on the declaration for this function here:
    
    https://github.com/ionic-team/stencil/blob/5b2c399f31cb689098f438eda4db2ee55c5412e1/src/compiler/public.ts#L73-L77
    
    this ports it to the implementation.

--- src/compiler/optimize/optimize-js.ts
+++ src/compiler/optimize/optimize-js.ts
@@ -4,6 +4,15 @@ import { Config, OptimizeJsInput, OptimizeJsOutput } from '../../declarations';
 import { minifyJs } from './minify-js';
 import { getTerserOptions } from './optimize-module';
 
+/**
+ * Utility function used by the compiler to optimize JavaScript. Knowing the
+ * JavaScript target will further apply minification optimizations beyond usual
+ * minification.
+ *
+ * @param inputOpts options to control how the JS optimization should take
+ * place
+ * @returns a promise wrapping an output object
+ */
 export const optimizeJs = async (inputOpts: OptimizeJsInput) => {
   const result: OptimizeJsOutput = {
     output: inputOpts.input,
