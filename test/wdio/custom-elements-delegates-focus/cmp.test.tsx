0f74078fce4e85582846ced1fe99fe1ea45794da
Christian Bromann <git@bromann.dev>
2024-03-13 18:10:07 -0700

    rewrite files

--- test/wdio/custom-elements-delegates-focus/cmp.test.tsx
+++ test/wdio/custom-elements-delegates-focus/cmp.test.tsx
@@ -1,27 +1,31 @@
-import { setupDomTests } from '../util';
+import { Fragment, h } from '@stencil/core';
+import { render } from '@wdio/browser-runner/stencil';
 
 describe('custom-elements-delegates-focus', () => {
-  const { setupDom, tearDownDom } = setupDomTests(document);
-  let app: HTMLElement;
-
-  beforeEach(async () => {
-    app = await setupDom('/custom-elements-delegates-focus/index.html');
+  before(() => {
+    render({
+      template:() => (
+        <>
+          <custom-elements-delegates-focus></custom-elements-delegates-focus>
+          <custom-elements-no-delegates-focus></custom-elements-no-delegates-focus>
+        </>
+      )
+    });
   });
-  afterEach(tearDownDom);
 
-  it('sets delegatesFocus correctly', async () => {
+  it('sets delegatesFocus correctly', () => {
     expect(customElements.get('custom-elements-delegates-focus')).toBeDefined();
 
-    const elm: Element = app.querySelector('custom-elements-delegates-focus');
+    const elm: Element = document.querySelector('custom-elements-delegates-focus');
 
     expect(elm.shadowRoot).toBeDefined();
     expect(elm.shadowRoot.delegatesFocus).toBe(true);
   });
 
-  it('does not set delegatesFocus when shadow is set to "true"', async () => {
+  it('does not set delegatesFocus when shadow is set to "true"', () => {
     expect(customElements.get('custom-elements-no-delegates-focus')).toBeDefined();
 
-    const elm: Element = app.querySelector('custom-elements-no-delegates-focus');
+    const elm: Element = document.querySelector('custom-elements-no-delegates-focus');
 
     expect(elm.shadowRoot).toBeDefined();
     expect(elm.shadowRoot.delegatesFocus).toBe(false);
