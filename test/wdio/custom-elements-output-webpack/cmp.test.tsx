0822e33f18e76b3f57846732cdfd94cfec82028f
Christian Bromann <git@bromann.dev>
2024-03-13 18:18:24 -0700

    migrate tests

--- test/wdio/custom-elements-output-webpack/cmp.test.tsx
+++ test/wdio/custom-elements-output-webpack/cmp.test.tsx
@@ -1,20 +1,24 @@
-import { setupDomTests } from '../util';
+import { Fragment, h } from '@stencil/core';
+import { render } from '@wdio/browser-runner/stencil';
 
 describe('custom-elements-output-webpack', () => {
-  const { setupDom, tearDownDom } = setupDomTests(document);
-  let app: HTMLElement;
-
-  beforeEach(async () => {
-    app = await setupDom('/custom-elements-output-webpack/index.html');
+  before(() => {
+    render({
+      template: () => (
+        <>
+          <custom-element-root></custom-element-root>
+        </>
+      ),
+    });
   });
-  afterEach(tearDownDom);
 
   it('defines components and their dependencies', async () => {
     expect(customElements.get('custom-element-root')).toBeDefined();
     expect(customElements.get('custom-element-child')).toBeDefined();
     expect(customElements.get('custom-element-nested-child')).toBeDefined();
 
-    const elm = app.querySelector('custom-element-root');
+    const elm = document.querySelector('custom-element-root');
+    await browser.waitUntil(() => Boolean(elm.shadowRoot.querySelector('custom-element-child')))
     const childElm = elm.shadowRoot.querySelector('custom-element-child');
     const childNestedElm = childElm.shadowRoot.querySelector('custom-element-nested-child');
 
