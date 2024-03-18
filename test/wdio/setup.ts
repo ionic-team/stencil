3f622c9a08e7bae14e835a798f1e89cb3a90d76f
Alice Pote <alice.writes.wrongs@gmail.com>
2024-03-18 15:32:16 -0400

    chore(wdio): port global-script test suite to wdio
    This one is more complicated than some of our other test suites, mainly
    because the Karma test in `test/karma/test-app/global-script` was
    actually testing a separate application in `test/karma/test-global-script`
    which was built as a separate project, in order that a separate build
    incorporating a [global
    script](https://stenciljs.com/docs/config#globalscript) could be tested.
    
    This retains this as a new separate project but keeps the same 'feel' as
    our existing WebdriverIO tests by:
    
    - locating the component and the spec file in a subdirectory of
      `test/wdio`, like our other tests
    - adding a new build command in `test/wdio`, `build.global-script`,
      which build just the project in `test/wdio/global-script` with a
      separate stencil configuration
      (`test/wdio/global-script.stencil.config.ts`)
      - this special separate build both excludes the existing Stencil
        components and is, in turn, excluded from the existing Stencil
        project
    - the `www` output target bundle is then selectively imported into
      WebdriverIO tests (via `test/wdio/setup.ts`) only when we're running
      the `global-script.test.tsx` test file
    
    Overall this gives us the ability to test different Stencil
    configuration options using the same setup and with a bit less
    complexity than we had in the original Karma setup. This can serve as a
    template for other Karma tests where we'll need a similar setup.

--- test/wdio/setup.ts
+++ test/wdio/setup.ts
@@ -2,3 +2,18 @@
  * load the testapp so that we don't have to import the components within the tests
  */
 await import('./dist/testapp/testapp.esm.js');
+
+// this is well justified!
+// https://github.com/webdriverio/webdriverio/blob/51ac8482284ad34dea3cc899872397fb734de617/packages/wdio-browser-runner/src/browser/setup.ts#L13-L21
+declare global {
+  interface Window {
+    __wdioSpec__: string;
+  }
+}
+
+/**
+ * load the separate test app for the global script tests, if appropriate to do so
+ */
+if (window.__wdioSpec__.includes('global-script.test.tsx')) {
+  await import('./www-global-script/build/testglobalscript.esm.js');
+}
