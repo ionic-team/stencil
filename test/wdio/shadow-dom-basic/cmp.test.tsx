fa737bc4210932da1c7ee095236ee267d15f67e0
Tanner Reits <tanner@ionic.io>
2024-03-19 16:18:20 -0400

    remove polyfill tests, fix firefox failure

--- test/wdio/shadow-dom-basic/cmp.test.tsx
+++ test/wdio/shadow-dom-basic/cmp.test.tsx
@@ -10,16 +10,12 @@ describe('shadow-dom-basic', () => {
 
   it('render', async () => {
     await $('shadow-dom-basic-root').waitForExist();
+
+    // Firefox fails without this
+    await browser.pause();
+
     const elm = document.querySelector('shadow-dom-basic-root');
 
-    if ('attachShadow' in HTMLElement.prototype) {
-      await testNativeShadow(elm);
-    } else {
-      await testPolyfilledShadow(elm);
-    }
-  });
-
-  async function testNativeShadow(elm: Element) {
     await expect(elm.shadowRoot).toBeDefined();
 
     const shadowDomBasic = elm.shadowRoot.lastElementChild;
@@ -38,28 +34,5 @@ describe('shadow-dom-basic', () => {
 
     const lightBG = window.getComputedStyle(shadowDomBasic.lastElementChild).backgroundColor;
     await expect(lightBG).toBe('rgb(255, 255, 0)');
-  }
-
-  async function testPolyfilledShadow(elm: Element) {
-    await expect(elm.shadowRoot).toBe(elm);
-
-    await expect(elm.classList.contains('sc-shadow-dom-basic-root-h')).toBe(true);
-
-    await expect(elm.children[0].nodeName).toBe('SHADOW-DOM-BASIC');
-    await expect(elm.children[0].classList.contains('sc-shadow-dom-basic-root')).toBe(true);
-    await expect(elm.children[0].classList.contains('sc-shadow-dom-basic-h')).toBe(true);
-    await expect(elm.children[0].classList.contains('sc-shadow-dom-basic-s')).toBe(true);
-
-    await expect(elm.children[0].children[0].nodeName).toBe('DIV');
-    await expect(elm.children[0].children[0].textContent.trim()).toBe('shadow');
-
-    await expect(elm.children[0].children[1].nodeName).toBe('DIV');
-    await expect(elm.children[0].children[1].textContent.trim()).toBe('light');
-
-    const shadowBG = window.getComputedStyle(elm.children[0].children[0]).backgroundColor;
-    await expect(shadowBG).toBe('rgb(0, 0, 0)');
-
-    const lightBG = window.getComputedStyle(elm.children[0].children[1]).backgroundColor;
-    await expect(lightBG).toBe('rgb(255, 255, 0)');
-  }
+  });
 });
