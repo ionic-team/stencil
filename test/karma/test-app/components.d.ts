05205f85f70f7de860400e2f4dca7f434ced3dac
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2024-03-22 17:14:02 -0400

    chore(wdio): migrate slot-no-default to wdio (#5573)
    port the `test/karma/test-app/slot-no-default` test suite to webdriverio
    
    git history has been preserved as much as possible here, using `git mv`
    to move files from the `karma` dir to `wdio/` in all possible cases.
    however, git may fail to mark some files as 'moved' if we change a file
    enough that it exceeds git's "sameness threshold".
    
    STENCIL-1191

--- test/karma/test-app/components.d.ts
+++ test/karma/test-app/components.d.ts
@@ -42,8 +42,6 @@ export namespace Components {
     }
     interface SlotNgIf {
     }
-    interface SlotNoDefault {
-    }
     interface SlotReplaceWrapper {
         "href"?: string;
     }
@@ -159,12 +157,6 @@ declare global {
         prototype: HTMLSlotNgIfElement;
         new (): HTMLSlotNgIfElement;
     };
-    interface HTMLSlotNoDefaultElement extends Components.SlotNoDefault, HTMLStencilElement {
-    }
-    var HTMLSlotNoDefaultElement: {
-        prototype: HTMLSlotNoDefaultElement;
-        new (): HTMLSlotNoDefaultElement;
-    };
     interface HTMLSlotReplaceWrapperElement extends Components.SlotReplaceWrapper, HTMLStencilElement {
     }
     var HTMLSlotReplaceWrapperElement: {
@@ -213,7 +205,6 @@ declare global {
         "slot-nested-default-order-child": HTMLSlotNestedDefaultOrderChildElement;
         "slot-nested-default-order-parent": HTMLSlotNestedDefaultOrderParentElement;
         "slot-ng-if": HTMLSlotNgIfElement;
-        "slot-no-default": HTMLSlotNoDefaultElement;
         "slot-replace-wrapper": HTMLSlotReplaceWrapperElement;
         "slot-replace-wrapper-root": HTMLSlotReplaceWrapperRootElement;
         "stencil-sibling": HTMLStencilSiblingElement;
@@ -258,8 +249,6 @@ declare namespace LocalJSX {
     }
     interface SlotNgIf {
     }
-    interface SlotNoDefault {
-    }
     interface SlotReplaceWrapper {
         "href"?: string;
     }
@@ -289,7 +278,6 @@ declare namespace LocalJSX {
         "slot-nested-default-order-child": SlotNestedDefaultOrderChild;
         "slot-nested-default-order-parent": SlotNestedDefaultOrderParent;
         "slot-ng-if": SlotNgIf;
-        "slot-no-default": SlotNoDefault;
         "slot-replace-wrapper": SlotReplaceWrapper;
         "slot-replace-wrapper-root": SlotReplaceWrapperRoot;
         "stencil-sibling": StencilSibling;
@@ -318,7 +306,6 @@ declare module "@stencil/core" {
             "slot-nested-default-order-child": LocalJSX.SlotNestedDefaultOrderChild & JSXBase.HTMLAttributes<HTMLSlotNestedDefaultOrderChildElement>;
             "slot-nested-default-order-parent": LocalJSX.SlotNestedDefaultOrderParent & JSXBase.HTMLAttributes<HTMLSlotNestedDefaultOrderParentElement>;
             "slot-ng-if": LocalJSX.SlotNgIf & JSXBase.HTMLAttributes<HTMLSlotNgIfElement>;
-            "slot-no-default": LocalJSX.SlotNoDefault & JSXBase.HTMLAttributes<HTMLSlotNoDefaultElement>;
             "slot-replace-wrapper": LocalJSX.SlotReplaceWrapper & JSXBase.HTMLAttributes<HTMLSlotReplaceWrapperElement>;
             "slot-replace-wrapper-root": LocalJSX.SlotReplaceWrapperRoot & JSXBase.HTMLAttributes<HTMLSlotReplaceWrapperRootElement>;
             "stencil-sibling": LocalJSX.StencilSibling & JSXBase.HTMLAttributes<HTMLStencilSiblingElement>;
