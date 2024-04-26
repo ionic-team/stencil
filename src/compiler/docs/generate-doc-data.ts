aea62759375ad6c2b5137834d43bec7a8448387e
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2024-04-26 12:46:21 -0400

    test(docs): unit test style doc generation (#5708)
    add unit tests for `src/compiler/docs/generate-doc-data.ts#getDocsStyles`.
    this was pulled out into a separate pull request from another effort,
    where we wish to add the `mode` that a stylesheet is associated with by
    a component's `@Component({})` decorator to the output of `docs-json`.
    these tests aren't strictly needed for that effort, but establish a
    baseline for tests taht will be added in the future.
    
    the `getDocsStyles` function was exported in order to test it.
    
    additional jsdoc and type annotations have been added to differentiate
    fields that are compiler metadata, and those that are used in our
    documentation intermediate representation.
    
    split from: STENCIL-1269 - CSS Documentation Should Account for Modes

--- src/compiler/docs/generate-doc-data.ts
+++ src/compiler/docs/generate-doc-data.ts
@@ -305,18 +305,26 @@ const getDocsEvents = (events: d.ComponentCompilerEvent[]): d.JsonDocsEvent[] =>
     }));
 };
 
-const getDocsStyles = (cmpMeta: d.ComponentCompilerMeta): d.JsonDocsStyle[] => {
+/**
+ * Transforms the {@link d.CompilerStyleDoc} metadata for a component into a {@link d.JsonDocsStyle}, providing sensible
+ * defaults where needed.
+ * @param cmpMeta the metadata for a single Stencil component, which contains the compiler style metadata
+ * @returns a new series containing a {@link d.JsonDocsStyle} entry for each {@link d.CompilerStyleDoc} entry.
+ */
+export const getDocsStyles = (cmpMeta: d.ComponentCompilerMeta): d.JsonDocsStyle[] => {
   if (!cmpMeta.styleDocs) {
     return [];
   }
 
-  return sortBy(cmpMeta.styleDocs, (o) => o.name.toLowerCase()).map((styleDoc) => {
-    return {
-      name: styleDoc.name,
-      annotation: styleDoc.annotation || '',
-      docs: styleDoc.docs || '',
-    };
-  });
+  return sortBy(cmpMeta.styleDocs, (compilerStyleDoc) => compilerStyleDoc.name.toLowerCase()).map(
+    (compilerStyleDoc) => {
+      return {
+        name: compilerStyleDoc.name,
+        annotation: compilerStyleDoc.annotation || '',
+        docs: compilerStyleDoc.docs || '',
+      };
+    },
+  );
 };
 
 const getDocsListeners = (listeners: d.ComponentCompilerListener[]): d.JsonDocsListener[] => {
