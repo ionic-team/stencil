171b239414e8b3e432482f0fc18219f97b2ce38a
Christian Bromann <git@bromann.dev>
2024-05-01 16:25:37 -0700

    fix tests

--- src/testing/test/functional.spec.tsx
+++ src/testing/test/functional.spec.tsx
@@ -1,12 +1,12 @@
 import { h } from '@stencil/core';
-import { newSpecPage } from '@stencil/core/testing';
+import { newSpecPage, SpecPage } from '@stencil/core/testing';
 
 import { ClassComponent } from './__fixtures__/cmp';
 
 describe('testing function and class components', () => {
   it('can render a single functional component', async () => {
     const MyFunctionalComponent = () => (<div>Hello World</div>)
-    const page = await newSpecPage({
+    const page: SpecPage = await newSpecPage({
       components: [MyFunctionalComponent],
       template: () => <MyFunctionalComponent></MyFunctionalComponent>,
     });
@@ -15,7 +15,7 @@ describe('testing function and class components', () => {
 
   it('can render a single functional component with props', async () => {
     const MyFunctionalComponent = (props: { foo: 'bar' }) => (<div>{props.foo}</div>)
-    const page = await newSpecPage({
+    const page: SpecPage = await newSpecPage({
       components: [MyFunctionalComponent],
       template: () => <MyFunctionalComponent foo="bar"></MyFunctionalComponent>,
     });
@@ -24,7 +24,7 @@ describe('testing function and class components', () => {
 
   it('can render a single functional component with children', async () => {
     const MyFunctionalComponent = (props: never, children) => (<div>{children}</div>)
-    const page = await newSpecPage({
+    const page: SpecPage = await newSpecPage({
       components: [MyFunctionalComponent],
       template: () => <MyFunctionalComponent>Hello World</MyFunctionalComponent>,
     });
@@ -33,7 +33,7 @@ describe('testing function and class components', () => {
 
   it('can render a single functional component with children and props', async () => {
     const MyFunctionalComponent = (props: { foo: 'bar' }, children) => (<div>{children} - {props.foo}</div>)
-    const page = await newSpecPage({
+    const page: SpecPage = await newSpecPage({
       components: [MyFunctionalComponent],
       template: () => <MyFunctionalComponent foo="bar">Hello World</MyFunctionalComponent>,
     });
@@ -42,13 +42,13 @@ describe('testing function and class components', () => {
 
   it('can render a class component with a functional component', async () => {
     const MyFunctionalComponent = (props: never, children: string) => (<div>I am a functional component - {children}</div>)
-    const page = await newSpecPage({
+    const page: SpecPage = await newSpecPage({
       components: [ClassComponent],
       template: () => <class-component>
         <MyFunctionalComponent>Yes I am!</MyFunctionalComponent>
       </class-component>,
     });
-    expect(page.root).toEqualHtml(`<class-component>
+    expect(page.bo).toEqualHtml(`<class-component>
   <mock:shadow-root>
     <div>
       <h1>
@@ -64,21 +64,32 @@ describe('testing function and class components', () => {
 `);
   });
 
-  /**
-   * This will not work as Stencil can only render a class component as a root
-   * TODO(@christian-bromann): create a Jira ticket for this?
-   */
-  it.skip('can render a functional component within a class component', async () => {
+  it('can render a functional component within a class component', async () => {
     const MyFunctionalComponent = (props: never, children: string) => (<div>
       <h1>I am a functional component</h1>
       {children}
     </div>)
-    const page = await newSpecPage({
+    const page: SpecPage = await newSpecPage({
       components: [ClassComponent],
       template: () => <MyFunctionalComponent>
         <class-component>Yes I am!</class-component>
       </MyFunctionalComponent>,
     });
-    expect(page.root).toEqualHtml('')
+    expect(page.body).toEqualHtml(`<div>
+    <h1>
+      I am a functional component
+    </h1>
+    <class-component>
+      <mock:shadow-root>
+        <div>
+          <h1>
+            I am a class component
+          </h1>
+          <slot></slot>
+        </div>
+      </mock:shadow-root>
+      Yes I am!
+    </class-component>
+  </div>`)
   });
 });
