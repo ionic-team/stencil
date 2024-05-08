0113022a716f8fb503a1558774be65041101ac8f
Christian Bromann <git@bromann.dev>
2024-05-08 08:29:33 -0700

    prettier

--- test/wdio/shadow-dom-mode/cmp.test.tsx
+++ test/wdio/shadow-dom-mode/cmp.test.tsx
@@ -16,12 +16,12 @@ describe('shadow-dom-mode', () => {
   it('renders', async () => {
     const blueElm = $('shadow-dom-mode[id="blue"]');
     await expect(blueElm).toHaveStyle({
-      'background-color': browser.isChromium ? 'rgba(0,0,255,1)' : 'rgb(0,0,255)'
+      'background-color': browser.isChromium ? 'rgba(0,0,255,1)' : 'rgb(0,0,255)',
     });
 
     const redElm = $('shadow-dom-mode[id="red"]');
     await expect(redElm).toHaveStyle({
-      'background-color': browser.isChromium ? 'rgba(255,0,0,1)' : 'rgb(255,0,0)'
+      'background-color': browser.isChromium ? 'rgba(255,0,0,1)' : 'rgb(255,0,0)',
     });
   });
 });
