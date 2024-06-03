ionic-team/stencil

commit 0b6621f21634b7498de0666a872ffcacc93fef87

feat(typescript): upgrade to TypeScript 5 (#4315)
This upgrades the version of TypeScript bundled with Stencil to 5.0.4. In order to support the upgrade we have to do a few things:

- edit the Rollup plugin we use to bundle typescript to account for changes in how typescript is bundled now (this changed for the 5.0.0 release)
- fix tests which assert on e.g. transformer output to account for small, mostly one-character differences in output with the strings we have in tests
- move the location where we prevent `.d.ts` files being written to disk during `stencil build` for test files
See also a documentation PR here: https://github.com/ionic-team/stencil-site/pull/1117

It was also necessary to make a few tweaks to how the Karma tests are run to account for some changes in how TypeScript resolves modules.

src/compiler/transpile/run-program.ts
@@ -40,6 +40,14 @@ export const runTsProgram = async (
   const emitCallback: ts.WriteFileCallback = (emitFilePath, data, _w, _e, tsSourceFiles) => {
     if (emitFilePath.endsWith('.js') || emitFilePath.endsWith('js.map')) {
       updateModule(config, compilerCtx, buildCtx, tsSourceFiles[0], data, emitFilePath, tsTypeChecker, null);
+    } else if (
+      emitFilePath.endsWith('.e2e.d.ts') ||
+      emitFilePath.endsWith('.spec.d.ts') ||
+      emitFilePath === 'e2e.d.ts' ||
+      emitFilePath === 'spec.d.ts'
+    ) {
+      // we don't want to write these to disk!
+      return;
     } else if (emitFilePath.endsWith('.d.ts')) {
       const srcDtsPath = normalizePath(tsSourceFiles[0].fileName);
       const relativeEmitFilepath = getRelativeDts(config, srcDtsPath, emitFilePath);

jest.config.js
@@ -11,6 +11,7 @@ module.exports = {
     '@stencil/core/mock-doc': '<rootDir>/mock-doc/index.cjs',
     '@stencil/core/testing': '<rootDir>/testing/index.js',
     '@utils': '<rootDir>/src/utils',
+    '^typescript$': '<rootDir>/scripts/build/typescript-modified-for-jest.js',
   },
   coverageDirectory: './coverage/',
   coverageReporters: ['json', 'lcov', 'text', 'clover'],

scripts/bundles/plugins/typescript-source-plugin.ts
@@ -62,57 +62,107 @@ async function bundleTypeScriptSource(tsPath: string, opts: BuildOptions): Promi
 
   // remove the default ts.getDefaultLibFilePath because it uses some
   // node apis and we'll be replacing it with our own anyways
-  code = removeFromSource(code, `ts.getDefaultLibFilePath = getDefaultLibFilePath;`);
+  // TODO(STENCIL-816): remove in-browser compilation
+  code = removeFromSource(code, `getDefaultLibFilePath: () => getDefaultLibFilePath,`);
 
   // remove the CPUProfiler since it uses node apis
-  code = removeFromSource(code, `enableCPUProfiler: enableCPUProfiler,`);
-  code = removeFromSource(code, `disableCPUProfiler: disableCPUProfiler,`);
+  // TODO(STENCIL-816): remove in-browser compilation
+  code = removeFromSource(code, `enableCPUProfiler,`);
+  // TODO(STENCIL-816): remove in-browser compilation
+  code = removeFromSource(code, `disableCPUProfiler,`);
+
+  // As of 5.0, because typescript is now bundled with esbuild the structure of
+  // the file we're dealing with here (`lib/typescript.js`) has changed.
+  // Previously there was an iife which got an object as an argument and just
+  // stuck properties onto it, something like
+  //
+  // ```js
+  // var ts = (function (ts) {
+  //   ts.someMethod = () => { ... };
+  // })(ts || ts = {});
+  // ```
+  //
+  // as of 5.0 it instead looks (conceptually) something like:
+  //
+  // ```js
+  // var ts = (function () {
+  //   const ts = {}
+  //   const define = (name, value) => {
+  //     Object.defineProperty(ts, name, value, { enumerable: true })
+  //   }
+  //   define('someMethod', () => { ... })
+  //   return ts;
+  // })();
+  // ```
+  //
+  // Note that the call to `Object.defineProperty` does not set `configurable` to `true`
+  // (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty#description)
+  // which means that later calls to do something like
+  //
+  // ```ts
+  // import ts from 'typescript';
+  //
+  // ts.someMethod = function myReplacementForSomeMethod () {
+  //   ...
+  // };
+  // ```
+  //
+  // will fail because without `configurable: true` you can't re-assign
+  // properties.
+  //
+  // All well and good, except for the fact that our patching of typescript to
+  // use for instance the in-memory file system depends on us being able to
+  // monkey-patch typescript in exactly this way. So in order to retain our
+  // current approach to patching TypeScript we need to edit this file in order
+  // to add `configurable: true` to the options passed to
+  // `Object.defineProperty`:
+  const TS_PROP_DEFINER = `__defProp(target, name, { get: all[name], enumerable: true });`;
+  const TS_PROP_DEFINER_RECONFIGURABLE = `__defProp(target, name, { get: all[name], enumerable: true, configurable: true });`;
+
+  code = code.replace(TS_PROP_DEFINER, TS_PROP_DEFINER_RECONFIGURABLE);
+
+  const jestTypesciptFilename = join(opts.scriptsBuildDir, 'typescript-modified-for-jest.js');
+  await fs.writeFile(jestTypesciptFilename, code);
+
+  // Here we transform the TypeScript source from a commonjs to an ES module.
+  // We do this so that we can add an import from the `@environment` module.
 
   // trim off the last part that sets module.exports and polyfills globalThis since
   // we don't want typescript to add itself to module.exports when in a node env
-  const tsEnding = `})(ts || (ts = {}));`;
+  const tsEnding = `if (typeof module !== "undefined" && module.exports) { module.exports = ts; }`;
+
   if (!code.includes(tsEnding)) {
     throw new Error(`"${tsEnding}" not found`);
   }
   const lastEnding = code.lastIndexOf(tsEnding);
-  code = code.slice(0, lastEnding + tsEnding.length);
+  code = code.slice(0, lastEnding);
 
-  // there's a billion unnecessary "var ts;" for namespaces
-  // but we'll be using the top level "const ts" instead
-  code = code.replace(/var ts;/g, '');
-
-  // minification is crazy better if it doesn't use typescript's
-  // namespace closures, like (function(ts) {...})(ts = ts || {});
-  code = code.replace(/ \|\| \(ts \= \{\}\)/g, '');
-
-  // make a nice clean default export
-  // "process.browser" is used by typescript to know if it should use the node sys or not
   const o: string[] = [];
   o.push(`// TypeScript ${opts.typescriptVersion}`);
   o.push(`import { IS_NODE_ENV } from '@environment';`);
   o.push(`process.browser = !IS_NODE_ENV;`);
-  o.push(`const ts = {};`);
   o.push(code);
   o.push(`export default ts;`);
   code = o.join('\n');
 
-  const { minify } = await import('terser');
+  // TODO(STENCIL-839): investigate minification issue w/ typescript 5.0
+  // const { minify } = await import('terser');
 
-  if (opts.isProd) {
-    const minified = await minify(code, {
-      ecma: 2018,
-      module: true,
-      compress: {
-        ecma: 2018,
-        passes: 2,
-      },
-      format: {
-        ecma: 2018,
-        comments: false,
-      },
-    });
-    code = minified.code;
-  }
+  // if (opts.isProd) {
+  //   const minified = await minify(code, {
+  //     ecma: 2018,
+  //     // module: true,
+  //     compress: {
+  //       ecma: 2018,
+  //       passes: 2,
+  //     },
+  //     format: {
+  //       ecma: 2018,
+  //       comments: false,
+  //     },
+  //   });
+  //   code = minified.code;
+  // }
 
   await fs.writeFile(cacheFile, code);
 

src/compiler/sys/typescript/typescript-sys.ts
@@ -198,7 +198,6 @@ export const patchTypescript = (config: d.Config, inMemoryFs: InMemoryFileSystem
       patchTsSystemWatch(config.sys, ts.sys);
     }
     patchTypeScriptResolveModule(config, inMemoryFs);
-    patchTypeScriptGetParsedCommandLineOfConfigFile();
     (ts as any).__patched = true;
   }
 };
@@ -242,27 +241,4 @@ export const getTypescriptPathFromUrl = (config: d.Config, tsExecutingUrl: strin
     return normalizePath(tsNodePath);
   }
   return url;
-};
-
-export const patchTypeScriptGetParsedCommandLineOfConfigFile = () => {
-  const orgGetParsedCommandLineOfConfigFile = ts.getParsedCommandLineOfConfigFile;
-
-  ts.getParsedCommandLineOfConfigFile = (configFileName, optionsToExtend, host, extendedConfigCache) => {
-    const results = orgGetParsedCommandLineOfConfigFile(configFileName, optionsToExtend, host, extendedConfigCache);
-
-    // manually filter out any .spec or .e2e files
-    results.fileNames = results.fileNames.filter((f) => {
-      // filter e2e tests
-      if (f.includes('.e2e.') || f.includes('/e2e.')) {
-        return false;
-      }
-      // filter spec tests
-      if (f.includes('.spec.') || f.includes('/spec.')) {
-        return false;
-      }
-      return true;
-    });
-
-    return results;
-  };
 };

src/compiler/transformers/test/convert-decorators.spec.ts
@@ -2,6 +2,7 @@ import * as ts from 'typescript';
 
 import { filterDecorators } from '../decorators-to-static/convert-decorators';
 import { transpileModule } from './transpile';
+import { formatCode } from './utils';
 
 /**
  * c for compact, c for class declaration, make of it what you will!
@@ -14,7 +15,7 @@ import { transpileModule } from './transpile';
  * @returns a formatted string!
  */
 function c(strings: TemplateStringsArray) {
-  return strings.join('').replace('\n', ' ').replace(/\s+/g, ' ');
+  return formatCode(strings.join(''));
 }
 
 describe('convert-decorators', () => {
@@ -29,32 +30,23 @@ describe('convert-decorators', () => {
     // we test the whole output here to ensure that the field has been
     // removed from the class body correctly and replaced with an initializer
     // in the constructor
-    expect(t.outputText).toBe(
+    expect(formatCode(t.outputText)).toBe(
       c`export class CmpA {
         constructor() {
           this.val = "initial value";
         }
-
         static get is() {
           return "cmp-a";
         }
-
         static get properties() {
           return {
             "val": {
               "type": "string",
               "mutable": false,
-              "complexType": {
-                "original": "string",
-                "resolved": "string",
-                "references": {}
-              },
+              "complexType": { "original": "string", "resolved": "string", "references": {} },
               "required": false,
               "optional": false,
-              "docs": {
-                "tags": [],
-                "text": ""
-              },
+              "docs": { "tags": [], "text": "" },
               "attribute": "val",
               "reflect": false,
               "defaultValue": "\\"initial value\\""
@@ -75,10 +67,10 @@ describe('convert-decorators', () => {
     // we test the whole output here to ensure that the field has been
     // removed from the class body correctly and replaced with an initializer
     // in the constructor
-    expect(t.outputText).toContain(
-      c`constructor() {
-          this.val = undefined;
-      }`
+    expect(formatCode(t.outputText)).toContain(
+      `  constructor() {
+    this.val = undefined;
+  }`
     );
   });
 
@@ -93,20 +85,16 @@ describe('convert-decorators', () => {
     // we test the whole output here to ensure that the field has been
     // removed from the class body correctly and replaced with an initializer
     // in the constructor
-    expect(t.outputText).toBe(
+    expect(formatCode(t.outputText)).toBe(
       c`export class CmpB {
         constructor() {
           this.count = 0;
         }
-
         static get is() {
           return "cmp-b";
         }
-
         static get states() {
-          return {
-            "count": {}
-          };
+          return { "count": {}};
         }}`
     );
   });
@@ -121,7 +109,7 @@ describe('convert-decorators', () => {
     // we test the whole output here to ensure that the field has been
     // removed from the class body correctly and replaced with an initializer
     // in the constructor
-    expect(t.outputText).toBe(
+    expect(formatCode(t.outputText)).toBe(
       c`export class CmpA {
         static get is() {
           return "cmp-a";
@@ -130,24 +118,22 @@ describe('convert-decorators', () => {
   });
 
   it('should add a super call to the constructor if necessary', () => {
-    const t = transpileModule(`
-    @Component({tag: 'cmp-a'})
+    const t = transpileModule(
+      `@Component({tag: 'cmp-a'})
       export class CmpA extends Foobar {
         @State() count: number = 0;
       }
-    `);
+    `
+    );
 
-    expect(t.outputText).toBe(
+    expect(formatCode(t.outputText)).toBe(
       c`export class CmpA extends Foobar {
         constructor() {
           super();
           this.count = 0;
         }
-
         static get states() {
-          return {
-            "count": {}
-          };
+          return { "count": {} };
         }}`
     );
   });
@@ -163,12 +149,11 @@ describe('convert-decorators', () => {
       }
     }`);
 
-    expect(t.outputText).toBe(
+    expect(formatCode(t.outputText)).toBe(
       c`export class MyComponent {
         constructor() {
           console.log('boop');
         }
-
         static get is() {
           return "my-component";
       }}`
@@ -188,17 +173,17 @@ describe('convert-decorators', () => {
       }
     }`);
 
-    expect(t.outputText).toContain(
-      c`constructor() {
-          this.count = undefined;
-          console.log('boop');
-        }`
+    expect(formatCode(t.outputText)).toContain(
+      `  constructor() {
+    this.count = undefined;
+    console.log('boop');
+  }`
     );
   });
 
   it('should allow user to initialize field in an existing constructor w/ @Prop', () => {
-    const t = transpileModule(`
-    @Component({
+    const t = transpileModule(
+      `@Component({
       tag: 'my-component',
     })
     export class MyComponent {
@@ -207,16 +192,17 @@ describe('convert-decorators', () => {
       constructor() {
         this.count = 3;
       }
-    }`);
+    }`
+    );
 
     // the initialization we do to `undefined` (since no value is present)
     // should be before the user's `this.count = 3` to ensure that their code
     // wins.
-    expect(t.outputText).toContain(
-      c`constructor() {
-          this.count = undefined;
-          this.count = 3;
-        }`
+    expect(formatCode(t.outputText)).toContain(
+      `  constructor() {
+    this.count = undefined;
+    this.count = 3;
+  }`
     );
   });
 
@@ -233,7 +219,7 @@ describe('convert-decorators', () => {
       }
     }`);
 
-    expect(t.outputText).toBe(
+    expect(formatCode(t.outputText)).toBe(
       c`export class Example {
         constructor() {
           this.classProps = ["variant", "theme"];
@@ -254,12 +240,12 @@ describe('convert-decorators', () => {
       }
     }`);
 
-    expect(t.outputText).toContain(
-      c`constructor() {
-        super();
-        this.foo = "bar";
-        console.log("hello!");
-      }`
+    expect(formatCode(t.outputText)).toContain(
+      `  constructor() {
+    super();
+    this.foo = 'bar';
+    console.log('hello!');
+  }`
     );
   });
 
@@ -271,20 +257,16 @@ describe('convert-decorators', () => {
       }
     `);
 
-    expect(t.outputText).toBe(
+    expect(formatCode(formatCode(t.outputText))).toBe(
       c`export class CmpA {
         constructor() {
           this.count = 0;
         }
-
         static get is() {
           return "cmp-a";
         }
-
         static get states() {
-          return {
-            "count": {}
-          };
+          return { "count": {} };
         }}`
     );
   });
@@ -306,12 +288,11 @@ describe('convert-decorators', () => {
     // we test the whole output here to ensure that the field has been
     // removed from the class body correctly and replaced with an initializer
     // in the constructor
-    expect(t.outputText).toBe(
+    expect(formatCode(t.outputText)).toBe(
       c`export class CmpA {
         static get is() {
           return "cmp-a";
         }
-
         static get events() {
           return [{
             "method": "myEventWithOptions",
@@ -319,15 +300,8 @@ describe('convert-decorators', () => {
             "bubbles": false,
             "cancelable": false,
             "composed": true,
-            "docs": {
-              "tags": [],
-              "text": ""
-            },
-            "complexType": {
-              "original": "{ mph: number }",
-              "resolved": "{ mph: number; }",
-              "references": {}
-            }
+            "docs": { "tags": [], "text": "" },
+            "complexType": { "original": "{ mph: number }", "resolved": "{ mph: number; }", "references": {} }
           }];
       }}`
     );

src/compiler/transformers/test/parse-styles.spec.ts
@@ -1,4 +1,5 @@
 import { getStaticGetter, transpileModule } from './transpile';
+import { formatCode } from './utils';
 
 describe('parse styles', () => {
   it('add static "styleUrl"', () => {
@@ -49,8 +50,10 @@ describe('parse styles', () => {
       })
       export class CmpA {}
     `);
-    expect(t.outputText).toEqual(
-      `const md = 'p{color:red}';const ios = 'p{color:black}';export class CmpA { static get is() { return "cmp-a"; } static get styles() { return { "md": md, "ios": ios }; }}`
+    expect(formatCode(t.outputText)).toEqual(
+      formatCode(
+        `const md = 'p{color:red}';const ios = 'p{color:black}';export class CmpA { static get is() { return "cmp-a"; } static get styles() { return { "md": md, "ios": ios }; }}`
+      )
     );
   });
 
@@ -81,8 +84,10 @@ describe('parse styles', () => {
       })
       export class CmpA {}
     `);
-    expect(t.outputText).toEqual(
-      `const styles = 'p{color:red}';export class CmpA { static get is() { return "cmp-a"; } static get styles() { return styles; }}`
+    expect(formatCode(t.outputText)).toEqual(
+      formatCode(
+        `const styles = 'p{color:red}';export class CmpA { static get is() { return "cmp-a"; } static get styles() { return styles; }}`
+      )
     );
   });
 });

src/compiler/transformers/test/proxy-custom-element-function.spec.ts
@@ -7,6 +7,7 @@ import { proxyCustomElement } from '../component-native/proxy-custom-element-fun
 import { PROXY_CUSTOM_ELEMENT } from '../core-runtime-apis';
 import * as TransformUtils from '../transform-utils';
 import { transpileModule } from './transpile';
+import { formatCode } from './utils';
 
 describe('proxy-custom-element-function', () => {
   const componentClassName = 'MyComponent';
@@ -80,8 +81,10 @@ describe('proxy-custom-element-function', () => {
       const transformer = proxyCustomElement(compilerCtx, transformOpts);
       const transpiledModule = transpileModule(code, null, compilerCtx, [], [transformer]);
 
-      expect(transpiledModule.outputText).toContain(
-        `export const ${componentClassName} = /*@__PURE__*/ __stencil_proxyCustomElement(class ${componentClassName} extends HTMLElement {}, true);`
+      expect(formatCode(transpiledModule.outputText)).toContain(
+        formatCode(
+          `export const ${componentClassName} = /*@__PURE__*/ __stencil_proxyCustomElement(class ${componentClassName} extends HTMLElement {}, true);`
+        )
       );
     });
 
@@ -92,8 +95,10 @@ describe('proxy-custom-element-function', () => {
         const transformer = proxyCustomElement(compilerCtx, transformOpts);
         const transpiledModule = transpileModule(code, null, compilerCtx, [], [transformer]);
 
-        expect(transpiledModule.outputText).toContain(
-          `export const foo = 'hello world!', ${componentClassName} = /*@__PURE__*/ __stencil_proxyCustomElement(class ${componentClassName} extends HTMLElement {}, true);`
+        expect(formatCode(transpiledModule.outputText)).toContain(
+          formatCode(
+            `export const foo = 'hello world!', ${componentClassName} = /*@__PURE__*/ __stencil_proxyCustomElement(class ${componentClassName} extends HTMLElement {}, true);`
+          )
         );
       });
 
@@ -103,8 +108,10 @@ describe('proxy-custom-element-function', () => {
         const transformer = proxyCustomElement(compilerCtx, transformOpts);
         const transpiledModule = transpileModule(code, null, compilerCtx, [], [transformer]);
 
-        expect(transpiledModule.outputText).toContain(
-          `export const ${componentClassName} = /*@__PURE__*/ __stencil_proxyCustomElement(class ${componentClassName} extends HTMLElement {}, true), foo = 'hello world!';`
+        expect(formatCode(transpiledModule.outputText)).toContain(
+          formatCode(
+            `export const ${componentClassName} = /*@__PURE__*/ __stencil_proxyCustomElement(class ${componentClassName} extends HTMLElement {}, true), foo = 'hello world!';`
+          )
         );
       });
 
@@ -114,8 +121,10 @@ describe('proxy-custom-element-function', () => {
         const transformer = proxyCustomElement(compilerCtx, transformOpts);
         const transpiledModule = transpileModule(code, null, compilerCtx, [], [transformer]);
 
-        expect(transpiledModule.outputText).toContain(
-          `export const foo = 'hello world!', ${componentClassName} = /*@__PURE__*/ __stencil_proxyCustomElement(class ${componentClassName} extends HTMLElement {}, true), bar = 'goodbye?';`
+        expect(formatCode(transpiledModule.outputText)).toContain(
+          formatCode(
+            `export const foo = 'hello world!', ${componentClassName} = /*@__PURE__*/ __stencil_proxyCustomElement(class ${componentClassName} extends HTMLElement {}, true), bar = 'goodbye?';`
+          )
         );
       });
     });
@@ -135,7 +144,7 @@ describe('proxy-custom-element-function', () => {
       const transformer = proxyCustomElement(compilerCtx, transformOpts);
       const transpiledModule = transpileModule(code, null, compilerCtx, [], [transformer]);
 
-      expect(transpiledModule.outputText).toBe(code);
+      expect(formatCode(transpiledModule.outputText)).toBe(formatCode(code));
     });
 
     it('returns the source file when no variable statements are found', () => {
@@ -155,7 +164,7 @@ describe('proxy-custom-element-function', () => {
       const transformer = proxyCustomElement(compilerCtx, transformOpts);
       const transpiledModule = transpileModule(code, null, compilerCtx, [], [transformer]);
 
-      expect(transpiledModule.outputText).toBe(code);
+      expect(transpiledModule.outputText.trim()).toBe(code);
     });
 
     it("returns the source file when variable statements don't match the component name", () => {
@@ -170,12 +179,12 @@ describe('proxy-custom-element-function', () => {
         } as d.Module;
       });
 
-      const code = `const ${componentClassName} = class extends HTMLElement {};`;
+      const code = `const ${componentClassName} = class extends HTMLElement { };`;
 
       const transformer = proxyCustomElement(compilerCtx, transformOpts);
       const transpiledModule = transpileModule(code, null, compilerCtx, [], [transformer]);
 
-      expect(transpiledModule.outputText).toBe(code);
+      expect(transpiledModule.outputText.trim()).toBe(code);
     });
   });
 });

src/compiler/transformers/test/rewrite-aliased-paths.spec.ts
@@ -7,6 +7,7 @@ import ts from 'typescript';
 import { patchTypescript } from '../../sys/typescript/typescript-sys';
 import { rewriteAliasedDTSImportPaths, rewriteAliasedSourceFileImportPaths } from '../rewrite-aliased-paths';
 import { transpileModule } from './transpile';
+import { formatCode } from './utils';
 
 /**
  * Helper function for running the transpilation for tests in this module.
@@ -61,8 +62,10 @@ describe('rewrite alias module paths transform', () => {
       }
     `);
 
-    expect(t.outputText).toBe(
-      'import { foo } from "./name/space";export class CmpA { render() { return h("some-cmp", null, foo("bar")); }}'
+    expect(formatCode(t.outputText)).toBe(
+      formatCode(
+        'import { foo } from "./name/space";export class CmpA { render() { return h("some-cmp", null, foo("bar")); }}'
+      )
     );
   });
 
@@ -76,8 +79,10 @@ describe('rewrite alias module paths transform', () => {
       }
     `);
 
-    expect(t.outputText).toBe(
-      'import { foo } from "./name/space/subdir";export class CmpA { render() { return h("some-cmp", null, foo("bar")); }}'
+    expect(formatCode(t.outputText)).toBe(
+      formatCode(
+        'import { foo } from "./name/space/subdir";export class CmpA { render() { return h("some-cmp", null, foo("bar")); }}'
+      )
     );
   });
 
@@ -95,8 +100,8 @@ describe('rewrite alias module paths transform', () => {
       }
     `);
 
-    expect(t.declarationOutputText).toBe(
-      'import { Foo } from "./name/space";export declare class CmpA { field: Foo; render(): any;}'
+    expect(formatCode(t.declarationOutputText)).toBe(
+      formatCode('import { Foo } from "./name/space";export declare class CmpA { field: Foo; render(): any;}')
     );
   });
 
@@ -109,8 +114,8 @@ describe('rewrite alias module paths transform', () => {
       }
     `);
 
-    expect(t.declarationOutputText).toBe(
-      'import { Foo } from "./name/space/subdir";export declare function fooUtil(foo: Foo): Foo;'
+    expect(formatCode(t.declarationOutputText)).toBe(
+      formatCode('import { Foo } from "./name/space/subdir";export declare function fooUtil(foo: Foo): Foo;')
     );
   });
 
@@ -124,8 +129,10 @@ describe('rewrite alias module paths transform', () => {
       }
     `);
 
-    expect(t.declarationOutputText).toBe(
-      'import { Foo } from "./name/space/subdir";import { Bar } from "./name/space";export declare function fooUtil(foo: Foo): Bar;'
+    expect(formatCode(t.declarationOutputText)).toBe(
+      formatCode(
+        'import { Foo } from "./name/space/subdir";import { Bar } from "./name/space";export declare function fooUtil(foo: Foo): Bar;'
+      )
     );
   });
 
@@ -139,8 +146,10 @@ describe('rewrite alias module paths transform', () => {
       }
     `);
 
-    expect(t.declarationOutputText).toBe(
-      'import { Foo } from "./name/space/subdir";import { Bar } from "./name/space";export declare function fooUtil(foo: Foo): Bar;'
+    expect(formatCode(t.declarationOutputText)).toBe(
+      formatCode(
+        'import { Foo } from "./name/space/subdir";import { Bar } from "./name/space";export declare function fooUtil(foo: Foo): Bar;'
+      )
     );
   });
 
@@ -168,8 +177,10 @@ describe('rewrite alias module paths transform', () => {
     //
     // we need to test that the relative path from `name/component.tsx` to
     // `name/space.ts` is resolved correctly as `'./space'`.
-    expect(t.outputText).toBe(
-      'import { foo } from "./space";export class CmpA { render() { return h("some-cmp", null, foo("bar")); }}'
+    expect(formatCode(t.outputText)).toBe(
+      formatCode(
+        'import { foo } from "./space";export class CmpA { render() { return h("some-cmp", null, foo("bar")); }}'
+      )
     );
   });
 
@@ -198,8 +209,15 @@ describe('rewrite alias module paths transform', () => {
     //
     // we need to test that the relative path from `name/component.tsx` to
     // `name/space/subdir.ts` is resolved correctly as `'./space/subdir'`.
-    expect(t.outputText).toBe(
-      'import { foo } from "./space/subdir";export class CmpA { render() { return h("some-cmp", null, foo("bar")); }}'
+    expect(formatCode(t.outputText)).toBe(
+      formatCode(
+        `import { foo } from "./space/subdir";
+        export class CmpA {
+          render() {
+            return h("some-cmp", null, foo("bar"));
+          }
+        }`
+      )
     );
   });
 });

commit 715f36c18ee10db82b186bb7c9665bf9aa1885ea

fix(emit): don't emit test files
This fixes a bug introduced in #4315, which upgraded to TypeScript 5. In
that PR we had to change the way that we prevented certain files from
being emitted by the typescript compiler because in the 5.0 release our
previous approach stopped working.

However, in porting over to a new approach that worked with TS 5.0 there
was an oversight. I misunderstood the intent of the old code as being to
merely prevent writing `.d.ts` files in the output, when actually it was
about preventing the compiled JavaScript from being written for
`.e2e.ts` and `.spec.ts` files.

This change ensures that we no longer emit these files.

fixes #5788
STENCIL-1325

src/compiler/transpile/run-program.ts
@@ -43,16 +43,18 @@ export const runTsProgram = async (
   const typesOutputTarget = config.outputTargets.filter(isOutputTargetDistTypes);
   const emittedDts: string[] = [];
   const emitCallback: ts.WriteFileCallback = (emitFilePath, data, _w, _e, tsSourceFiles) => {
-    if (emitFilePath.endsWith('.js') || emitFilePath.endsWith('js.map')) {
-      updateModule(config, compilerCtx, buildCtx, tsSourceFiles[0], data, emitFilePath, tsTypeChecker, null);
-    } else if (
-      emitFilePath.endsWith('.e2e.d.ts') ||
-      emitFilePath.endsWith('.spec.d.ts') ||
-      emitFilePath === 'e2e.d.ts' ||
-      emitFilePath === 'spec.d.ts'
+    if (
+      emitFilePath.includes('e2e.') ||
+      emitFilePath.includes('spec.') ||
+      emitFilePath.endsWith('e2e.d.ts') ||
+      emitFilePath.endsWith('spec.d.ts')
     ) {
       // we don't want to write these to disk!
       return;
+    }
+
+    if (emitFilePath.endsWith('.js') || emitFilePath.endsWith('js.map')) {
+      updateModule(config, compilerCtx, buildCtx, tsSourceFiles[0], data, emitFilePath, tsTypeChecker, null);
     } else if (emitFilePath.endsWith('.d.ts')) {
       const srcDtsPath = normalizePath(tsSourceFiles[0].fileName);
       const relativeEmitFilepath = getRelativeDts(config, srcDtsPath, emitFilePath);