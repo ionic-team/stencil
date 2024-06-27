5fabf72518028b8b2b20a7186e99273e19106e47
Christian Bromann <git@bromann.dev>
2024-06-27 09:39:12 -0700

    use custom stencil config for building end-to-end tests

---
+++ test/end-to-end/stencil.build.config.ts
@@ -0,0 +1,9 @@
+import { config as baseConfig } from './stencil.config';
+
+/**
+ * We are using a slightly different tsconfig to build the end-to-end tests
+ * as we are doing type assertions on the hydrate module which is not available
+ * when the project hasn't been built.
+ */
+baseConfig.tsconfig = './tsconfig.build.json';
+export const config = baseConfig
