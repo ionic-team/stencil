ace66cbf5d025dc34f1008499ef407ba35da9017
Christian Bromann <git@bromann.dev>
2024-06-27 10:46:07 -0700

    prettier

--- test/end-to-end/exportMap/index.js
+++ test/end-to-end/exportMap/index.js
@@ -1,13 +1,13 @@
-const assert = require('node:assert')
+const assert = require('node:assert');
 
-const { version } = require('@stencil/core/compiler')
-const { run } = require('@stencil/core/cli')
-const { h } = require('@stencil/core')
-const { MockDocument } = require('@stencil/core/mock-doc')
-const appData = require('@stencil/core/internal/app-data')
+const { version } = require('@stencil/core/compiler');
+const { run } = require('@stencil/core/cli');
+const { h } = require('@stencil/core');
+const { MockDocument } = require('@stencil/core/mock-doc');
+const appData = require('@stencil/core/internal/app-data');
 
-assert(typeof version === 'string')
-assert(typeof run, 'function')
-assert(typeof h === 'function')
-assert(typeof MockDocument === 'function')
-assert(Object.keys(appData).length === 3)
+assert(typeof version === 'string');
+assert(typeof run, 'function');
+assert(typeof h === 'function');
+assert(typeof MockDocument === 'function');
+assert(Object.keys(appData).length === 3);
