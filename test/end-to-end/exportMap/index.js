c41057c900169b60bc41504121e00886c9c8e0ad
Christian Bromann <git@bromann.dev>
2024-07-18 16:28:55 -0700

    fix: add @stencil/core/testing/jest-preset to export map

--- test/end-to-end/exportMap/index.js
+++ test/end-to-end/exportMap/index.js
@@ -7,6 +7,7 @@ const { MockDocument } = require('@stencil/core/mock-doc');
 const appData = require('@stencil/core/internal/app-data');
 const { createNodeLogger } = require('@stencil/core/sys/node');
 const { createTesting } = require('@stencil/core/testing');
+const preset = require('@stencil/core/testing/jest-preset');
 
 assert(typeof version === 'string');
 assert(typeof run, 'function');
@@ -15,6 +16,7 @@ assert(typeof MockDocument === 'function');
 assert(Object.keys(appData).length === 3);
 assert(typeof createNodeLogger === 'function');
 assert(typeof createTesting === 'function');
+assert(preset.moduleFileExtensions);
 
 console.log(`🎉 All CJS imports successfully resolved!`);
 console.log('✅ passed!\n');
