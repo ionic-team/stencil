80810d6c3d146dbfb6b3bae3ea9023df67b0bec0
Christian Bromann <git@bromann.dev>
2024-08-16 15:08:30 -0700

    fix(runtime): have fallback for style setting

--- src/runtime/styles.ts
+++ src/runtime/styles.ts
@@ -112,6 +112,8 @@ export const addStyle = (styleContainerNode: any, cmpMeta: d.ComponentRuntimeMet
                * at the beginning of the shadow root node
                */
               (styleContainerNode as HTMLElement).prepend(styleElm);
+            } else {
+              styleContainerNode.append(styleElm);
             }
           }
 
