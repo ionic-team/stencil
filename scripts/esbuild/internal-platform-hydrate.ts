81135c7156812e7128ef3df61b2d14bc04e1cb86
George Payne <georgepayne@hotmail.com>
2024-10-01 18:51:21 +0200

    fix(hydrate): Add missing alias to hydrate build to fix app globals

--- scripts/esbuild/internal-platform-hydrate.ts
+++ scripts/esbuild/internal-platform-hydrate.ts
@@ -54,6 +54,7 @@ export async function getInternalPlatformHydrateBundles(opts: BuildOptions): Pro
     plugins: [
       externalAlias('@utils/shadow-css', '../client/shadow-css.js'),
       externalAlias('@app-data', '@stencil/core/internal/app-data'),
+      externalAlias('@app-globals', '@stencil/core/internal/app-globals'),
     ],
   };
 
