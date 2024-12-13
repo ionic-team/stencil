37078e90a694d1b1826fa4bec9c7d899590be7a5
Christian Bromann <git@bromann.dev>
2024-12-13 15:29:32 -0800

    fix(mock-doc): don't force template tags to have a shadowroot

--- src/mock-doc/serialize-node.ts
+++ src/mock-doc/serialize-node.ts
@@ -126,19 +126,6 @@ function* streamToHtml(
       yield '<' + tag;
       output.currentLineWidth += tag.length + 1;
 
-      /**
-       * ToDo(https://github.com/ionic-team/stencil/issues/4111): the shadow root class is `#document-fragment`
-       * and has no mode attribute. We should consider adding a mode attribute.
-       */
-      if (
-        tag === 'template' &&
-        (!(node as Element).getAttribute || !(node as Element).getAttribute('shadowrootmode'))
-      ) {
-        const mode = ` shadowrootmode="open"`;
-        yield mode;
-        output.currentLineWidth += mode.length;
-      }
-
       const attrsLength = (node as HTMLElement).attributes.length;
       const attributes =
         opts.prettyHtml && attrsLength > 1
