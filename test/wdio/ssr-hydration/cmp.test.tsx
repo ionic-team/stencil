3912dbd1420ddfa7fe26107d4f15979346a1b098
John Jenkins <john.jenkins@nanoporetech.com>
2025-01-29 12:03:12 +0000

    fix(ssr): correctly resolve slots during hydration

--- test/wdio/ssr-hydration/cmp.test.tsx
+++ test/wdio/ssr-hydration/cmp.test.tsx
@@ -50,17 +50,19 @@ describe('ssr-shadow-cmp', () => {
     // wait for Stencil to take over and reconcile
     await browser.waitUntil(async () => customElements.get('ssr-shadow-cmp'));
     expect(typeof customElements.get('ssr-shadow-cmp')).toBe('function');
+
     await expect(getNodeNames(document.querySelector('ssr-shadow-cmp').childNodes)).toBe(
       `text comment div comment text`,
     );
 
     document.querySelector('#stage')?.remove();
+    await browser.waitUntil(async () => !document.querySelector('#stage'));
   });
 
   it('checks perf when loading lots of the same component', async () => {
     performance.mark('start');
 
-    const { html } = await renderToString(
+    await renderToString(
       Array(50)
         .fill(0)
         .map((_, i) => `<ssr-shadow-cmp>Value ${i}</ssr-shadow-cmp>`)
@@ -71,15 +73,39 @@ describe('ssr-shadow-cmp', () => {
         constrainTimeouts: false,
       },
     );
-    const stage = document.createElement('div');
-    stage.setAttribute('id', 'stage');
-    stage.setHTMLUnsafe(html);
-    document.body.appendChild(stage);
-
     performance.mark('end');
     const renderTime = performance.measure('render', 'start', 'end').duration;
+    await expect(renderTime).toBeLessThan(50);
+  });
 
-    await expect(renderTime).toBeLessThan(100);
+  it('resolves slots correctly during client-side hydration', async () => {
+    if (!document.querySelector('#stage')) {
+      const { html } = await renderToString(
+        `
+        <ssr-shadow-cmp>
+          <p>Default slot content</p>
+        </ssr-shadow-cmp>
+      `,
+        {
+          fullDocument: true,
+          serializeShadowRoot: true,
+          constrainTimeouts: false,
+        },
+      );
+      const stage = document.createElement('div');
+      stage.setAttribute('id', 'stage');
+      stage.setHTMLUnsafe(html);
+      document.body.appendChild(stage);
+    }
+
+    // @ts-expect-error resolved through WDIO
+    const { defineCustomElements } = await import('/dist/loader/index.js');
+    defineCustomElements().catch(console.error);
+
+    // wait for Stencil to take over and reconcile
+    await browser.waitUntil(async () => customElements.get('ssr-shadow-cmp'));
+    expect(typeof customElements.get('ssr-shadow-cmp')).toBe('function');
+    await expect(document.querySelector('ssr-shadow-cmp').textContent).toBe(' Default slot content ');
 
     document.querySelector('#stage')?.remove();
   });
