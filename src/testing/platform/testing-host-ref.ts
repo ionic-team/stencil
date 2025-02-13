6424713749fd9174cc24cbcf3e1242e8483a0f38
Daniel Leroux <daniel.leroux@siemens.com>
2025-02-13 07:28:44 +0100

    remove WeapMap implementation from hydration and testing

--- src/testing/platform/testing-host-ref.ts
+++ src/testing/platform/testing-host-ref.ts
@@ -1,37 +1,26 @@
 import type * as d from '@stencil/core/internal';
 
-import { hostRefs } from './testing-constants';
-
 /**
  * Retrieve the data structure tracking the component by its runtime reference
  * @param elm the reference to the element
  * @returns the corresponding Stencil reference data structure, or undefined if one cannot be found
  */
 export const getHostRef = (elm: d.RuntimeRef | undefined): d.HostRef | undefined => {
-  return hostRefs.get(elm);
-};
+  if (elm.__stencil__getHostRef) {
+    return elm.__stencil__getHostRef();
+  }
 
-/**
- * Given a {@link d.RuntimeRef} remove the corresponding {@link d.HostRef} from
- * the {@link hostRefs} WeakMap.
- *
- * @param ref the runtime ref of interest
- * @returns — true if the element was successfully removed, or false if it was not present.
- */
-export const deleteHostRef = (ref: d.RuntimeRef) => hostRefs.delete(ref);
+  return undefined;
+};
 
 /**
  * Add the provided `hostRef` instance to the global {@link hostRefs} map, using the provided `lazyInstance` as a key.
  * @param lazyInstance a Stencil component instance
  * @param hostRef an optional reference to Stencil's tracking data for the component. If none is provided, one will be created.
- * @returns the updated `hostRefs` data structure
  * @throws if the provided `lazyInstance` coerces to `null`, or if the `lazyInstance` does not have a `constructor`
  * property
  */
-export const registerInstance = (
-  lazyInstance: any,
-  hostRef: d.HostRef | null | undefined,
-): Map<d.RuntimeRef, d.HostRef> => {
+export const registerInstance = (lazyInstance: any, hostRef: d.HostRef | null | undefined) => {
   if (lazyInstance == null || lazyInstance.constructor == null) {
     throw new Error(`Invalid component constructor`);
   }
@@ -44,8 +33,8 @@ export const registerInstance = (
     hostRef = getHostRef(elm);
   }
 
+  lazyInstance.__stencil__getHostRef = () => hostRef;
   hostRef.$lazyInstance$ = lazyInstance;
-  return hostRefs.set(lazyInstance, hostRef);
 };
 
 /**
@@ -65,5 +54,5 @@ export const registerHost = (elm: d.HostElement, cmpMeta: d.ComponentRuntimeMeta
   hostRef.$onReadyPromise$ = new Promise((r) => (hostRef.$onReadyResolve$ = r));
   elm['s-p'] = [];
   elm['s-rc'] = [];
-  hostRefs.set(elm, hostRef);
+  elm.__stencil__getHostRef = () => hostRef;
 };
