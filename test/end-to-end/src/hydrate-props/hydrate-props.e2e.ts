07e43b02d9e349634809fb6748b4b00dae8b0abb
Christian Bromann <git@bromann.dev>
2025-02-13 16:44:59 -0800

    prettier

--- test/end-to-end/src/hydrate-props/hydrate-props.e2e.ts
+++ test/end-to-end/src/hydrate-props/hydrate-props.e2e.ts
@@ -2,7 +2,6 @@ type HydrateModule = typeof import('../../hydrate');
 let renderToString: HydrateModule['renderToString'];
 
 describe('different types of decorated properties and states render on both server and client', () => {
-
   beforeAll(async () => {
     // @ts-ignore may not be existing when project hasn't been built
     const mod = await import('../../hydrate');
@@ -10,21 +9,24 @@ describe('different types of decorated properties and states render on both serv
   });
 
   it('renders default values', async () => {
-    const { html } = await renderToString(`
+    const { html } = await renderToString(
+      `
       <my-cmp foo-prop="foo1" bar-prop="bar1"></my-cmp>
       <my-cmp fooProp="foo2" barProp="bar2"></my-cmp>
-    `, {
-      fullDocument: false,
-    });
+    `,
+      {
+        fullDocument: false,
+      },
+    );
     // html template renders kebab case props
-    expect(html).toContain('<!--t.1.1.1.0-->foo1 - bar1<')
+    expect(html).toContain('<!--t.1.1.1.0-->foo1 - bar1<');
     // html template doesn't support camelcase
-    expect(html).toContain('<!--t.4.1.1.0--> - bar<')
+    expect(html).toContain('<!--t.4.1.1.0--> - bar<');
     // jsx template renders kebab case
-    expect(html).toContain('<!--t.2.1.1.0-->foo3 - bar3<')
-    expect(html).toContain('<!--t.5.1.1.0-->foo3 - bar3<')
+    expect(html).toContain('<!--t.2.1.1.0-->foo3 - bar3<');
+    expect(html).toContain('<!--t.5.1.1.0-->foo3 - bar3<');
     // jsx template renders camel case
-    expect(html).toContain('<!--t.3.1.1.0-->foo4 - bar4<')
-    expect(html).toContain('<!--t.6.1.1.0-->foo4 - bar4<')
+    expect(html).toContain('<!--t.3.1.1.0-->foo4 - bar4<');
+    expect(html).toContain('<!--t.6.1.1.0-->foo4 - bar4<');
   });
 });
